095aecd72b5f35b655fd9fc0e43c1a2c53b4c9c2
thirofoo <thirofoo@gmail.com>
2021-10-03 12:16:31 +0900

    [WA] SubmissionID: 26329653 at 2021-10-03 12:16:31 (1633230991)

---
+++ APG4b/APG4b_cm_26329653_WA.cpp
@@ -0,0 +1,31 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/APG4b/submissions/26329653
+ * Submitted at: 2021-10-03 12:16:31
+ * Problem URL: https://atcoder.jp/contests/APG4b/tasks/APG4b_cm
+ * Result: WA
+ * Execution Time: 7 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int A,B;
+  cin >> A >> B;
+  int i=1 , j=1;
+  
+  
+  cout << "A:";
+  while( i <= A ){
+    cout << "]";
+    i++;
+  }
+  cout << endl;
+  
+  cout << "B:";
+  while( j <= B ){
+    cout << "]";
+    j++;
+  }
+}
