2cf501e76572f0170f2da2ad4e3adbb948a95c39
thirofoo <thirofoo@gmail.com>
2021-10-03 11:41:00 +0900

    [AC] SubmissionID: 26328973 at 2021-10-03 11:41:00 (1633228860)

---
+++ APG4b/APG4b_cr_26328973_AC.cpp
@@ -0,0 +1,18 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/APG4b/submissions/26328973
+ * Submitted at: 2021-10-03 11:41:00
+ * Problem URL: https://atcoder.jp/contests/APG4b/tasks/APG4b_cr
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  // ここに追記
+  int A, B;
+  cin >> A >> B;
+  cout << A + B << endl;
+}
