21c9221a5ed339a96cfde164724e91326c1651b1
thirofoo <thirofoo@gmail.com>
2021-10-03 11:18:11 +0900

    [AC] SubmissionID: 26328496 at 2021-10-03 11:18:11 (1633227491)

---
+++ APG4b/APG4b_a_26328496_AC.cpp
@@ -0,0 +1,15 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/APG4b/submissions/26328496
+ * Submitted at: 2021-10-03 11:18:11
+ * Problem URL: https://atcoder.jp/contests/APG4b/tasks/APG4b_a
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  cout << "Hello, world!" << endl;
+}
