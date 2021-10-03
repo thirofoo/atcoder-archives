1fa9d084bbf040a0a5b62be73cca7f8de3c0bf42
thirofoo <thirofoo@gmail.com>
2021-10-03 11:29:18 +0900

    [WA] SubmissionID: 26328758 at 2021-10-03 11:29:18 (1633228158)

---
+++ APG4b/APG4b_cu_26328758_WA.cpp
@@ -0,0 +1,16 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/APG4b/submissions/26328758
+ * Submitted at: 2021-10-03 11:29:18
+ * Problem URL: https://atcoder.jp/contests/APG4b/tasks/APG4b_cu
+ * Result: WA
+ * Execution Time: 7 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  cout << "いつも" << 2525 << endl;
+  cout << "AtCorderくん" << endl;
+}
