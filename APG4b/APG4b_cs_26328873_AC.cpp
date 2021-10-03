e2727214f8793c49c6d5b13f6b52aae4bbadf0bd
thirofoo <thirofoo@gmail.com>
2021-10-03 11:36:29 +0900

    [AC] SubmissionID: 26328873 at 2021-10-03 11:36:29 (1633228589)

---
+++ APG4b/APG4b_cs_26328873_AC.cpp
@@ -0,0 +1,20 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/APG4b/submissions/26328873
+ * Submitted at: 2021-10-03 11:36:29
+ * Problem URL: https://atcoder.jp/contests/APG4b/tasks/APG4b_cs
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int seconds = 365 * 24 * 60 * 60;
+
+  cout << seconds << endl;
+  cout << seconds*2 << endl;
+  cout << seconds*5 << endl;
+  cout <<seconds*10 << endl;
+}
