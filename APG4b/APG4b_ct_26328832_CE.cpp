0b987ef99103827e27eadc199cbf14f5366db6fe
thirofoo <thirofoo@gmail.com>
2021-10-03 11:34:11 +0900

    [CE] SubmissionID: 26328832 at 2021-10-03 11:34:11 (1633228451)

---
+++ APG4b/APG4b_ct_26328832_CE.cpp
@@ -0,0 +1,15 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/APG4b/submissions/26328832
+ * Submitted at: 2021-10-03 11:34:11
+ * Problem URL: https://atcoder.jp/contests/APG4b/tasks/APG4b_ct
+ * Result: CE
+ * Execution Time: None ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  cout << 100*(100+1) /　2 << endl;
+}
