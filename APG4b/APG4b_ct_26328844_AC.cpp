890f7a04f21d3ed5159840f87579fafdb2699010
thirofoo <thirofoo@gmail.com>
2021-10-03 11:34:48 +0900

    [AC] SubmissionID: 26328844 at 2021-10-03 11:34:48 (1633228488)

---
+++ APG4b/APG4b_ct_26328844_AC.cpp
@@ -0,0 +1,15 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/APG4b/submissions/26328844
+ * Submitted at: 2021-10-03 11:34:48
+ * Problem URL: https://atcoder.jp/contests/APG4b/tasks/APG4b_ct
+ * Result: AC
+ * Execution Time: 7 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  cout << 100*(100+1)/2 << endl;
+}
