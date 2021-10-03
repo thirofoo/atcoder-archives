8f503074f6834241ffcd16d4648ba9e0ed8a9d93
thirofoo <thirofoo@gmail.com>
2021-10-03 12:18:51 +0900

    [AC] SubmissionID: 26329693 at 2021-10-03 12:18:51 (1633231131)

---
+++ APG4b/APG4b_cm_26329693_AC.cpp
@@ -0,0 +1,32 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/APG4b/submissions/26329693
+ * Submitted at: 2021-10-03 12:18:51
+ * Problem URL: https://atcoder.jp/contests/APG4b/tasks/APG4b_cm
+ * Result: AC
+ * Execution Time: 7 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int A,B;
+  cin >> A >> B;
+  int i=1 , j=1;
+  
+  
+  cout << "A:";
+  while( i <= A ){
+    cout << "]";
+    i++;
+  }
+  cout << endl;
+  
+  cout << "B:";
+  while( j <= B ){
+    cout << "]";
+    j++;
+  }
+  cout << endl;
+}
