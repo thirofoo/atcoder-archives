a6a9d4f19681bb776a38e1a9fc4eaa0f4c7b0365
thirofoo <thirofoo@gmail.com>
2021-10-03 11:21:48 +0900

    [AC] SubmissionID: 26328569 at 2021-10-03 11:21:48 (1633227708)

---
+++ APG4b/APG4b_cv_26328569_AC.cpp
@@ -0,0 +1,16 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/APG4b/submissions/26328569
+ * Submitted at: 2021-10-03 11:21:48
+ * Problem URL: https://atcoder.jp/contests/APG4b/tasks/APG4b_cv
+ * Result: AC
+ * Execution Time: 10 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  cout << "こんにちは" << endl;
+  cout << "AtCoder" << endl;
+}
