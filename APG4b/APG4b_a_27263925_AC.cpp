cd008cc36737a3568f110a0717f625913ff71d8d
thirofoo <thirofoo@gmail.com>
2021-11-14 22:51:52 +0900

    [AC] SubmissionID: 27263925 at 2021-11-14 22:51:52 (1636897912)

---
+++ APG4b/APG4b_a_27263925_AC.cpp
@@ -0,0 +1,15 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/APG4b/submissions/27263925
+ * Submitted at: 2021-11-14 22:51:52
+ * Problem URL: https://atcoder.jp/contests/APG4b/tasks/APG4b_a
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  cout << "Hello, world!" << endl;
+}
