5cfc212ec40f3400af571142883f56ece4910906
thirofoo <thirofoo@gmail.com>
2021-11-14 14:28:57 +0900

    [AC] SubmissionID: 27250591 at 2021-11-14 14:28:57 (1636867737)

---
+++ abc088/abc088_b_27250591_AC.cpp
@@ -0,0 +1,37 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc088/submissions/27250591
+ * Submitted at: 2021-11-14 14:28:57
+ * Problem URL: https://atcoder.jp/contests/abc088/tasks/abc088_b
+ * Result: AC
+ * Execution Time: 7 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int N,alice=0,bob=0;
+  cin >> N;
+  vector<int> number(N);
+  for(int i=0;i<N;i++){
+  	cin >> number.at(i);
+  }
+  sort(number.begin(),number.end());
+  reverse(number.begin(),number.end());
+  if(N%2 == 0){
+  	for(int i=0;i<N/2;i++){
+      alice += number.at(2*i);
+      bob += number.at(2*i+1);
+    }
+    cout << alice - bob << endl;
+  }
+  else{
+    for(int i=0;i<(N-1)/2;i++){
+      alice += number.at(2*i);
+      bob += number.at(2*i+1);
+    }
+    alice += number.at(N-1);
+    cout << alice - bob << endl;
+  }
+}
