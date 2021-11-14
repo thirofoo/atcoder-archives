894a430bcf809183bdb7028c4a0f82c8e6258148
thirofoo <thirofoo@gmail.com>
2021-11-14 14:34:08 +0900

    [CE] SubmissionID: 27250671 at 2021-11-14 14:34:08 (1636868048)

---
+++ abc088/abc088_a_27250671_CE.cpp
@@ -0,0 +1,22 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc088/submissions/27250671
+ * Submitted at: 2021-11-14 14:34:08
+ * Problem URL: https://atcoder.jp/contests/abc088/tasks/abc088_a
+ * Result: CE
+ * Execution Time: None ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int price,tip;
+  cin >> price >> tip;
+  if(price%500 <= A){
+  	cout << "Yes" << endl;
+  }
+  else{
+  	cout << "No" << endl;
+  }
+}
