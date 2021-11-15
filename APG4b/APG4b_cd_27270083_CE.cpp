26990273114a77c3cce9387166c3718ee35b0fbe
thirofoo <thirofoo@gmail.com>
2021-11-15 13:04:46 +0900

    [CE] SubmissionID: 27270083 at 2021-11-15 13:04:46 (1636949086)

---
+++ APG4b/APG4b_cd_27270083_CE.cpp
@@ -0,0 +1,64 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/APG4b/submissions/27270083
+ * Submitted at: 2021-11-15 13:04:46
+ * Problem URL: https://atcoder.jp/contests/APG4b/tasks/APG4b_cd
+ * Result: CE
+ * Execution Time: None ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// 参照渡しを用いて、呼び出し側の変数の値を変更する
+void saiten(vector<vector<int>> &x(9,vector<int>(9),int &a,int &b) {
+  // 呼び出し側のAの各マスを正しい値に修正する
+  for(int i=0;i<9;i++){
+  	for(int j=0;j<9;j++){
+      if(x[i][j] == (i+1)*(j+1)){
+      	a += 1;
+      }
+      else{
+      	b += 1;
+        x[i][j] = (i+1)*(j+1);
+      }
+    }
+  }
+  // Aのうち、正しい値の書かれたマスの個数を correct_count に入れる
+  // Aのうち、誤った値の書かれたマスの個数を wrong_count に入れる
+ 
+  // ここにプログラムを追記
+}
+ 
+ 
+// -------------------
+// ここから先は変更しない
+// -------------------
+int main() {
+  // A君の回答を受け取る
+  vector<vector<int>> A(9, vector<int>(9));
+  for (int i = 0; i < 9; i++) {
+    for (int j = 0; j < 9; j++) {
+      cin >> A.at(i).at(j);
+    }
+  }
+ 
+  int correct_count = 0; // ここに正しい値のマスの個数を入れる
+  int wrong_count = 0;   // ここに誤った値のマスの個数を入れる
+ 
+  // A, correct_count, wrong_countを参照渡し
+  saiten(A, correct_count, wrong_count);
+ 
+  // 正しく修正した表を出力
+  for (int i = 0; i < 9; i++) {
+    for (int j = 0; j < 9; j++) {
+      cout << A.at(i).at(j);
+      if (j < 8) cout << " ";
+      else cout << endl;
+    }
+  }
+  // 正しいマスの個数を出力
+  cout << correct_count << endl;
+  // 誤っているマスの個数を出力
+  cout << wrong_count << endl;
+}
