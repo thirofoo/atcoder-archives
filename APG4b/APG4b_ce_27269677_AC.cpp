557b35427bd27d192e439ff128ca69025b21af89
thirofoo <thirofoo@gmail.com>
2021-11-15 12:12:30 +0900

    [AC] SubmissionID: 27269677 at 2021-11-15 12:12:30 (1636945950)

---
+++ APG4b/APG4b_ce_27269677_AC.cpp
@@ -0,0 +1,42 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/APG4b/submissions/27269677
+ * Submitted at: 2021-11-15 12:12:30
+ * Problem URL: https://atcoder.jp/contests/APG4b/tasks/APG4b_ce
+ * Result: AC
+ * Execution Time: 10 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  int N, M;
+  cin >> N >> M;
+  vector<int> A(M), B(M);
+  for (int i = 0; i < M; i++) {
+    cin >> A.at(i) >> B.at(i);
+  }
+ 
+  // N×Nのchar型の2次元配列のすべての要素を'-'で初期化
+  vector<vector<char>> table(N, vector<char>(N, '-'));
+ 
+  for (int i = 0; i < M; i++) {
+    // 1〜N → 0〜N-1 に変換
+    A.at(i)--; B.at(i)--;
+    table.at(A.at(i)).at(B.at(i)) = 'o';  // AはBに勝った
+    table.at(B.at(i)).at(A.at(i)) = 'x';  // BはAに負けた
+  }
+ 
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      cout << table.at(i).at(j);
+      if (j == N - 1) {
+        cout << endl;  // 行末なら改行
+      }
+      else {
+        cout << " ";  // 行末でないなら空白を出力
+      }
+    }
+  }
+}
