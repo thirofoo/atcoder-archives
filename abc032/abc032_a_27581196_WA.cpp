7a9220cdb10d50c14db3772873b5bdb622aee9a7
thirofoo <thirofoo@gmail.com>
2021-11-29 01:49:59 +0900

    [WA] SubmissionID: 27581196 at 2021-11-29 01:49:59 (1638118199)

---
+++ abc032/abc032_a_27581196_WA.cpp
@@ -0,0 +1,47 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc032/submissions/27581196
+ * Submitted at: 2021-11-29 01:49:59
+ * Problem URL: https://atcoder.jp/contests/abc032/tasks/abc032_a
+ * Result: WA
+ * Execution Time: 7 ms
+ */
+
+#include <bits/stdc++.h>
+#include <cmath> //abs関数で絶対値を使える。
+#define _GLIBCXX_DEBUG
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> P;
+typedef long double ld;
+// #define rep(i,n) for (ll i = 0; i < n; ++i)
+
+//ABC032 C問題　「列」　※尺取り法
+int main(){
+    int N,K;
+    int length = 0;
+    int right = 0;
+    int scale = 0;
+    ll multi = 1;
+    cin >> N >> K;
+    vector<int> Q(N);
+    for (ll i = 0; i < N; ++i){
+        cin >> Q[i];
+        if(Q[i] == 0)return cout << N << endl,0;
+    }
+    for(int left=0;left<N;left++){
+        for(right;right < N;right++){
+            if(multi*Q[right] <= K ){
+                multi*=Q[right];
+                scale++;
+                continue;
+            }
+            break;
+        }
+        length = max(length,scale);
+        scale--;
+        if(multi % Q[left] == 0)multi/=Q[left];
+    }
+    cout << length << endl;
+    return 0;
+}
