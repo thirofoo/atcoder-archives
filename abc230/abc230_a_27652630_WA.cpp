8fb30132f453b23b838e8aad76be2c683ef4552a
thirofoo <thirofoo@gmail.com>
2021-12-03 21:13:08 +0900

    [WA] SubmissionID: 27652630 at 2021-12-03 21:13:08 (1638533588)

---
+++ abc230/abc230_a_27652630_WA.cpp
@@ -0,0 +1,20 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc230/submissions/27652630
+ * Submitted at: 2021-12-03 21:13:08
+ * Problem URL: https://atcoder.jp/contests/abc230/tasks/abc230_a
+ * Result: WA
+ * Execution Time: 8 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int a;
+  cin >> a;
+  if(a<10){
+    cout << "AGC00" + a << endl;
+  }
+  else cout << "AGC0" + a << endl;
+}
