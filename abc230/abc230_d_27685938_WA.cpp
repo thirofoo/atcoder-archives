e762e52b821550dcedb370608a2c28046875df4c
thirofoo <thirofoo@gmail.com>
2021-12-04 16:17:23 +0900

    [WA] SubmissionID: 27685938 at 2021-12-04 16:17:23 (1638602243)

---
+++ abc230/abc230_d_27685938_WA.cpp
@@ -0,0 +1,41 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc230/submissions/27685938
+ * Submitted at: 2021-12-04 16:17:23
+ * Problem URL: https://atcoder.jp/contests/abc230/tasks/abc230_d
+ * Result: WA
+ * Execution Time: 129 ms
+ */
+
+#include <bits/stdc++.h>
+#include <cmath> //abs関数で絶対値を使える。
+#define _GLIBCXX_DEBUG
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> P;
+typedef long double ld;
+#define rep(i,n) for (ll i = 0; i < n; ++i)
+
+int main(){
+    int n,d;
+    int j = 1;
+    int ans = 0;
+    cin >> n >> d;
+    vector<pair<int,int>> wall;
+    rep(i,n){
+        int a,b;
+        cin >> a >> b;
+        wall.push_back(make_pair(a,b));
+    }
+    sort(wall.begin(),wall.end());
+    for(int i=0;i<n;i++){
+        for(j;j<n;j++){
+            if(wall[j].first <= wall[i].second+d-1)continue;
+            break;
+        }
+        i = j-1;
+        ans++;
+    }
+    cout << ans << endl;
+    return 0;
+}
