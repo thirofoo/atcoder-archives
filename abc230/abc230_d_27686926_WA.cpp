02ec0517f59ddef123d77a5e5616408a3982aab6
thirofoo <thirofoo@gmail.com>
2021-12-04 17:06:52 +0900

    [WA] SubmissionID: 27686926 at 2021-12-04 17:06:52 (1638605212)

---
+++ abc230/abc230_d_27686926_WA.cpp
@@ -0,0 +1,39 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc230/submissions/27686926
+ * Submitted at: 2021-12-04 17:06:52
+ * Problem URL: https://atcoder.jp/contests/abc230/tasks/abc230_d
+ * Result: WA
+ * Execution Time: 131 ms
+ */
+
+#include <bits/stdc++.h>
+#include <cmath>
+#define _GLIBCXX_DEBUG
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> P;
+typedef long double ld;
+#define rep(i,n) for (ll i = 0; i < n; ++i)
+
+int main(){
+    int n,d;
+    int j = 1;
+    int ans = 0;
+    cin >> n >> d;
+    vector<pair<int,int>> wall;
+    rep(i,n){
+        int a,b;
+        cin >> a >> b;
+        wall.push_back(make_pair(a,b));
+    }
+    sort(wall.begin(),wall.end());
+    int i=0;
+    while(i<n){
+        while (j < n && wall[j].first - wall[i].second < d) ++j;
+        i = j;
+        ans++;
+    }
+    cout << ans << endl;
+    return 0;
+}
