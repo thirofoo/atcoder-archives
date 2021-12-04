bab87106fce8c6deffee945c22eafeb8792be7cd
thirofoo <thirofoo@gmail.com>
2021-12-04 18:15:38 +0900

    [AC] SubmissionID: 27688182 at 2021-12-04 18:15:38 (1638609338)

---
+++ abc210/abc210_b_27688182_AC.cpp
@@ -0,0 +1,29 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc210/submissions/27688182
+ * Submitted at: 2021-12-04 18:15:38
+ * Problem URL: https://atcoder.jp/contests/abc210/tasks/abc210_b
+ * Result: AC
+ * Execution Time: 10 ms
+ */
+
+#include <bits/stdc++.h>
+#include <cmath> //abs関数で絶対値を使える。
+#define _GLIBCXX_DEBUG
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> P;
+typedef long double ld;
+#define rep(i,n) for (ll i = 0; i < n; ++i)
+
+int main(){
+    int n;
+    int i = 0;
+    string s;
+    cin >> n;
+    cin >> s;
+    while(s[i] == '0')i++;
+    if(i%2 == 0)cout << "Takahashi" << endl;
+    else cout << "Aoki" << endl;
+    return 0;
+}
