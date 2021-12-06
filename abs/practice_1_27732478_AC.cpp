734aa1336496c2d803a478faae283a03cf94e7ff
thirofoo <thirofoo@gmail.com>
2021-12-06 13:06:42 +0900

    [AC] SubmissionID: 27732478 at 2021-12-06 13:06:42 (1638763602)

---
+++ abs/practice_1_27732478_AC.cpp
@@ -0,0 +1,26 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abs/submissions/27732478
+ * Submitted at: 2021-12-06 13:06:42
+ * Problem URL: https://atcoder.jp/contests/abs/tasks/practice_1
+ * Result: AC
+ * Execution Time: 7 ms
+ */
+
+#include <bits/stdc++.h>
+#include <cmath>
+#define _GLIBCXX_DEBUG
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> P;
+typedef long double ld;
+#define rep(i,n) for (ll i = 0; i < n; ++i)
+
+int main(){
+    int a,b,c;
+    string s;
+    cin >> a >> b >> c >> s;
+    cout << a+b+c;
+    cout << " " + s << endl;
+    return 0;
+}
