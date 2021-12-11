a4496bdb14f91130a03f647e28731e57b115b349
thirofoo <thirofoo@gmail.com>
2021-12-11 21:24:08 +0900

    [WA] SubmissionID: 27831854 at 2021-12-11 21:24:08 (1639225448)

---
+++ abc231/abc231_a_27831854_WA.cpp
@@ -0,0 +1,22 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc231/submissions/27831854
+ * Submitted at: 2021-12-11 21:24:08
+ * Problem URL: https://atcoder.jp/contests/abc231/tasks/abc231_a
+ * Result: WA
+ * Execution Time: 8 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int, int> P;
+typedef long long ll;
+typedef long double ld;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    int d;
+    cin >> d;
+    cout << d/100 << endl;
+    return 0;
+}
