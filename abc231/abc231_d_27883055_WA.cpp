cb2fc4d807becacd7a803e92eab81ad67b5474df
thirofoo <thirofoo@gmail.com>
2021-12-12 21:34:36 +0900

    [WA] SubmissionID: 27883055 at 2021-12-12 21:34:36 (1639312476)

---
+++ abc231/abc231_d_27883055_WA.cpp
@@ -0,0 +1,36 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc231/submissions/27883055
+ * Submitted at: 2021-12-12 21:34:36
+ * Problem URL: https://atcoder.jp/contests/abc231/tasks/abc231_d
+ * Result: WA
+ * Execution Time: 156 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int, int> P;
+typedef long long ll;
+typedef long double ld;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    int n,m;
+    cin >> n >> m;
+    vector<P> condition(0);
+    map<int,int> count;
+    rep(i,n+1)count[i] = 0;
+    rep(i,m){
+        int a,b;
+        cin >> a >> b;
+        condition.push_back(make_pair(a,b));
+    }
+    rep(i,m){
+        int a,b;
+        tie(a,b) = condition[i];
+        count[a]++;count[b]++;
+    }
+    rep(i,n+1)if(count[i] >= 3)return cout << "No" << endl,0;
+    cout << "Yes" << endl;
+    return 0;
+}
