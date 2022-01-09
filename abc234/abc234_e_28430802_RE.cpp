5a469975e75eb7f2a07e44977a3b8cec2f85cc64
thirofoo <thirofoo@gmail.com>
2022-01-09 16:07:59 +0900

    [RE] SubmissionID: 28430802 at 2022-01-09 16:07:59 (1641712079)

---
+++ abc234/abc234_e_28430802_RE.cpp
@@ -0,0 +1,50 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc234/submissions/28430802
+ * Submitted at: 2022-01-09 16:07:59
+ * Problem URL: https://atcoder.jp/contests/abc234/tasks/abc234_e
+ * Result: RE
+ * Execution Time: 111 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+typedef pair<int, int> P;
+typedef long long ll;
+typedef long double ld;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    ll ans = 11111111111,a = -4;
+    string x; cin >> x;
+    if(x.size() <= 2)cout << x << endl;
+    else if(x.size() > 10){
+        if(x[0] >= x[1])rep(i,x.size())cout << x[0];
+        else rep(i,x.size())cout << x[0]+1;
+        cout << endl;
+    }
+    else{
+        while(true){
+            bool frag1 = true,frag2 = true;
+            string cand1 = "",cand2 = "";
+            cand1.push_back(x[0]);
+            cand2.push_back(x[0]+1);
+            rep(i,x.size()-1){
+                cand1.push_back(cand1[i] + a);
+                cand2.push_back(cand2[i] + a);
+                if(cand1[i+1] > '9' || cand1[i+1] < '0'){frag1 = false;}
+                if(cand2[i+1] > '9' || cand2[i+1] < '0'){frag2 = false;}
+            }
+            if(frag1 && cand1 >= x)ans = min(ans,stoll(cand1));
+            if(frag2 && cand2 >= x)ans = min(ans,stoll(cand2));
+            a++;
+            if(a == 5)break;
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
