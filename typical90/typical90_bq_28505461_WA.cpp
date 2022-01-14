b3a09ed55e1f035d846c819a2afd7c2ebaf716d0
thirofoo <thirofoo@gmail.com>
2022-01-14 18:29:53 +0900

    [WA] SubmissionID: 28505461 at 2022-01-14 18:29:53 (1642152593)

---
+++ typical90/typical90_bq_28505461_WA.cpp
@@ -0,0 +1,39 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/typical90/submissions/28505461
+ * Submitted at: 2022-01-14 18:29:53
+ * Problem URL: https://atcoder.jp/contests/typical90/tasks/typical90_bq
+ * Result: WA
+ * Execution Time: 8 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+typedef pair<int, int> P;
+typedef long long ll;
+typedef long double ld;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int s = 1000000007;
+
+int main() {
+    ll n,k,count = 1; cin >> n >> k;
+    vector<ll> mod(64);
+    mod[0] = k-2;
+    for(int i=0;i<63;i++){
+        ll tmp = mod[i];
+        mod[i+1] = (tmp*tmp)%s;
+        cout << tmp << " ";
+    }
+    cout << endl;
+    ll ans = k*(k-1)%s;
+    rep(i,64){
+        if((n-2)&(1LL << i)) (ans *= mod[i]) %= s;
+    }
+    cout << ans << endl;
+    return 0;
+}
