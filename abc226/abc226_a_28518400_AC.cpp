f318635226be78ce723518f0a484eb4876db7f8b
thirofoo <thirofoo@gmail.com>
2022-01-15 14:50:13 +0900

    [AC] SubmissionID: 28518400 at 2022-01-15 14:50:13 (1642225813)

---
+++ abc226/abc226_a_28518400_AC.cpp
@@ -0,0 +1,25 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc226/submissions/28518400
+ * Submitted at: 2022-01-15 14:50:13
+ * Problem URL: https://atcoder.jp/contests/abc226/tasks/abc226_a
+ * Result: AC
+ * Execution Time: 11 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+typedef pair<int, int> P;
+typedef long long ll;
+typedef long double ld;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    double x; cin >> x;
+    cout <<(int)(x+0.5) << endl;
+    return 0;
+}
