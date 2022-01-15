5627e90c0f99a5562e3d668da8301c934c925c68
thirofoo <thirofoo@gmail.com>
2022-01-15 16:39:59 +0900

    [CE] SubmissionID: 28520585 at 2022-01-15 16:39:59 (1642232399)

---
+++ abc218/abc218_a_28520585_CE.cpp
@@ -0,0 +1,26 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc218/submissions/28520585
+ * Submitted at: 2022-01-15 16:39:59
+ * Problem URL: https://atcoder.jp/contests/abc218/tasks/abc218_a
+ * Result: CE
+ * Execution Time: None ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+typedef pair<int, int> P;
+typedef long long ll;
+typedef long double ld;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    int n;string s; cin >> n >> s;
+    if(s[n-1] = 'o')xout << "Yes" << endl;
+    else cout << "No" << endl;
+    return 0;
+}  
