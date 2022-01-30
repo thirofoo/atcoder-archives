b0ef32f6ec06bf8b2d4f3255f466b429ec364281
thirofoo <thirofoo@gmail.com>
2022-01-30 21:09:54 +0900

    [WA] SubmissionID: 28908747 at 2022-01-30 21:09:54 (1643544594)

---
+++ abc237/abc237_b_28908747_WA.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc237/submissions/28908747
+ * Submitted at: 2022-01-30 21:09:54
+ * Problem URL: https://atcoder.jp/contests/abc237/tasks/abc237_b
+ * Result: WA
+ * Execution Time: 2 ms
+ */
+
+// code not found
