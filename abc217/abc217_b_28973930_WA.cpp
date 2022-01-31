b829d95a3566af95a04e99fda6d0903390dbd391
thirofoo <thirofoo@gmail.com>
2022-01-31 18:55:31 +0900

    [WA] SubmissionID: 28973930 at 2022-01-31 18:55:31 (1643622931)

---
+++ abc217/abc217_b_28973930_WA.cpp
@@ -0,0 +1,32 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc217/submissions/28973930
+ * Submitted at: 2022-01-31 18:55:31
+ * Problem URL: https://atcoder.jp/contests/abc217/tasks/abc217_b
+ * Result: WA
+ * Execution Time: 7 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+typedef pair<int, int> P;
+typedef long long ll;
+typedef long double ld;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    vector<string> str = {"ABC","ARC","AGC","AHC"};
+    vector<int> ans(4,0);
+    rep(i,3){
+        string tmp; cin >> tmp;
+        rep(j,4){
+            if(tmp == str[j])ans[i]++;
+        }
+    }
+    rep(i,4)if(!ans[i])cout << str[i] << endl;;
+    return 0;
+}
