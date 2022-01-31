f37215487cd75f3a42ac68f2968253bc553728a8
thirofoo <thirofoo@gmail.com>
2022-01-31 20:23:24 +0900

    [AC] SubmissionID: 28975248 at 2022-01-31 20:23:24 (1643628204)

---
+++ abc227/abc227_c_28975248_AC.cpp
@@ -0,0 +1,31 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc227/submissions/28975248
+ * Submitted at: 2022-01-31 20:23:24
+ * Problem URL: https://atcoder.jp/contests/abc227/tasks/abc227_c
+ * Result: AC
+ * Execution Time: 257 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+typedef pair<int, int> P;
+typedef long long ll;
+typedef long double ld;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    cout << setprecision(10);
+    ll n,ans = 0; cin >> n;
+    for(ll a=1;a*a*a<=n;a++){
+        for(ll b=a;a*b*b<=n;b++){
+            ans += n/a/b-b+1;
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
