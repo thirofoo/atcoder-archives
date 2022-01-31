7183df57a896f42c2632049707f872f4530b48bd
thirofoo <thirofoo@gmail.com>
2022-01-31 18:48:32 +0900

    [AC] SubmissionID: 28973793 at 2022-01-31 18:48:32 (1643622512)

---
+++ abc221/abc221_b_28973793_AC.cpp
@@ -0,0 +1,37 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc221/submissions/28973793
+ * Submitted at: 2022-01-31 18:48:32
+ * Problem URL: https://atcoder.jp/contests/abc221/tasks/abc221_b
+ * Result: AC
+ * Execution Time: 7 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+typedef pair<int, int> P;
+typedef long long ll;
+typedef long double ld;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    string s,t; cin >> s >> t;
+    int count = 0;
+    vector<char> a,b;
+    rep(i,s.size()){
+        a.push_back(s[i]); b.push_back(t[i]);
+        if(s[i] != t[i]){
+            if(s[i] != t[i+1] || s[i+1] != t[i])count++;
+        }
+        if(count == 2)return cout << "No" << endl,0;
+    }
+    sort(a.begin(),a.end());
+    sort(b.begin(),b.end());
+    if(a == b)cout << "Yes" << endl;
+    else cout << "No" << endl;
+    return 0;
+}
