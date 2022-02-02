b2891e15e83071df87073cfde0ba8a8511f958e6
thirofoo <thirofoo@gmail.com>
2022-02-02 12:00:06 +0900

    [AC] SubmissionID: 29000859 at 2022-02-02 12:00:06 (1643770806)

---
+++ abc215/abc215_a_29000859_AC.cpp
@@ -0,0 +1,26 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc215/submissions/29000859
+ * Submitted at: 2022-02-02 12:00:06
+ * Problem URL: https://atcoder.jp/contests/abc215/tasks/abc215_a
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+typedef pair<int, int> P;
+typedef long long ll;
+typedef long double ld;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    string s; cin >> s;
+    if(s == "Hello,World!")cout << "AC" << endl;
+    else cout << "WA" << endl;
+    return 0;
+}
