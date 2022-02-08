d7760a9aaf87367585cae8f2f095f910e2260042
thirofoo <thirofoo@gmail.com>
2022-02-08 23:08:12 +0900

    [WA] SubmissionID: 29167950 at 2022-02-08 23:08:12 (1644329292)

---
+++ abc212/abc212_d_29167950_WA.cpp
@@ -0,0 +1,43 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc212/submissions/29167950
+ * Submitted at: 2022-02-08 23:08:12
+ * Problem URL: https://atcoder.jp/contests/abc212/tasks/abc212_d
+ * Result: WA
+ * Execution Time: 208 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+typedef long long ll;
+typedef pair<ll, int> P;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+ 
+int main() {
+    int q; cin >> q;
+    ll cnt = 0,add;
+    priority_queue<P,vector<P>,greater<P>> ball;
+    vector<ll> inc; inc.push_back(0);
+    rep(i,q){
+        int p; cin >> p;
+        if(p == 1){
+            ll x; cin >> x;
+            ball.push(P(x,cnt));
+        }
+        else if(p == 2){
+            ll x; cin >> x;
+            inc.push_back(x+*(inc.end()-1));
+            cnt++;
+        }
+        else{
+            auto [x,y] = ball.top(); ball.pop();
+            add = inc[cnt]-inc[y];
+            cout << x + add << endl;
+        }
+    }
+    return 0;
+}
