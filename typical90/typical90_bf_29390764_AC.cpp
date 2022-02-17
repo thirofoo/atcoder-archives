4a7ff6c8c034e15f66aa31aa5b9880788903a33d
thirofoo <thirofoo@gmail.com>
2022-02-17 22:59:59 +0900

    [AC] SubmissionID: 29390764 at 2022-02-17 22:59:59 (1645106399)

---
+++ typical90/typical90_bf_29390764_AC.cpp
@@ -0,0 +1,38 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/typical90/submissions/29390764
+ * Submitted at: 2022-02-17 22:59:59
+ * Problem URL: https://atcoder.jp/contests/typical90/tasks/typical90_bf
+ * Result: AC
+ * Execution Time: 7 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+typedef long long ll;
+typedef pair<int, int> P;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    ll n,k; cin >> n >> k;
+    if(n == 0)return cout << 0 << endl,0;
+    vector<int> num(100001,0);
+    int a = n,ans = 0;
+    while(!num[a]){
+        ans++; num[a] = ans;
+        int b = a,cnt = 0;
+        while(b != 0){
+            cnt += b%10; b /= 10;
+        }
+        cnt += b;
+        a = (a+cnt)%100000;
+    }
+    int T = ans-num[a]+1;
+    if(k <= num[a])cout << find(num.begin(),num.end(),k+1)-num.begin() << endl; 
+    else cout << find(num.begin(),num.end(),((k-num[a]+1)%T)+num[a])-num.begin() << endl; 
+    return 0;
+}
