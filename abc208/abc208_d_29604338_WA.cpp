c1d60a5246c5390f7b2a5605730f3f8efa0b8be2
thirofoo <thirofoo@gmail.com>
2022-02-23 18:35:04 +0900

    [WA] SubmissionID: 29604338 at 2022-02-23 18:35:04 (1645608904)

---
+++ abc208/abc208_d_29604338_WA.cpp
@@ -0,0 +1,38 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc208/submissions/29604338
+ * Submitted at: 2022-02-23 18:35:04
+ * Problem URL: https://atcoder.jp/contests/abc208/tasks/abc208_d
+ * Result: WA
+ * Execution Time: 511 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+typedef long long ll;
+typedef pair<int, int> P;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    int n,m; cin >> n >> m;
+    vector<vector<vector<ll>>> dp(n+1,vector<vector<ll>>(n+1,vector<ll>(n,1e9)));
+    rep(i,m){
+        ll a,b,c; cin >> a >> b >> c;
+        dp[0][a-1][b-1] = c;
+    }
+    rep(i,n)dp[0][i][i] = 0;
+    //ワーシャルフロイド
+    rep(k,n)rep(i,n)rep(j,n){
+        dp[k+1][i][j] = min(dp[k][i][j],dp[k][i][k]+dp[k][k][j]);
+    }
+    int ans = 0;
+    for(ll k=1;k<=n;k++)rep(i,n)rep(j,n){
+        if(dp[k][i][j] != INT_MAX)ans += dp[k][i][j];
+    }
+    cout << ans << endl;
+    return 0;
+}
