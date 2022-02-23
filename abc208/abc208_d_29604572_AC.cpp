cda87c35414020f1d4ceffb80444333094ad53a4
thirofoo <thirofoo@gmail.com>
2022-02-23 18:47:20 +0900

    [AC] SubmissionID: 29604572 at 2022-02-23 18:47:20 (1645609640)

---
+++ abc208/abc208_d_29604572_AC.cpp
@@ -0,0 +1,38 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc208/submissions/29604572
+ * Submitted at: 2022-02-23 18:47:20
+ * Problem URL: https://atcoder.jp/contests/abc208/tasks/abc208_d
+ * Result: AC
+ * Execution Time: 161 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+typedef long long ll;
+typedef pair<int, int> P;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    int n,m; cin >> n >> m;
+    vector<vector<ll>> dp(n+1,vector<ll>(n+1,1e12));
+    rep(i,m){
+        ll a,b,c; cin >> a >> b >> c;
+        dp[a-1][b-1] = c;
+    }
+    rep(i,n)dp[i][i] = 0;
+    //ワーシャルフロイド
+    ll ans = 0;
+    rep(k,n){
+        rep(i,n)rep(j,n){
+            dp[i][j] = min(dp[i][j],dp[i][k]+dp[k][j]);
+            if(dp[i][j] != 1e12)ans += dp[i][j];
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
