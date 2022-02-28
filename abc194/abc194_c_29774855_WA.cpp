733f335896f2553b725e74687b9cd524382ac4fb
thirofoo <thirofoo@gmail.com>
2022-02-28 21:41:31 +0900

    [WA] SubmissionID: 29774855 at 2022-02-28 21:41:31 (1646052091)

---
+++ abc194/abc194_c_29774855_WA.cpp
@@ -0,0 +1,40 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc194/submissions/29774855
+ * Submitted at: 2022-02-28 21:41:31
+ * Problem URL: https://atcoder.jp/contests/abc194/tasks/abc194_c
+ * Result: WA
+ * Execution Time: 62 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+typedef long long ll;
+typedef pair<int, int> P;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    int n; cin >> n;
+    ll ans = 0;
+    vector<int> a(n),a1(n),a2(n);
+    rep(i,n){
+        cin >> a[i];
+        if(i == 0){
+            a1[i] = a[i];
+            a2[i] = a[i]*a[i];
+        }
+        else{
+            a1[i] = a[i]+a1[i-1];
+            a2[i] = a[i]*a[i]+a2[i-1];
+        }
+    }
+    for(int i=1;i<n;i++){
+        ans += i*pow(a[i],2)-2*a1[i-1]*a[i]+a2[i-1];
+    }
+    cout << ans << endl;
+    return 0;
+}
