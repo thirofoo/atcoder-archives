0f91186ffb1fa1a26a192712bd419ba755b5e457
thirofoo <thirofoo@gmail.com>
2022-03-01 15:41:45 +0900

    [TLE] SubmissionID: 29786163 at 2022-03-01 15:41:45 (1646116905)

---
+++ abc216/abc216_e_29786163_TLE.cpp
@@ -0,0 +1,49 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc216/submissions/29786163
+ * Submitted at: 2022-03-01 15:41:45
+ * Problem URL: https://atcoder.jp/contests/abc216/tasks/abc216_e
+ * Result: TLE
+ * Execution Time: 2332 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+typedef long long ll;
+typedef pair<int, int> P;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    ll n,k; cin >> n >> k;
+    vector<ll> a(n);
+    rep(i,n)cin >> a[i];
+    sort(a.begin(),a.end(),[](ll x,ll y){
+        return x > y;
+    });
+    ll left = 0,right = a[0];
+    while(right-left > 1){
+        ll mid = (left+right)/2;
+
+        ll ride_cnt = 0; 
+        rep(i,n){
+            if(mid > a[i])break;
+            ride_cnt += a[i]-mid+1;
+        }
+        if(ride_cnt > k)left = mid;
+        else right = mid;
+    }
+    ll ans = 0;
+    rep(i,n){
+        if(right > a[i])break;
+        ans += (a[i]-right+1)*(right+a[i])/2;
+        k -= a[i]-right+1;
+    }
+    ans += k*left;
+    cout << ans << endl;
+    if(k < 0)rep(i,1e9)cout << "A" << '\n';
+    return 0;
+}
