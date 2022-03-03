d752364c0156c70ff3fcf78722c97c07eda7083e
thirofoo <thirofoo@gmail.com>
2022-03-03 15:42:51 +0900

    [RE] SubmissionID: 29821233 at 2022-03-03 15:42:51 (1646289771)

---
+++ abc206/abc206_d_29821233_RE.cpp
@@ -0,0 +1,33 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc206/submissions/29821233
+ * Submitted at: 2022-03-03 15:42:51
+ * Problem URL: https://atcoder.jp/contests/abc206/tasks/abc206_d
+ * Result: RE
+ * Execution Time: 148 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+typedef long long ll;
+typedef pair<int, int> P;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    int n,ans = 0; cin >> n;
+    vector<int> a(n);
+    rep(i,n)cin >> a[i];
+    dsu d(n);
+    rep(i,n/2){
+        if(!d.same(a[i],a[n-1-i])){
+            ans++;
+            d.merge(a[i],a[n-1-i]);
+        }
+    }
+    cout << ans << '\n';
+    return 0;
+}
