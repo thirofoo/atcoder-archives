b6d3d35d6a712965323153314b566239dc47dfab
thirofoo <thirofoo@gmail.com>
2022-03-09 16:43:18 +0900

    [WA] SubmissionID: 29968819 at 2022-03-09 16:43:18 (1646811798)

---
+++ abc201/abc201_c_29968819_WA.cpp
@@ -0,0 +1,57 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc201/submissions/29968819
+ * Submitted at: 2022-03-09 16:43:18
+ * Problem URL: https://atcoder.jp/contests/abc201/tasks/abc201_c
+ * Result: WA
+ * Execution Time: 4 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+typedef long long ll;
+typedef pair<int, int> P;
+typedef tuple<int, int, int> T;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    string s; cin >> s;
+    int a = 0,b = 0,c = 0;
+    set<int> v;
+    rep(i,10){
+        if(s[i] == 'o'){
+            a++;
+            v.insert(i);
+        }
+        else if(s[i] == 'x')b++;
+        else c++;
+    }
+    if(a >= 5)return cout << 0 << endl,0;
+    if(a == 4)return cout << 1 << endl,0;
+    int ans = 0;
+    rep(i,10){
+        if(s[i] == 'x')continue;
+        rep(j,10){
+            if(s[j] == 'x')continue;
+            rep(k,10){
+                if(s[k] == 'x')continue;
+                rep(l,10){
+                    if(s[l] == 'x')continue;
+                    set<int> w;
+                    if(s[i] == 'o')w.insert(i);
+                    if(s[j] == 'o')w.insert(j);
+                    if(s[k] == 'o')w.insert(k);
+                    if(s[l] == 'o')w.insert(l);
+                    if(v == w)ans++;
+                }
+            }
+        }
+    }
+
+    cout << ans << endl;
+    return 0;
+}
