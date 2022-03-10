933fbf599d97ec39d9d4e4df31cd9a8186770a50
thirofoo <thirofoo@gmail.com>
2022-03-10 19:32:15 +0900

    [WA] SubmissionID: 29988315 at 2022-03-10 19:32:15 (1646908335)

---
+++ abc200/abc200_d_29988315_WA.cpp
@@ -0,0 +1,79 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc200/submissions/29988315
+ * Submitted at: 2022-03-10 19:32:15
+ * Problem URL: https://atcoder.jp/contests/abc200/tasks/abc200_d
+ * Result: WA
+ * Execution Time: 8 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+typedef long long ll;
+typedef pair<int, int> P;
+typedef tuple<int, int, int> T;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    int n; cin >> n;
+    vector<int> a(n);
+    rep(i,n)cin >> a[i];
+    vector<vector<vector<int>>> ans(200);
+    if(n < 9){
+        for(int i=0;i<(1 << n);i++){
+            vector<int> tmp;
+            int cnt = 0;
+            rep(j,n){
+                if(i & (1 << j)){
+                    tmp.push_back(j+1);
+                    cnt += a[j];
+                    cnt %= 200;
+                }
+            }
+            ans[cnt].push_back(tmp);
+            if(ans[cnt].size() >= 2){
+                cout << "Yes" << '\n';
+                for(auto k:ans[cnt]){
+                    cout << k.size() << " ";
+                    for(auto l:k){
+                        cout << l << " ";
+                    }
+                    cout << '\n';
+                }
+                return 0;
+            }
+        }
+        cout << "No" << '\n';
+    }
+    else{
+        for(int i=0;i<(1 << 9);i++){
+            vector<int> tmp;
+            int cnt = 0;
+            rep(j,9){
+                if(i & (1 << j)){
+                    tmp.push_back(j+1);
+                    cnt += a[j];
+                    cnt %= 200;
+                }
+            }
+            ans[cnt].push_back(tmp);
+            if(ans[cnt].size() >= 2){
+                cout << "Yes" << '\n';
+                for(auto k:ans[cnt]){
+                    cout << k.size() << " ";
+                    for(auto l:k){
+                        cout << l << " ";
+                    }
+                    cout << '\n';
+                }
+                return 0;
+            }
+        }
+        cout << "No" << '\n';
+    }
+    return 0;
+}
