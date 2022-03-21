7d7c15bd13ff91dc01ce50eca8fd1c28b28490c8
thirofoo <thirofoo@gmail.com>
2022-03-21 16:54:06 +0900

    [AC] SubmissionID: 30324378 at 2022-03-21 16:54:06 (1647849246)

---
+++ typical90/typical90_j_30324378_AC.cpp
@@ -0,0 +1,40 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/typical90/submissions/30324378
+ * Submitted at: 2022-03-21 16:54:06
+ * Problem URL: https://atcoder.jp/contests/typical90/tasks/typical90_j
+ * Result: AC
+ * Execution Time: 224 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+typedef long long ll;
+typedef pair<int, int> P;
+typedef tuple<int, int, int> T;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    int n; cin >> n;
+    vector<int> a1,a2;
+    a1.push_back(0);
+    a2.push_back(0);
+    rep(i,n){
+        int c,p; cin >> c >> p;
+        int tmp1 = (c == 1 ? a1.back()+p : a1.back());
+        int tmp2 = (c == 2 ? a2.back()+p : a2.back());
+        a1.push_back(tmp1);
+        a2.push_back(tmp2);
+    }
+
+    int q; cin >> q;
+    rep(i,q){
+        int l,r; cin >> l >> r;
+        cout << a1[r]-a1[l-1] << " " << a2[r]-a2[l-1] << endl;
+    }
+    return 0;
+}
