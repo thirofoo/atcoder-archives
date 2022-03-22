f83b09c04e7581d86ac1fbcc0c788fa9a6145580
thirofoo <thirofoo@gmail.com>
2022-03-22 18:02:59 +0900

    [AC] SubmissionID: 30346575 at 2022-03-22 18:02:59 (1647939779)

---
+++ typical90/typical90_p_30346575_AC.cpp
@@ -0,0 +1,39 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/typical90/submissions/30346575
+ * Submitted at: 2022-03-22 18:02:59
+ * Problem URL: https://atcoder.jp/contests/typical90/tasks/typical90_p
+ * Result: AC
+ * Execution Time: 414 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+typedef long long ll;
+typedef pair<int, int> P;
+typedef tuple<int, int, int> T;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    int n,a,b,c; cin >> n >> a >> b >> c;
+    priority_queue<int> todo;
+    todo.push(a); todo.push(b); todo.push(c);
+    a = todo.top(); todo.pop();
+    b = todo.top(); todo.pop();
+    c = todo.top(); todo.pop();
+
+    ll ans = LLONG_MAX;
+    for(ll i=9999;i>=0;i--){
+        for(ll j=9999-i;j>=0;j--){
+            if(n-a*i-b*j >= 0 && (n-a*i-b*j)%c == 0){
+                ans = min(ans,i+j+(n-a*i-b*j)/c);
+            }
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
