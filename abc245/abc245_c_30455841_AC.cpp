90df457abd954ec83f852e534230fee69cb9488c
thirofoo <thirofoo@gmail.com>
2022-03-26 21:36:42 +0900

    [AC] SubmissionID: 30455841 at 2022-03-26 21:36:42 (1648298202)

---
+++ abc245/abc245_c_30455841_AC.cpp
@@ -0,0 +1,43 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc245/submissions/30455841
+ * Submitted at: 2022-03-26 21:36:42
+ * Problem URL: https://atcoder.jp/contests/abc245/tasks/abc245_c
+ * Result: AC
+ * Execution Time: 120 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+typedef long long ll;
+typedef pair<int, int> P;
+typedef tuple<int, int, int> T;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    ll n,k; cin >> n >> k;
+    vector<ll> a(n),b(n);
+    rep(i,n)cin >> a[i];
+    rep(i,n)cin >> b[i];
+
+    vector<int> frag_a(n,false),frag_b(n,false);
+    frag_a[0] = true;
+    frag_b[0] = true;
+
+    ll s = a[0];
+    for(int i=1;i<n;i++){
+        bool frag = false;
+        if(a[i-1]-k <= a[i] && a[i] <= a[i-1]+k && frag_a[i-1])frag = true,frag_a[i] = true;
+        if(b[i-1]-k <= a[i] && a[i] <= b[i-1]+k && frag_b[i-1])frag = true,frag_a[i] = true;
+        if(a[i-1]-k <= b[i] && b[i] <= a[i-1]+k && frag_a[i-1])frag = true,frag_b[i] = true;
+        if(b[i-1]-k <= b[i] && b[i] <= b[i-1]+k && frag_b[i-1])frag = true,frag_b[i] = true;
+        if(!frag)return cout << "No" << endl,0;
+    }
+    cout << "Yes" << endl;
+
+    return 0;
+}
