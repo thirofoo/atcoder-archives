096d082fc4460eac474e231d38d9424047612f0e
thirofoo <thirofoo@gmail.com>
2022-03-26 17:26:11 +0900

    [AC] SubmissionID: 30418374 at 2022-03-26 17:26:11 (1648283171)

---
+++ ahc009/ahc009_a_30418374_AC.cpp
@@ -0,0 +1,67 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/ahc009/submissions/30418374
+ * Submitted at: 2022-03-26 17:26:11
+ * Problem URL: https://atcoder.jp/contests/ahc009/tasks/ahc009_a
+ * Result: AC
+ * Execution Time: 12 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+typedef long long ll;
+typedef pair<int, int> P;
+typedef tuple<int, int, int> T;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+ 
+vector<int> vx = {-1,0,1,0};
+vector<int> vy = {0,1,0,-1};
+ 
+unsigned int randInt() {
+    static unsigned int tx = 123456789, ty=362436069, tz=521288629, tw=88675123;
+    unsigned int tt = (tx^(tx<<11));
+    tx = ty; ty = tz; tz = tw;
+    return ( tw=(tw^(tw>>19))^(tt^(tt>>8)) );
+}
+ 
+int main() {
+    int sx,sy,tx,ty; double p; cin >> sx >> sy >> tx >> ty >> p;
+    vector<string> r_wall(20),d_wall(19);
+    rep(i,20)cin >> r_wall[i];
+    rep(i,19)cin >> d_wall[i];
+
+    rep(i,20)r_wall[i] += "1";
+    d_wall.push_back("11111111111111111111");
+    int rand = (int)((double)1/p);
+
+    rep(i,20){
+        rep(j,20){
+            if(i == 0 || j == 0)continue;
+            if(r_wall[i][j] && d_wall[i][j]){
+                d_wall[i-1][j] = '1';
+            }
+        }
+    }
+ 
+    int cnt = 0,nx = sx,ny = sy,time = 10;
+    string ans = "";
+    while(ans.size() < 200){
+
+        if(randInt()%rand)ans += "R";
+        if(randInt()%rand)ans += "D";
+        if(ans.size()%3 == 0){
+            if(randInt()%rand)ans += "U";
+            if(randInt()%rand)ans += "L";
+        }
+
+        // if(randInt()%rand)ans += "U";
+        // if(randInt()%rand)ans += "L";
+    }
+    ans.resize(200);
+    cout << ans << endl;
+    return 0;
+}
