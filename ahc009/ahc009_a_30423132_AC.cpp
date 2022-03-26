db9beeed5064d63316c7e951e5f4b5be3686269d
thirofoo <thirofoo@gmail.com>
2022-03-26 18:52:45 +0900

    [AC] SubmissionID: 30423132 at 2022-03-26 18:52:45 (1648288365)

---
+++ ahc009/ahc009_a_30423132_AC.cpp
@@ -0,0 +1,47 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/ahc009/submissions/30423132
+ * Submitted at: 2022-03-26 18:52:45
+ * Problem URL: https://atcoder.jp/contests/ahc009/tasks/ahc009_a
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+ 
+unsigned int randInt() {
+    static unsigned int tx = 123456789, ty=362436069, tz=521288629, tw=88675123;
+    unsigned int tt = (tx^(tx<<11));
+    tx = ty; ty = tz; tz = tw;
+    return ( tw=(tw^(tw>>19))^(tt^(tt>>8)) );
+}
+ 
+int main() {
+    int sx,sy,tx,ty; double p; cin >> sx >> sy >> tx >> ty >> p;
+    vector<string> r_wall(20),d_wall(19);
+    rep(i,20)cin >> r_wall[i];
+    rep(i,19)cin >> d_wall[i];
+
+    int rand = (int)((double)1/p);
+ 
+    int nx = sx,ny = sy;
+    string ans = "";
+    while(ans.size() < 160){
+        ans += "RD";
+        if(randInt()%rand)ans += "R";
+        if(randInt()%rand)ans += "D";
+        if(ans.size()%11 == 5){
+            ans += "UUR";
+        }
+        else if(ans.size()%10 == 0){
+            ans += "LLD";
+        }
+    }
+    while(ans.size() <= 200)ans += "LLLUUURRRDDD";
+    ans.resize(200);
+    cout << ans << endl;
+    return 0;
+}
