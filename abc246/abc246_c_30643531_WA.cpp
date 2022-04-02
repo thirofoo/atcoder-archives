8a2a56f80cd4c6364581909ede5c365a5dc70603
thirofoo <thirofoo@gmail.com>
2022-04-02 21:25:16 +0900

    [WA] SubmissionID: 30643531 at 2022-04-02 21:25:16 (1648902316)

---
+++ abc246/abc246_c_30643531_WA.cpp
@@ -0,0 +1,53 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc246/submissions/30643531
+ * Submitted at: 2022-04-02 21:25:16
+ * Problem URL: https://atcoder.jp/contests/abc246/tasks/abc246_c
+ * Result: WA
+ * Execution Time: 105 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+typedef long long ll;
+typedef pair<int, int> P;
+typedef tuple<int, int, int> T;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    int n,k,x; cin >> n >> k >> x;
+    priority_queue<int> v;
+    rep(i,n){
+        int a; cin >> a;
+        v.push(a);
+    }
+
+    while(k){
+        int tmp = v.top();
+        if(tmp <= x)break;
+        v.pop();
+        int a = tmp/x;
+        if(k >= a){
+            k -= a;
+            tmp %= x;
+        }
+        else{
+            tmp -= k*x;
+            k = 0;
+        }
+        if(tmp == 0)tmp = x;
+        v.push(tmp);
+    }
+
+    ll ans = 0;
+    while(!v.empty()){
+        if(k)v.pop(),k--;
+        else ans += v.top(),v.pop();
+    }
+    cout << ans << endl;
+    return 0;
+}
