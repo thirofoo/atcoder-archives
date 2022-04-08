89e1a68c343357b017d655f196bc14c2b5d6a90c
thirofoo <thirofoo@gmail.com>
2022-04-08 16:11:50 +0900

    [WA] SubmissionID: 30786943 at 2022-04-08 16:11:50 (1649401910)

---
+++ abc189/abc189_b_30786943_WA.cpp
@@ -0,0 +1,32 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc189/submissions/30786943
+ * Submitted at: 2022-04-08 16:11:50
+ * Problem URL: https://atcoder.jp/contests/abc189/tasks/abc189_b
+ * Result: WA
+ * Execution Time: 8 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+typedef long long ll;
+typedef pair<int, int> P;
+typedef tuple<int, int, int> T;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    cout << fixed << setprecision(100);
+    double n,x; cin >> n >> x;
+    double now = 0;
+    rep(i,n){
+        double v,p; cin >> v >> p;
+        now += v*(p/100);
+        if(x < now)return cout << i+1 << endl,0;
+    }
+    cout << -1 << endl;
+    return 0;
+}
