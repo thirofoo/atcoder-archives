f4c94cd754eb09ad496e38dd86bba36c68157ee9
thirofoo <thirofoo@gmail.com>
2022-04-09 18:15:52 +0900

    [AC] SubmissionID: 30805482 at 2022-04-09 18:15:52 (1649495752)

---
+++ arc130/arc130_a_30805482_AC.cpp
@@ -0,0 +1,33 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/arc130/submissions/30805482
+ * Submitted at: 2022-04-09 18:15:52
+ * Problem URL: https://atcoder.jp/contests/arc130/tasks/arc130_a
+ * Result: AC
+ * Execution Time: 16 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+typedef long long ll;
+typedef pair<int, int> P;
+typedef tuple<int, int, int> T;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    ll n,cnt = 1,ans = 0; string s; cin >> n >> s;
+    rep(i,s.size()-1){
+        if(s[i+1] == s[i])cnt++;
+        else{
+            ans += cnt*(cnt-1)/2;
+            cnt = 1;
+        }
+    }
+    ans += cnt*(cnt-1)/2;
+    cout << ans << endl;
+    return 0;
+}
