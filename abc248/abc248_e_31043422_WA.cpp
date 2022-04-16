740e0f224ea1a94f89f0a7df48a14b14a45223d4
thirofoo <thirofoo@gmail.com>
2022-04-16 22:41:17 +0900

    [WA] SubmissionID: 31043422 at 2022-04-16 22:41:17 (1650116477)

---
+++ abc248/abc248_e_31043422_WA.cpp
@@ -0,0 +1,50 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc248/submissions/31043422
+ * Submitted at: 2022-04-16 22:41:17
+ * Problem URL: https://atcoder.jp/contests/abc248/tasks/abc248_e
+ * Result: WA
+ * Execution Time: 162 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+typedef long longll;
+typedef pair<long double,long double> P;
+typedef tuple<long double, long double, long double> T;
+#define rep(i, n) for(long double i = 0; i < n; i++)
+
+int main() {
+    cout << fixed << setprecision(10);
+    long double n,k; cin >> n >> k;
+    vector<P> vertex;
+    map<P,set<long double>> a;
+    set<P> c;
+    rep(i,n){
+        long double x,y; cin >> x >> y;
+        vertex.push_back(P(x,y));
+    }
+    if(n == 1 || k == 1)return cout << "Infinity" << endl,0;
+    rep(i,n){
+        rep(j,n){
+            if(i == j)continue;
+            auto [x1,y1] = vertex[i];
+            auto [x2,y2] = vertex[j];
+            long double k = (x1 == x2 ? LLONG_MAX : (y1-y2)/(x1-x2));
+            long double b = (x1 == x2 ? x1 : y1+k*(-x1));
+            a[P(k,b)].insert(i);
+            a[P(k,b)].insert(j);
+            c.insert(P(k,b));
+        }
+    }
+    long double ans = 0;
+    for(auto l:c){
+        if(a[l].size() >= k)ans++;
+    }
+    cout << ans << endl;
+    return 0;
+}
