da17cff7f2635a97824172fc0ae464b242af2e55
thirofoo <thirofoo@gmail.com>
2022-04-22 21:04:59 +0900

    [AC] SubmissionID: 31155119 at 2022-04-22 21:04:59 (1650629099)

---
+++ future-contest-2018-qual/future_contest_2018_qual_a_31155119_AC.cpp
@@ -0,0 +1,54 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/future-contest-2018-qual/submissions/31155119
+ * Submitted at: 2022-04-22 21:04:59
+ * Problem URL: https://atcoder.jp/contests/future-contest-2018-qual/tasks/future_contest_2018_qual_a
+ * Result: AC
+ * Execution Time: 15 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+typedef long long ll;
+typedef pair<int, int> P;
+typedef tuple<int, int, int> T;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+ 
+unsigned int randInt() {
+    static unsigned int tx = 123456789, ty=362436069, tz=521288629, tw=88675123;
+    unsigned int tt = (tx^(tx<<11));
+    tx = ty; ty = tz; tz = tw;
+    return ( tw=(tw^(tw>>19))^(tt^(tt>>8)) );
+}
+ 
+int main() {
+    //入力受付
+    auto start = chrono::system_clock::now();
+    vector<vector<int>> field(100,vector<int>(100,0));
+    vector<T> query;
+    rep(i,100)rep(j,100)cin >> field[i][j];
+    int cnt = 1000;
+ 
+    //処理パート
+    rep(i,1000){
+        int x = randInt()%100;
+        int y = randInt()%100;
+        int z = randInt()%100 + 1;
+        query.push_back(T(x,y,z));
+    }
+ 
+    //出力
+    cout << cnt << endl;
+    rep(i,cnt){
+        auto [x,y,h] = query[i];
+        cout << x << " " << y << " " << h << endl;
+    }
+    // auto end = chrono::system_clock::now();
+    // cout << "Passed time : " << chrono::duration_cast<chrono::milliseconds>(end-start).count() << "ms" << endl;
+ 
+    return 0;
+}
