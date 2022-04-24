5cd420c027519a5255b3cd90ebf1b17fb2d7369d
thirofoo <thirofoo@gmail.com>
2022-04-24 22:24:19 +0900

    [TLE] SubmissionID: 31243842 at 2022-04-24 22:24:19 (1650806659)

---
+++ arc139/arc139_a_31243842_TLE.cpp
@@ -0,0 +1,48 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/arc139/submissions/31243842
+ * Submitted at: 2022-04-24 22:24:19
+ * Problem URL: https://atcoder.jp/contests/arc139/tasks/arc139_a
+ * Result: TLE
+ * Execution Time: 2205 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+typedef long long ll;
+typedef pair<int, int> P;
+typedef tuple<int, int, int> T;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    ll n; cin >> n;
+    vector<ll> t(n),s(n);
+    rep(i,n)cin >> t[i];
+    rep(i,n){
+        s[i] = (1LL << t[i]);
+    }
+    ll cnt,ans = s[0];
+    rep(i,n-1){
+        // cout << ans << endl;
+        if(t[i+1] < t[i])ans = (ans | s[i+1]);
+        else{
+            cnt = t[i+1];
+            while((s[i+1] | (1LL << cnt)) <= ans){
+                cnt++;
+            }
+            ans = s[i+1] | (1LL << cnt);
+        }
+
+        // cnt = t[i];
+        // while((s[i+1] | (1LL << cnt)) <= ans){
+        //     cnt++;
+        // }
+        // ans = s[i+1] | (1LL << cnt);
+    }
+    cout << ans << endl;
+    return 0;
+}
