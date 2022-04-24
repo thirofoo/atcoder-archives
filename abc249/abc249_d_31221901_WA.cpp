965454ef8e7bf884fe68ae5ccdf07b4eca8856aa
thirofoo <thirofoo@gmail.com>
2022-04-24 11:20:03 +0900

    [WA] SubmissionID: 31221901 at 2022-04-24 11:20:03 (1650766803)

---
+++ abc249/abc249_d_31221901_WA.cpp
@@ -0,0 +1,57 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc249/submissions/31221901
+ * Submitted at: 2022-04-24 11:20:03
+ * Problem URL: https://atcoder.jp/contests/abc249/tasks/abc249_d
+ * Result: WA
+ * Execution Time: 1805 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+typedef long long ll;
+typedef pair<int, int> P;
+typedef tuple<int, int, int> T;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+vector<int> factorize(int n){
+    vector<int> solve;
+    solve.push_back(1);
+    if(n != 1)solve.push_back(n);
+    for(int i=2;i*i<=n;i++){
+        if(n%i)continue;
+        solve.push_back(i);
+        if(i != n/i)solve.push_back(n/i);
+    }
+    sort(solve.begin(),solve.end());
+    return solve;
+}
+
+int main() {
+    int n; cin >> n;
+    vector<int> a(n);
+    map<int,int> a_num;
+    vector<vector<int>> fact(n);
+    rep(i,n){
+        cin >> a[i];
+        a_num[a[i]]++;
+        fact[i] = factorize(a[i]);
+    }
+
+    ll ans = 0;
+    rep(i,n){
+        int size = fact[i].size();
+        rep(j,size){
+            int tmp1 = a_num[fact[i][j]]; 
+            int tmp2 = a_num[a[i]/fact[i][j]]; 
+            ans += tmp1*tmp2;
+        }
+    }
+    cout << ans << endl;
+
+    return 0;
+}
