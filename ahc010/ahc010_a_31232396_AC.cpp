071ab8569a3a395f0ae5c1ecdf0d170b4c56d10f
thirofoo <thirofoo@gmail.com>
2022-04-24 18:25:59 +0900

    [AC] SubmissionID: 31232396 at 2022-04-24 18:25:59 (1650792359)

---
+++ ahc010/ahc010_a_31232396_AC.cpp
@@ -0,0 +1,241 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/ahc010/submissions/31232396
+ * Submitted at: 2022-04-24 18:25:59
+ * Problem URL: https://atcoder.jp/contests/ahc010/tasks/ahc010_a
+ * Result: AC
+ * Execution Time: 1958 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+typedef long long ll;
+typedef pair<int, int> P;
+typedef tuple<int, int, int> T;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+//左上右下の順での移動表現
+vector<int> dx = {0,-1,0,1};
+vector<int> dy = {-1,0,1,0};
+
+//tileの状況
+vector<string> tiles(30);
+//tileを何回回転するかのメモ
+vector<vector<int>> r_time(30,vector<int>(30,0));
+//to[i][j][k]:tiles[i][j]に方向dから入った時の出る方向。
+vector<vector<vector<int>>> to(30,vector<vector<int>>(30,vector<int>(4)));
+
+namespace utility {
+    struct timer {
+        chrono::system_clock::time_point start;
+ 
+        // 開始時間を記録
+        void CodeStart() {
+            start = chrono::system_clock::now();
+        }
+ 
+        // 経過時間 (s) を返す
+        double elapsed() const {
+            using namespace std::chrono;
+            return (double)duration_cast<milliseconds>(system_clock::now() - start).count();
+        }
+    } mytm;
+}
+
+//Xor-shift (超高速乱数生成)
+unsigned int randInt() {
+    static unsigned int tx = 123456789, ty=362436069, tz=521288629, tw=88675123;
+    unsigned int tt = (tx^(tx<<11));
+    tx = ty; ty = tz; tz = tw;
+    return ( tw=(tw^(tw>>19))^(tt^(tt>>8)) );
+}
+
+vector<char> ch1 = {'0','1','2','3'};
+vector<char> ch2 = {'4','5'};
+vector<char> ch3 = {'6','7'};
+char to_ch(int i,int j){
+    char now = tiles[i][j],tmp;
+    if('0' <= now && now <= '3')tmp = ch1[(now-'0'+r_time[i][j])%4];
+    else if(now == '4' || now == '5')tmp = ch2[(now-'0'+r_time[i][j])%2];
+    else tmp = ch3[(now-'0'+r_time[i][j])%2];
+    return tmp;
+}
+
+void update(int i,int j){
+    switch(to_ch(i,j)){
+        case '0':
+            to[i][j][0] = 1;
+            to[i][j][1] = 0;
+            to[i][j][2] = -1;
+            to[i][j][3] = -1;
+            break;
+        case '1':
+            to[i][j][0] = 3;
+            to[i][j][1] = -1;
+            to[i][j][2] = -1;
+            to[i][j][3] = 0;
+            break;
+        case '2':
+            to[i][j][0] = -1;
+            to[i][j][1] = -1;
+            to[i][j][2] = 3;
+            to[i][j][3] = 2;
+            break;
+        case '3':
+            to[i][j][0] = -1;
+            to[i][j][1] = 2;
+            to[i][j][2] = 1;
+            to[i][j][3] = -1;
+            break;
+        case '4':
+            to[i][j][0] = 1;
+            to[i][j][1] = 0;
+            to[i][j][2] = 3;
+            to[i][j][3] = 2;
+            break;
+        case '5':
+            to[i][j][0] = 3;
+            to[i][j][1] = 2;
+            to[i][j][2] = 1;
+            to[i][j][3] = 0;
+            break;
+        case '6':
+            to[i][j][0] = 2;
+            to[i][j][1] = -1;
+            to[i][j][2] = 0;
+            to[i][j][3] = -1;
+            break;
+        case '7':
+            to[i][j][0] = -1;
+            to[i][j][1] = 3;
+            to[i][j][2] = -1;
+            to[i][j][3] = 1;
+            break;
+    };
+}
+
+void to_memo(){
+    rep(i,30)rep(j,30)update(i,j);
+}
+
+ll CalcScore(){
+    ll add = 0,length1 = 0,length2 = 0;
+    vector<vector<vector<bool>>> visited(30,vector<vector<bool>>(30,vector<bool>(4,false)));
+    rep(i,30){
+        rep(j,30){
+            rep(k,4){
+                if(visited[i][j][k])continue;
+                int sx = i,sy = j,sd = k,tmp_l = 0,x = i,y = j,d = k;
+                bool frag = false;
+                while(true){
+                    visited[x][y][d] = true;
+                    int nd = to[x][y][d];
+                    if(nd == -1){
+                        frag = true;
+                        break;
+                    }
+                    visited[x][y][nd] = true;
+                    x += dx[nd];
+                    y += dy[nd];
+                    if(x < 0 || 30 <= x || y < 0 || 30 <= y){
+                        frag = true;
+                        break;
+                    }
+                    //方向反転
+                    d = (nd+2)%4;
+                    tmp_l++;
+                    if(x == sx && y == sy && d == sd)break;
+                }
+                if(frag)add += tmp_l; 
+                else if(tmp_l > length1){
+                    length2 = length1;
+                    length1 = tmp_l;
+                }
+                else if(tmp_l > length2)length2 = tmp_l;
+                // if(tmp_l)cout << i << " " << j << " " << k << " : " << tmp_l << endl;
+            }
+        }
+    }
+    return length1*length2*100000 + add;
+}
+
+ll CalcOne(int i,int j){
+    ll add = 0,length = 0;
+    rep(k,4){
+        int sx = i,sy = j,sd = k,tmp_l = 0,x = i,y = j,d = k;
+        bool frag = false;
+        while(true){
+            int nd = to[x][y][d];
+            if(nd == -1){
+                frag = true;
+                break;
+            }
+            x += dx[nd];
+            y += dy[nd];
+            if(x < 0 || 30 <= x || y < 0 || 30 <= y){
+                frag = true;
+                break;
+            }
+            //方向反転
+            d = (nd+2)%4;
+            tmp_l++;
+            if(x == sx && y == sy && d == sd)break;
+        }
+        if(frag)add += tmp_l; 
+        else length = tmp_l;
+        // if(tmp_l)cout << i << " " << j << " " << k << " : " << tmp_l << endl;
+    }
+    return length*100 + add;
+}
+
+int main() {
+    utility::mytm.CodeStart();
+    
+    ll best_score = 0;
+    string best_str = "";
+    rep(i,30)cin >> tiles[i];
+
+    //処理part
+    while(utility::mytm.elapsed() <= 1000){
+        // cout << best_score << " ";
+        string now_str = "";
+        rep(i,30)rep(j,30){
+            r_time[i][j] = randInt()%4;
+            now_str += to_string(r_time[i][j]);
+        }
+        to_memo();
+        ll now_score = CalcScore();
+        if(now_score > best_score){
+            best_score = now_score;
+            best_str = now_str;
+        }
+    }
+
+    while(utility::mytm.elapsed() <= 1950){
+        // cout << best_score << " ";
+        int ni = randInt()%30;
+        int nj = randInt()%30;
+        r_time[ni][nj]++;
+        r_time[ni][nj] %= 4;
+        ll p_score = CalcOne(ni,nj);
+        update(ni,nj);
+        ll l_score = CalcOne(ni,nj);
+        // cout << p_score << " " << l_score << "  ";
+
+        // best_score += CalcDiff();
+        if(l_score > p_score){
+            best_str[ni*30+nj] = to_string(r_time[ni][nj])[0];
+            best_score += l_score - p_score;
+        }
+    }
+    cout << best_str << endl;
+
+    // cout << best_score << endl;
+    // cout << utility::mytm.elapsed() << endl;
+
+    return 0;
+}
