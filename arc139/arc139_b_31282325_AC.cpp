c80b2c42e878a2bc7b90020e4abd48697ad287a0
thirofoo <thirofoo@gmail.com>
2022-04-26 17:13:09 +0900

    [AC] SubmissionID: 31282325 at 2022-04-26 17:13:09 (1650960789)

---
+++ arc139/arc139_b_31282325_AC.cpp
@@ -0,0 +1,59 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/arc139/submissions/31282325
+ * Submitted at: 2022-04-26 17:13:09
+ * Problem URL: https://atcoder.jp/contests/arc139/tasks/arc139_b
+ * Result: AC
+ * Execution Time: 22 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+typedef long long ll;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> T;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    ll t,n,a,b,x,y,z;
+    cin >> t;
+
+    rep(i,t){
+        ll ans = LLONG_MAX;
+        cin >> n >> a >> b >> x >> y >> z;
+
+        //+1あたりのコストが少ない方をyとする。
+        if(y*b > z*a){
+            swap(y,z); swap(a,b);
+        }
+
+        //c1 : +aするのにコストが少ない方
+        //c2 : +bするのにコストが少ない方
+        ll c1 = min(y,a*x),c2 = min(z,b*x);
+        if(n/a <= a-1){
+            for(ll j=0;j<=n/a;j++){
+                ll rest = n-a*j,cost = c1*j;
+                cost += (rest/b)*c2;
+                rest -= (rest/b)*b;
+                cost += x*rest;
+                ans = min(ans,cost);
+            }
+        }
+        else{
+            for(ll j=0;j<=a-1;j++){
+                ll rest = n-b*j,cost = c2*j;
+                if(rest < 0)continue;
+                cost += (rest/a)*c1;
+                rest -= (rest/a)*a;
+                cost += x*rest;
+                ans = min(ans,cost);
+            }
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
