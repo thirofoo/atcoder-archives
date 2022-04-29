bbcd38a901c2bf1cf15997b0949fd3bc4dbd60b3
thirofoo <thirofoo@gmail.com>
2022-04-29 11:08:20 +0900

    [WA] SubmissionID: 31333491 at 2022-04-29 11:08:20 (1651198100)

---
+++ abc149/abc149_c_31333491_WA.cpp
@@ -0,0 +1,36 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc149/submissions/31333491
+ * Submitted at: 2022-04-29 11:08:20
+ * Problem URL: https://atcoder.jp/contests/abc149/tasks/abc149_c
+ * Result: WA
+ * Execution Time: 8 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+typedef long long ll;
+typedef pair<int, int> P;
+typedef tuple<int, int, int> T;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+vector<bool> is_prime;
+void Eratosthenes(int n){
+    for(int i=2;i<=n;i++){
+        if(!is_prime[i])continue;
+        for(int j=2;i*j<=n;j++)is_prime[i*j] = false;
+    }
+}
+
+int main() {
+    ll x; cin >> x;
+    is_prime.assign(2*x,false);
+    Eratosthenes(2*x);
+    while(!is_prime[x])x++;
+    cout << x << endl;
+    return 0;
+}
