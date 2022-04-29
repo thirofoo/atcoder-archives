cc144535d210fd0fc28a08b3ed7ffcb414096e36
thirofoo <thirofoo@gmail.com>
2022-04-29 11:10:45 +0900

    [AC] SubmissionID: 31333521 at 2022-04-29 11:10:45 (1651198245)

---
+++ abc149/abc149_c_31333521_AC.cpp
@@ -0,0 +1,71 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc149/submissions/31333521
+ * Submitted at: 2022-04-29 11:10:45
+ * Problem URL: https://atcoder.jp/contests/abc149/tasks/abc149_c
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+typedef long long ll;
+typedef pair<int, int> P;
+typedef tuple<int, int, int> T;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+ 
+struct prime_test{
+    unsigned int rand() {
+        static unsigned int tx = 123456789, ty=362436069, tz=521288629, tw=88675123;
+        unsigned int tt = (tx^(tx<<11));
+        tx = ty; ty = tz; tz = tw;
+        return ( tw=(tw^(tw>>19))^(tt^(tt>>8)) );
+    }
+ 
+    ll mod_pow(ll a,ll b,int mod){
+        ll cnt = 1,ans = 1;
+        vector<ll> b_mod;
+        b_mod.push_back(a%mod);
+        while(b >= (1LL << cnt)){
+            b_mod.push_back((b_mod[cnt-1]*b_mod[cnt-1])%mod);
+            cnt++;
+        }
+        cnt = 0;
+        while(true){
+            if(b < (1LL << cnt))break;
+            if(b & (1LL << cnt)){
+                ans *= b_mod[cnt]; 
+                ans %= mod;
+            }
+            cnt++;
+        }
+        return ans;
+    }
+    
+    bool is_prime(ll n){
+        if(n == 2)return true;
+        if(n == 1 || (n&1) == 0)return false;
+        ll tmp  = n-1;
+        while(tmp%2 == 0)tmp >>= 1;
+        rep(i,10){
+             ll a = rand()%(n-1) + 1,d = tmp;
+            ll y = mod_pow(a,tmp,n);
+            while(d != n-1 && y != 1 && y != n-1){
+                y = (y*y)%n; d <<= 1;
+            }
+            if(y != n-1 && (d&1) == 0)return false;
+        }
+        return true;
+    }
+} pt;
+ 
+int main() {
+    ll x; cin >> x;
+    while(!pt.is_prime(x))x++;
+    cout << x << endl;
+    return 0;
+}
