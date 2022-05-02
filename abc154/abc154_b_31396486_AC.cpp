2f2d6c9d77c8e7ba344e4595198d7d59ec156152
thirofoo <thirofoo@gmail.com>
2022-05-02 17:11:24 +0900

    [AC] SubmissionID: 31396486 at 2022-05-02 17:11:24 (1651479084)

---
+++ abc154/abc154_b_31396486_AC.cpp
@@ -0,0 +1,26 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc154/submissions/31396486
+ * Submitted at: 2022-05-02 17:11:24
+ * Problem URL: https://atcoder.jp/contests/abc154/tasks/abc154_b
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+typedef long long ll;
+typedef pair<int, int> P;
+typedef tuple<int, int, int> T;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    string s; cin >> s;
+    rep(i,s.size())cout << "x";
+    cout << endl;
+    return 0;
+}
