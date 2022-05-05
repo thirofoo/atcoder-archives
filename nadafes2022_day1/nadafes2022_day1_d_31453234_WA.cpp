d30ab0f6086edba099a2a8eefd6a401a8112bfed
thirofoo <thirofoo@gmail.com>
2022-05-05 20:03:44 +0900

    [WA] SubmissionID: 31453234 at 2022-05-05 20:03:44 (1651748624)

---
+++ nadafes2022_day1/nadafes2022_day1_d_31453234_WA.cpp
@@ -0,0 +1,41 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/nadafes2022_day1/submissions/31453234
+ * Submitted at: 2022-05-05 20:03:44
+ * Problem URL: https://atcoder.jp/contests/nadafes2022_day1/tasks/nadafes2022_day1_d
+ * Result: WA
+ * Execution Time: 29 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+typedef long long ll;
+typedef pair<int, int> P;
+typedef tuple<int, int, int> T;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    int n; cin >> n;
+    if(n%2)cout << -1 << endl;
+    else{
+        int cnt = 0;
+        deque<int> ans;
+        vector<int> a;
+        rep(i,n)ans.push_back(i);
+        while(!ans.empty()){
+            int tmp = (cnt%2 == 0 ? ans.front() : ans.back());
+            a.push_back(tmp);
+            if(cnt%2 == 0)ans.pop_front();
+            else ans.pop_back();
+            cnt++;
+        }
+        cout << 0 << " ";
+        rep(i,n-1)cout << (a[i+1]-a[i]+n)%n << " ";
+        cout << endl; 
+    }
+    return 0;
+}
