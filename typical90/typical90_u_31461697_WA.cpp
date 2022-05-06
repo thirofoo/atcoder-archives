3756342b0b4f6686491a348cada7bdaf4d30f288
thirofoo <thirofoo@gmail.com>
2022-05-06 10:43:33 +0900

    [WA] SubmissionID: 31461697 at 2022-05-06 10:43:33 (1651801413)

---
+++ typical90/typical90_u_31461697_WA.cpp
@@ -0,0 +1,39 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/typical90/submissions/31461697
+ * Submitted at: 2022-05-06 10:43:33
+ * Problem URL: https://atcoder.jp/contests/typical90/tasks/typical90_u
+ * Result: WA
+ * Execution Time: 50 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+typedef long long ll;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> T;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(false);
+    int n,m; cin >> n >> m;
+    scc_graph Graph(n);
+    rep(i,m){
+        int a,b; cin >> a >> b;
+        a--; b--;
+        Graph.add_edge(a,b);
+    }
+    vector<vector<int>> scc = Graph.scc();
+    ll ans = 0;
+    for(auto k:scc){
+        int size = k.size();
+        ans += (size-1)*size/2;
+    }
+    cout << ans << endl;
+    return 0;
+}
