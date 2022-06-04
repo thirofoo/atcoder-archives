f5a3ba272bcf582d0c16315b4390db19fc9b1526
thirofoo <thirofoo@gmail.com>
2022-06-04 21:17:28 +0900

    [TLE] SubmissionID: 32215962 at 2022-06-04 21:17:28 (1654345048)

---
+++ abc254/abc254_c_32215962_TLE.cpp
@@ -0,0 +1,41 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc254/submissions/32215962
+ * Submitted at: 2022-06-04 21:17:28
+ * Problem URL: https://atcoder.jp/contests/abc254/tasks/abc254_c
+ * Result: TLE
+ * Execution Time: 2205 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+typedef long long ll;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> T;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(false);
+    
+    int n,k; cin >> n >> k;
+    vector<int> a(n),b(n);
+    rep(i,n)cin >> a[i];
+    b = a;
+    priority_queue<int,vector<int>,greater<int>> q;
+    rep(i,n-k){
+        for(int j=i;j<n;j+=k)q.push(a[j]);
+        for(int j=i;j<n;j+=k){
+            a[j] = q.top();
+            q.pop();
+        }
+    }
+    sort(b.begin(),b.end());
+    if(a == b)cout << "Yes" << endl;
+    else cout << "No" << endl;
+    return 0;
+}
