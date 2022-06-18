8239cc31ef953b5eb4dcdcb1e08427eb7184187b
thirofoo <thirofoo@gmail.com>
2022-06-18 21:07:49 +0900

    [AC] SubmissionID: 32544135 at 2022-06-18 21:07:49 (1655554069)

---
+++ abc256/abc256_b_32544135_AC.cpp
@@ -0,0 +1,35 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc256/submissions/32544135
+ * Submitted at: 2022-06-18 21:07:49
+ * Problem URL: https://atcoder.jp/contests/abc256/tasks/abc256_b
+ * Result: AC
+ * Execution Time: 4 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+typedef long long ll;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> T;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(false);
+    
+    int n; cin >> n;
+    vector<int> a(n);
+    rep(i,n)cin >> a[i];
+    reverse(a.begin(),a.end());
+    rep(i,n-1)a[i+1] += a[i];
+    int ans = 0;
+    rep(i,n)if(a[i] <= 3)ans++;
+    cout << n-ans << endl;
+
+    return 0;
+}
