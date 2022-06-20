8de2e18da08ae7c9a24bae1a627ff43caec7ea76
thirofoo <thirofoo@gmail.com>
2022-06-20 12:07:13 +0900

    [WA] SubmissionID: 32616782 at 2022-06-20 12:07:13 (1655694433)

---
+++ arc142/arc142_c_32616782_WA.cpp
@@ -0,0 +1,42 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/arc142/submissions/32616782
+ * Submitted at: 2022-06-20 12:07:13
+ * Problem URL: https://atcoder.jp/contests/arc142/tasks/arc142_c
+ * Result: WA
+ * Execution Time: 16 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+typedef long long ll;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> T;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(false);
+    
+    int n,m = INT_MAX; cin >> n;
+    vector<int> p;
+    for(int i=3;i<=n;i++){
+        cout << "? " << 1 << " " << i << endl;
+        int d1,d2; cin >> d1;
+        cout << "? " << 2 << " " << i << endl;
+        cin >> d2;
+        m = min(d1+d2,m);
+        if(d1+d2 == 3)p.push_back(i);
+    }
+    if(m != 3 || p.size() == 1)return cout << "! " << m << endl,0;
+    cout << "? " << p[0] << " " << p[1] << endl;
+    int d; cin >> d;
+    if(d == 1)cout << "! " << 3 << endl;
+    else cout << "! " << 1 << endl;
+
+    return 0;
+}
