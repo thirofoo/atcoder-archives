ae20674203ecaced2433e2973ad7cda26ab0873d
thirofoo <thirofoo@gmail.com>
2022-06-26 11:12:15 +0900

    [AC] SubmissionID: 32761745 at 2022-06-26 11:12:15 (1656209535)

---
+++ arc127/arc127_a_32761745_AC.cpp
@@ -0,0 +1,52 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/arc127/submissions/32761745
+ * Submitted at: 2022-06-26 11:12:15
+ * Problem URL: https://atcoder.jp/contests/arc127/tasks/arc127_a
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+typedef long long ll;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> T;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+ll solve(ll s){
+    if(s == 1)return 1;
+    else return pow(10,s-1)+solve(s-1);
+}
+
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(false);
+    
+    ll n; cin >> n;
+    string nstr = to_string(n);
+    ll ans = 0,size = nstr.size();
+
+    rep(i,size){
+        if(nstr[i] == '0')break;
+
+        if(nstr[i] != '1'){
+            ans += solve(size-i);
+            break;
+        }
+        else{
+            string tmp = "";
+            for(int j=i;j<size;j++)tmp += nstr[j];
+            ans += stoll(tmp)-(ll)pow(10LL,size-i-1)+1;
+        }
+    }
+
+    for(int j=size-1;j>=1;j--)ans += solve(j);
+    cout << ans << endl;
+
+    return 0;
+}
