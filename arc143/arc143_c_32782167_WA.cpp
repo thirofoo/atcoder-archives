138ade913ea720c4d04a2d5509d3b8beddefdf74
thirofoo <thirofoo@gmail.com>
2022-06-26 22:59:04 +0900

    [WA] SubmissionID: 32782167 at 2022-06-26 22:59:04 (1656251944)

---
+++ arc143/arc143_c_32782167_WA.cpp
@@ -0,0 +1,51 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/arc143/submissions/32782167
+ * Submitted at: 2022-06-26 22:59:04
+ * Problem URL: https://atcoder.jp/contests/arc143/tasks/arc143_c
+ * Result: WA
+ * Execution Time: 30 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+typedef long long ll;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> T;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+ll euclid(ll a,ll b){
+    if(b != 0)return euclid(b,a%b);
+    else return a;
+}
+
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(false);
+    
+    ll n,x,y; cin >> n >> x >> y;
+    vector<ll> a(n);
+    rep(i,n)cin >> a[i];
+    if(x == y){
+        ll tmp = a[0];
+        rep(i,n)tmp = euclid(tmp,a[i]);
+        if(tmp%2 == 0)cout << "Second" << endl;
+        else cout << "First" << endl;
+    }
+    else{
+        ll a1 = 0,a2 = 0;
+        rep(i,n){
+            a[i] %= (x+y);
+            if(a[i] >= x && a[i] < x+y)a1++;
+            else a2++;
+        }
+        if(a1 >= a2)cout << "First" << endl;
+        else cout << "Second" << endl;
+    }
+
+    return 0;
+}
