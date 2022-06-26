985a1381ac227b3c16edf24e7eee6d60a0672df5
thirofoo <thirofoo@gmail.com>
2022-06-26 21:16:10 +0900

    [WA] SubmissionID: 32773736 at 2022-06-26 21:16:10 (1656245770)

---
+++ arc143/arc143_a_32773736_WA.cpp
@@ -0,0 +1,37 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/arc143/submissions/32773736
+ * Submitted at: 2022-06-26 21:16:10
+ * Problem URL: https://atcoder.jp/contests/arc143/tasks/arc143_a
+ * Result: WA
+ * Execution Time: 7 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+typedef long long ll;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> T;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(false);
+    
+    ll a,b,c; cin >> a >> b >> c;
+    priority_queue<int> sor;
+    sor.push(a);
+    sor.push(b);
+    sor.push(c);
+    a = sor.top(); sor.pop();
+    b = sor.top(); sor.pop();
+    c = sor.top(); sor.pop();
+    if(a > b+c)cout << -1 << endl;
+    else cout << a << endl;
+
+    return 0;
+}
