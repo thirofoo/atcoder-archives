acacb5fb0379a0d346666ca40f2b619fb7a8946b
thirofoo <thirofoo@gmail.com>
2022-06-30 13:03:16 +0900

    [AC] SubmissionID: 32844546 at 2022-06-30 13:03:16 (1656561796)

---
+++ abc151/abc151_e_32844546_AC.cpp
@@ -0,0 +1,43 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc151/submissions/32844546
+ * Submitted at: 2022-06-30 13:03:16
+ * Problem URL: https://atcoder.jp/contests/abc151/tasks/abc151_e
+ * Result: AC
+ * Execution Time: 38 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+typedef long long ll;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> T;
+typedef modint1000000007 mint;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(false);
+    
+    ll n,k; cin >> n >> k;
+    vector<ll> a(n),accum(n+1,0);
+    vector<mint> kaizyo(n+1,0);
+    rep(i,n)cin >> a[i];
+    sort(a.begin(),a.end());
+    rep(i,n)accum[i+1] = accum[i] + a[i];
+    if(k == 1)return cout << 0 << endl,0;
+    kaizyo[0] = 1;
+    rep(i,n)kaizyo[i+1] = kaizyo[i]*(i+1);
+    mint ans = 0;
+    //right-leftで全探索する
+    for(int i=k-1;i<n;i++){
+        ans += (kaizyo[i-1]/kaizyo[k-2]/kaizyo[i-1-(k-2)])*( (accum[n]-accum[i]) - (accum[n-i]) );
+    }
+    cout << ans.val() << endl;
+    
+    return 0;
+}
