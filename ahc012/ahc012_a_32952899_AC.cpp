7daece63c7c80f17cdefefa661e65dc9a002705e
thirofoo <thirofoo@gmail.com>
2022-07-03 15:10:03 +0900

    [AC] SubmissionID: 32952899 at 2022-07-03 15:10:03 (1656828603)

---
+++ ahc012/ahc012_a_32952899_AC.cpp
@@ -0,0 +1,55 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/ahc012/submissions/32952899
+ * Submitted at: 2022-07-03 15:10:03
+ * Problem URL: https://atcoder.jp/contests/ahc012/tasks/ahc012_a
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+typedef long long ll;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> T;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+vector<int> a(10);
+vector<P> stro;
+int n,k;
+
+namespace utility {
+    struct timer {
+        chrono::system_clock::time_point start;
+        // 開始時間を記録
+        void CodeStart() {
+            start = chrono::system_clock::now();
+        }
+        // 経過時間 (s) を返す
+        double elapsed() const {
+        using namespace std::chrono;
+            return (double)duration_cast<milliseconds>(system_clock::now() - start).count();
+        }
+    } mytm;
+}
+
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(false);
+    
+    cin >> n >> k;
+    rep(i,10)cin >> a[i];
+    rep(i,n){
+        int x,y; cin >> x >> y;
+        stro.push_back(P(x,y));
+    }
+
+    utility::mytm.CodeStart();
+    cout << 0 << endl;
+    
+    return 0;
+}
