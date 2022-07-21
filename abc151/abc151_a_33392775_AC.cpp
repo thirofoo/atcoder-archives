da0006b3b6f8fee2a6974e5f3ca6f034daf215b4
thirofoo <thirofoo@gmail.com>
2022-07-21 18:21:07 +0900

    [AC] SubmissionID: 33392775 at 2022-07-21 18:21:07 (1658395267)

---
+++ abc151/abc151_a_33392775_AC.cpp
@@ -0,0 +1,29 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc151/submissions/33392775
+ * Submitted at: 2022-07-21 18:21:07
+ * Problem URL: https://atcoder.jp/contests/abc151/tasks/abc151_a
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+typedef long long ll;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> T;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(false);
+    
+    char z; cin >> z;
+    cout << (char)(z+1) << endl;
+    
+    return 0;
+}
