d2244fe08e5772e55e9f3e0feb4124b67f054276
thirofoo <thirofoo@gmail.com>
2022-07-31 22:32:38 +0900

    [WA] SubmissionID: 33687349 at 2022-07-31 22:32:38 (1659274358)

---
+++ abc262/abc262_d_33687349_WA.cpp
@@ -0,0 +1,44 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc262/submissions/33687349
+ * Submitted at: 2022-07-31 22:32:38
+ * Problem URL: https://atcoder.jp/contests/abc262/tasks/abc262_d
+ * Result: WA
+ * Execution Time: 416 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+typedef long long ll;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> T;
+typedef modint998244353 mint;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(false);
+    
+    ll n; cin >> n;
+    vector<ll> a(n);
+    rep(i,n)cin >> a[i];
+    vector<vector<vector<mint>>> dp(n+1,vector<vector<mint>>(n+1,vector<mint>(n,0)));
+    rep(i,n)dp[i+1][0][0] = 1;
+    mint ans = 0;
+    for(int k=1;k<=n;k++){
+        rep(l,n){
+            for(int i=n-1;i>=0;i--){
+                rep(j,k){
+                    dp[k][i+1][(j+a[i])%k] += dp[k][i][j];
+                }
+            }
+        }
+        ans += dp[k][k][0];
+    }
+    cout << ans.val() << endl;
+    return 0;
+}
