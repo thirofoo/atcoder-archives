dc6125754fe95f3674efdd05d9c24aff8f260d59
thirofoo <thirofoo@gmail.com>
2022-08-06 21:08:13 +0900

    [AC] SubmissionID: 33809062 at 2022-08-06 21:08:13 (1659787693)

---
+++ abc263/abc263_b_33809062_AC.cpp
@@ -0,0 +1,36 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc263/submissions/33809062
+ * Submitted at: 2022-08-06 21:08:13
+ * Problem URL: https://atcoder.jp/contests/abc263/tasks/abc263_b
+ * Result: AC
+ * Execution Time: 5 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+typedef long long ll;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> T;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(false);
+    
+    int n; cin >> n;
+    vector<int> p(n,0);
+    rep(i,n-1)cin >> p[i+1];
+    int now = n,ans = 0;
+    while(now != 1){
+        ans++;
+        now = p[now-1];
+    }
+    cout << ans << endl;
+    
+    return 0;
+}
