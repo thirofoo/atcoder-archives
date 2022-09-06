9b7fac449341c8c41b0caababeaed596e2e949b1
thirofoo <thirofoo@gmail.com>
2022-09-06 16:44:28 +0900

    [WA] SubmissionID: 34651360 at 2022-09-06 16:44:28 (1662450268)

---
+++ abc223/abc223_e_34651360_WA.cpp
@@ -0,0 +1,58 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc223/submissions/34651360
+ * Submitted at: 2022-09-06 16:44:28
+ * Problem URL: https://atcoder.jp/contests/abc223/tasks/abc223_e
+ * Result: WA
+ * Execution Time: 8 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+template<class T, class... Ts> void print(const T& a, const Ts&... b){cout << a;(cout << ... << (cout << ' ', b));cout << '\n';}
+template<class... T> void input(T&... a){(cin >> ... >> a);}
+void print(){cout << '\n';}
+typedef long long ll;
+typedef long double ld;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> T;
+// typedef modint1000000007 mint;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+bool inRect2(ll x,ll y,ll a,ll b){
+    // fieldの縦横2通り
+    rep(i,2){
+        // aを入れるための必要最低限の高さ
+        ll length = ceil((ld)a/(ld)x);
+        if(y > length && (y-length)*x >= b)return true;
+        swap(x,y);
+    }
+    return false;
+}
+
+bool inRect3(ll x,ll y,ll a,ll b,ll c){
+    // fieldの縦横の2通り
+    rep(i,2){
+        // aを入れるための必要最低限の高さ
+        ll length = ceil((ld)a/(ld)x);
+        if(y > length && inRect2(x,y-length,b,c))return true;
+        swap(x,y);
+    }
+    return false;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    // 解説AC。天才パズルゲー&実装が思いつかない。
+    // fieldの縦横・A,B,Cそれぞれの縦横を全てシミュレーションする。
+    ll x,y,a,b,c; input(x,y,a,b,c);
+    print((inRect3(x,y,a,b,c) ? "Yes" : "No"));
+    
+    return 0;
+}
