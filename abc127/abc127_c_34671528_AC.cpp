d6b26d5d8149ac97d80361a37d2f1bfb4f8a8eb3
thirofoo <thirofoo@gmail.com>
2022-09-07 18:24:16 +0900

    [AC] SubmissionID: 34671528 at 2022-09-07 18:24:16 (1662542656)

---
+++ abc127/abc127_c_34671528_AC.cpp
@@ -0,0 +1,41 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc127/submissions/34671528
+ * Submitted at: 2022-09-07 18:24:16
+ * Problem URL: https://atcoder.jp/contests/abc127/tasks/abc127_c
+ * Result: AC
+ * Execution Time: 24 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+template<class T, class... Ts> void print(const T& a, const Ts&... b){cout << a;(cout << ... << (cout << ' ', b));cout << '\n';}
+template<class... T> void input(T&... a){(cin >> ... >> a);}
+void print(){cout << '\n';}
+typedef long long ll;
+typedef long double ld;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> T;
+// typedef modint1000000007 mint;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    ll n,m; input(n,m);
+    ll left,right; input(left,right);
+    rep(i,m-1){
+        ll l,r; input(l,r);
+        left = max(left,l);
+        right = min(right,r);
+    }
+    if(left > right)print(0);
+    else print(right-left+1);
+    
+    return 0;
+}
