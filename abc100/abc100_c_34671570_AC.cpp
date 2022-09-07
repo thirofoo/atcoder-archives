b85ecbe8c3be7df06423d7c8086be11f07c78c50
thirofoo <thirofoo@gmail.com>
2022-09-07 18:26:32 +0900

    [AC] SubmissionID: 34671570 at 2022-09-07 18:26:32 (1662542792)

---
+++ abc100/abc100_c_34671570_AC.cpp
@@ -0,0 +1,43 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc100/submissions/34671570
+ * Submitted at: 2022-09-07 18:26:32
+ * Problem URL: https://atcoder.jp/contests/abc100/tasks/abc100_c
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+template<class T, class... Ts> void print(const T& a, const Ts&... b){cout << a;(cout << ... << (cout << ' ', b));cout << '\n';}
+template<class... T> void input(T&... a){(cin >> ... >> a);}
+void print(){cout << '\n';}
+typedef long long ll;
+typedef long double ld;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> T;
+// typedef modint1000000007 mint;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    ll n; input(n);
+    vector<ll> a(n);
+    rep(i,n)input(a[i]);
+    ll ans = 0;
+    rep(i,n){
+        while(a[i]%2 == 0){
+            a[i] /= 2;
+            ans++;
+        }
+    }
+    print(ans);
+    
+    return 0;
+}
