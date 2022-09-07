29ede0aa03bf1b7e676109fb8fae1decb0b20ba8
thirofoo <thirofoo@gmail.com>
2022-09-07 18:39:39 +0900

    [AC] SubmissionID: 34671793 at 2022-09-07 18:39:39 (1662543579)

---
+++ abc046/abc046_b_34671793_AC.cpp
@@ -0,0 +1,36 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc046/submissions/34671793
+ * Submitted at: 2022-09-07 18:39:39
+ * Problem URL: https://atcoder.jp/contests/abc046/tasks/abc046_b
+ * Result: AC
+ * Execution Time: 7 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+template<class T, class... Ts> void print(const T& a, const Ts&... b){cout << a;(cout << ... << (cout << ' ', b));cout << '\n';}
+template<class... T> void input(T&... a){(cin >> ... >> a);}
+void print(){cout << '\n';}
+typedef long long ll;
+typedef long double ld;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> T;
+// typedef modint1000000007 mint;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    ll n,k; input(n,k);
+    ll ans = k; n--;
+    while(n--)ans *= (k-1);
+    print(ans);
+    
+    return 0;
+}
