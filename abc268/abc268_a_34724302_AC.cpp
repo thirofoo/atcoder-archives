1ac37f332a37bff12f8db323e47711fd6b037cab
thirofoo <thirofoo@gmail.com>
2022-09-10 21:00:45 +0900

    [AC] SubmissionID: 34724302 at 2022-09-10 21:00:45 (1662811245)

---
+++ abc268/abc268_a_34724302_AC.cpp
@@ -0,0 +1,40 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc268/submissions/34724302
+ * Submitted at: 2022-09-10 21:00:45
+ * Problem URL: https://atcoder.jp/contests/abc268/tasks/abc268_a
+ * Result: AC
+ * Execution Time: 5 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+template<class T, class... Ts> void print(const T& a, const Ts&... b){cout << a;(cout << ... << (cout << ' ', b));cout << '\n';}
+template<class... T> void input(T&... a){(cin >> ... >> a);}
+void print(){cout << '\n';}
+typedef long long ll;
+typedef long double ld;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> T;
+// typedef modint1000000007 mint;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    ll a,b,c,d,e; input(a,b,c,d,e);
+    set<ll> cnt;
+    cnt.insert(a);
+    cnt.insert(b);
+    cnt.insert(c);
+    cnt.insert(d);
+    cnt.insert(e);
+    print(cnt.size());
+    
+    return 0;
+}
