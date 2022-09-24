0c8678906f141946beb029b31f82f6be3810c1dd
thirofoo <thirofoo@gmail.com>
2022-09-24 21:18:01 +0900

    [WA] SubmissionID: 35104101 at 2022-09-24 21:18:01 (1664021881)

---
+++ abc270/abc270_b_35104101_WA.cpp
@@ -0,0 +1,39 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc270/submissions/35104101
+ * Submitted at: 2022-09-24 21:18:01
+ * Problem URL: https://atcoder.jp/contests/abc270/tasks/abc270_b
+ * Result: WA
+ * Execution Time: 8 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+template<class T, class... Ts> void print(const T& a, const Ts&... b){cout << a;(cout << ... << (cout << ' ', b));cout << '\n';}
+template<class... T> void input(T&... a){(cin >> ... >> a);}
+void print(){cout << '\n';}
+typedef long long ll;
+typedef long double ld;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> T;
+// typedef modint1000000007 mint;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    ll x,y,z; input(x,y,z);
+    if((y >= 0 && z >= 0 && z >= y) || ((y <= 0 && z <= 0 && z <= y))){
+        if( (y >= 0 && y >= x) || (y <= 0 && y <= x) )print(abs(-x));
+        else print(-1);
+    }
+    else if((x >= 0 && y >= 0 && y >= x) || (x <= 0 && y <= 0 && y <= x))print(abs(x));
+    else print(abs(z)+abs(x-z));
+    
+    return 0;
+}
