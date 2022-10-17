3fbad5d923857d2271d56e4205c51fe4ff2f7b4e
thirofoo <thirofoo@gmail.com>
2022-10-17 10:39:13 +0900

    [AC] SubmissionID: 35742872 at 2022-10-17 10:39:13 (1665970753)

---
+++ abc272/abc272_b_35742872_AC.cpp
@@ -0,0 +1,52 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc272/submissions/35742872
+ * Submitted at: 2022-10-17 10:39:13
+ * Problem URL: https://atcoder.jp/contests/abc272/tasks/abc272_b
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+template<class T, class... Ts> void print(const T& a, const Ts&... b){cout << a;(cout << ... << (cout << ' ', b));cout << '\n';}
+template<class... T> void input(T&... a){(cin >> ... >> a);}
+void print(){cout << '\n';}
+typedef long long ll;
+typedef long double ld;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> T;
+// typedef modint1000000007 mint;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    ll n,m; input(n,m);
+    vector<vector<bool>> visited(n,vector<bool>(n,false));
+    rep(i,n)visited[i][i] = true;
+    while(m--){
+        ll k; input(k);
+        vector<ll> x(k);
+        rep(i,k){
+            input(x[i]);
+            x[i]--;
+        }
+        rep(i,k){
+            for(int j=i+1;j<k;j++){
+                visited[x[i]][x[j]] = true;
+                visited[x[j]][x[i]] = true;
+            }
+        }
+    }
+    bool f = true;
+    rep(i,n)rep(j,n)f &= visited[i][j];
+    print((f ? "Yes" : "No"));
+    
+    return 0;
+}
