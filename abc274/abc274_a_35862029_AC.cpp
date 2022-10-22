dacecea3058f3248d765ea97453eb0e44f2e8e25
thirofoo <thirofoo@gmail.com>
2022-10-22 21:01:36 +0900

    [AC] SubmissionID: 35862029 at 2022-10-22 21:01:36 (1666440096)

---
+++ abc274/abc274_a_35862029_AC.cpp
@@ -0,0 +1,35 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc274/submissions/35862029
+ * Submitted at: 2022-10-22 21:01:36
+ * Problem URL: https://atcoder.jp/contests/abc274/tasks/abc274_a
+ * Result: AC
+ * Execution Time: 5 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+template<class T, class... Ts> void print(const T& a, const Ts&... b){cout << a;(cout << ... << (cout << ' ', b));cout << '\n';}
+template<class... T> void input(T&... a){(cin >> ... >> a);}
+void print(){cout << '\n';}
+typedef long long ll;
+typedef long double ld;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> T;
+// typedef modint1000000007 mint;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    cout << setprecision(3) << fixed;
+    ld a,b; input(a,b);
+    print(b/a);
+    
+    return 0;
+}
