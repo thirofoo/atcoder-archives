083541d0b4812660924ec45b30478688e78820ee
thirofoo <thirofoo@gmail.com>
2022-11-03 11:36:57 +0900

    [AC] SubmissionID: 36167688 at 2022-11-03 11:36:57 (1667443017)

---
+++ abc183/abc183_f_36167688_AC.cpp
@@ -0,0 +1,65 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc183/submissions/36167688
+ * Submitted at: 2022-11-03 11:36:57
+ * Problem URL: https://atcoder.jp/contests/abc183/tasks/abc183_f
+ * Result: AC
+ * Execution Time: 247 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+template<class T, class... Ts> void print(const T& a, const Ts&... b){cout << a;(cout << ... << (cout << ' ', b));cout << '\n';}
+template<class... T> void input(T&... a){(cin >> ... >> a);}
+void print(){cout << '\n';}
+typedef long long ll;
+typedef long double ld;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> T;
+// typedef modint1000000007 mint;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    // 解説AC
+    ll n,q; input(n,q);
+    vector<map<ll,ll>> dict(n);
+    rep(i,n){
+        ll c; input(c); c--;
+        // 連結成分iにおけるクラスcの個数
+        dict[i][c] = 1;
+    }
+    dsu uf(n);
+
+    while(q--){
+        ll t,a,b; input(t,a,b);
+        a--; b--;
+        if(t == 1){
+            if(a > b)swap(a,b);
+            if(uf.same(a,b))continue;
+            ll sx = uf.size(a),sy = uf.size(b);
+            if(sx < sy)swap(a,b);
+            // 連結成分x > 連結成分y の状況を作る
+            ll x = uf.leader(a),y = uf.leader(b);
+
+            // 大きい成分に小さい成分を足す様に回す
+            // => for文がO(N)でなくmaxO(logN)になる
+            // (merge sortっぽい状況になる時が最大計算量)
+            for(auto [cla,num]:dict[y]){
+                dict[x][cla] += num;
+            }
+            uf.merge(a,b);
+        }
+        else{
+            print(dict[uf.leader(a)][b]);
+        }
+    }
+    
+    return 0;
+}
