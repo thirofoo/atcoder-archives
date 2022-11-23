743e7e7244ed85c5a66c669c53b8552881686881
thirofoo <thirofoo@gmail.com>
2022-11-23 19:16:17 +0900

    [AC] SubmissionID: 36737744 at 2022-11-23 19:16:17 (1669198577)

---
+++ abc169/abc169_a_36737744_AC.cpp
@@ -0,0 +1,34 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc169/submissions/36737744
+ * Submitted at: 2022-11-23 19:16:17
+ * Problem URL: https://atcoder.jp/contests/abc169/tasks/abc169_a
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+template<class T, class... Ts> void print(const T& a, const Ts&... b){cout << a;(cout << ... << (cout << ' ', b));cout << '\n';}
+template<class... T> void input(T&... a){(cin >> ... >> a);}
+void print(){cout << '\n';}
+typedef long long ll;
+typedef long double ld;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> T;
+// typedef modint1000000007 mint;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    ll a,b; input(a,b);
+    print(a*b);
+    
+    return 0;
+}
