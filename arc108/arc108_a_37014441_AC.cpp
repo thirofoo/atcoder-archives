ade6cfd7d9b5c83d6a271c36beff094b226d2e5c
thirofoo <thirofoo@gmail.com>
2022-12-04 16:56:55 +0900

    [AC] SubmissionID: 37014441 at 2022-12-04 16:56:55 (1670140615)

---
+++ arc108/arc108_a_37014441_AC.cpp
@@ -0,0 +1,52 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/arc108/submissions/37014441
+ * Submitted at: 2022-12-04 16:56:55
+ * Problem URL: https://atcoder.jp/contests/arc108/tasks/arc108_a
+ * Result: AC
+ * Execution Time: 24 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+template<class T, class... Ts> void print(const T& a, const Ts&... b){cout << a;(cout << ... << (cout << ' ', b));cout << '\n';}
+template<class... T> void input(T&... a){(cin >> ... >> a);}
+void print(){cout << '\n';}
+typedef long long ll;
+typedef long double ld;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> T;
+// typedef modint1000000007 mint;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+//約数列挙ver(1,nも含む)
+vector<ll> divisor(ll n){
+    vector<ll> div;
+    for(ll i=1;i*i<=n;i++){
+        if(n%i)continue;
+        div.push_back(i);
+        if(i != n/i)div.push_back(n/i);
+    }
+    return div;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    ll s,p; input(s,p);
+    vector<ll> d = divisor(p);
+    map<ll,ll> mp;
+    for(auto ele:d){
+        if(mp.count(ele))break;
+        if(ele + p/ele == s)return print("Yes"),0;
+        mp[ele]++;
+    }
+    print("No");
+    
+    return 0;
+}
