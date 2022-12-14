16f9b796b28d334d8e10ee98017c348a87411bb6
thirofoo <thirofoo@gmail.com>
2022-12-14 23:54:56 +0900

    [AC] SubmissionID: 37273096 at 2022-12-14 23:54:56 (1671029696)

---
+++ abc172/abc172_d_37273096_AC.cpp
@@ -0,0 +1,67 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc172/submissions/37273096
+ * Submitted at: 2022-12-14 23:54:56
+ * Problem URL: https://atcoder.jp/contests/abc172/tasks/abc172_d
+ * Result: AC
+ * Execution Time: 1202 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+template<class T, class... Ts> void print(const T& a, const Ts&... b){cout << a;(cout << ... << (cout << ' ', b));cout << '\n';}
+template<class... T> void input(T&... a){(cin >> ... >> a);}
+void print(){cout << '\n';}
+typedef long long ll;
+typedef long double ld;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> T;
+// typedef modint1000000007 mint;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+// d[i] : i以下の最小の素因数 (エラトステネス風に前計算)
+vector<ll> d;
+void fact_prepare(ll n){
+    // n以下の高速化前計算
+    n++;
+    d.assign(n,0);
+    for(ll i=2;i<n;i++){
+        if(d[i])continue;
+        for(ll j=i;j<n;j+=i){
+            if(!d[j])d[j] = i;
+        }
+    }
+}
+
+//素因数列挙ver (圧縮)
+ll factorize(ll n){
+    ll ans = 1;
+    ll num = d[n],cnt = 0;
+    while(d[n]){
+        if(d[n] == num)cnt++;
+        else {
+            ans *= cnt+1;
+            num = d[n];
+            cnt = 1;
+        }
+        n /= d[n];
+    }
+    ans *= cnt+1;
+    return ans;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    ll n,ans = 0; input(n);
+    fact_prepare(n);
+    rep(i,n)ans += factorize(i+1)*(i+1);
+    print(ans);
+    
+    return 0;
+}
