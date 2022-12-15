81581b7c797db2b23da9a251a362b451eb71d1b7
thirofoo <thirofoo@gmail.com>
2022-12-15 13:52:09 +0900

    [AC] SubmissionID: 37279726 at 2022-12-15 13:52:09 (1671079929)

---
+++ abc163/abc163_d_37279726_AC.cpp
@@ -0,0 +1,41 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc163/submissions/37279726
+ * Submitted at: 2022-12-15 13:52:09
+ * Problem URL: https://atcoder.jp/contests/abc163/tasks/abc163_d
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+template<class T, class... Ts> void print(const T& a, const Ts&... b){cout << a;(cout << ... << (cout << ' ', b));cout << '\n';}
+template<class... T> void input(T&... a){(cin >> ... >> a);}
+void print(){cout << '\n';}
+typedef long long ll;
+typedef long double ld;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> T;
+typedef modint1000000007 mint;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    ll n,k; input(n,k);
+    vector<ll> r(n+1,0);
+    rep(i,n)r[i+1] += r[i]+i+1;
+    mint ans = 0;
+    for(ll i=k;i<n+1;i++){
+        ans += r[n]-r[n-i] - r[i-1] + 1;
+    }
+    ans++;
+    print(ans.val());
+    
+    return 0;
+}
