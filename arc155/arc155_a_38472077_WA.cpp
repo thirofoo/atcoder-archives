bf2d47dee08b567be21988987c93bc0f164f87a5
thirofoo <thirofoo@gmail.com>
2023-01-30 08:40:30 +0900

    [WA] SubmissionID: 38472077 at 2023-01-30 08:40:30 (1675035630)

---
+++ arc155/arc155_a_38472077_WA.cpp
@@ -0,0 +1,62 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/arc155/submissions/38472077
+ * Submitted at: 2023-01-30 08:40:30
+ * Problem URL: https://atcoder.jp/contests/arc155/tasks/arc155_a
+ * Result: WA
+ * Execution Time: 7 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+template<class T, class... Ts> void print(const T& a, const Ts&... b){cout << a;(cout << ... << (cout << ' ', b));cout << '\n';}
+template<class... T> void input(T&... a){(cin >> ... >> a);}
+void print(){cout << '\n';}
+typedef long long ll;
+typedef long double ld;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> T;
+// typedef modint1000000007 mint;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    ll t; input(t);
+    while(t--){
+        ll n,k; string s; input(n,k,s);
+        ll tmp = k%(2*n);
+        bool f = true;
+        ll left = 0,right = n-tmp-1;
+        while(left < right){
+            f &= (s[left] == s[right]);
+            left++;
+            right--;
+        }
+        left = tmp,right = n-1;
+        while(left < right){
+            f &= (s[left] == s[right]);
+            left++;
+            right--;
+        }
+        if(f)print(k%n == tmp ? "Yes" : "No");
+        else{
+            bool f2 = true;
+            left = 0,right = n-1;
+            while(left < right){
+                f2 &= (s[left] == s[right]);
+                left++;
+                right--;
+            }
+            if(f2)print(k%n == 0 ? "Yes" : "No");
+            else print(k%(2*n) == n ? "Yes" : "No");
+        }
+    }
+    
+    return 0;
+}
