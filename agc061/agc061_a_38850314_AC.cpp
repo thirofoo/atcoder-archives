5189150b8ccd2174a61305a798a8f74ae78428b1
thirofoo <thirofoo@gmail.com>
2023-02-13 00:35:01 +0900

    [AC] SubmissionID: 38850314 at 2023-02-13 00:35:01 (1676216101)

---
+++ agc061/agc061_a_38850314_AC.cpp
@@ -0,0 +1,74 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/agc061/submissions/38850314
+ * Submitted at: 2023-02-13 00:35:01
+ * Problem URL: https://atcoder.jp/contests/agc061/tasks/agc061_a
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+template<class T, class... Ts> void print(const T& a, const Ts&... b){cout << a;(cout << ... << (cout << ' ', b));cout << '\n';}
+template<class... T> void input(T&... a){(cin >> ... >> a);}
+void print(){cout << '\n';}
+typedef long long ll;
+typedef long double ld;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> T;
+// typedef modint1000000007 mint;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+bool is_nCk_odd(ll n, ll k){
+    if(n <= 1)return 1;
+    return (n&k)==k;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+
+    ll t; input(t);
+    while(t--){
+        ll n,k; input(n,k);
+        if(k == 1)print(2);
+        else if(n%2 == 0){
+            if(is_nCk_odd(n/2-1,(k-1)/2)){
+                if(k%2 == 0)print(k-1);
+                else print(k+1);
+            }
+            else{
+                print(k);
+            }
+        }
+        else{
+            ll p;
+            k--;
+            if(is_nCk_odd((n-1)/2-1,(k-1)/2)){
+                if(k%2 == 0)p = k-1;
+                else p = k+1;
+                // cout << "odd ";
+            }
+            else{
+                p = k;
+                // cout << "even ";
+            }
+            // cout << p << " ";
+            p++;
+            // cout << p << " ";
+            if((n-1)/2-1 >= (p-1)/2 && is_nCk_odd((n-1)/2-1,(p-1)/2)){
+                if(p%2 == 0)print(p-1);
+                else print(p+1);
+            }
+            else{
+                print(p);
+            }
+        }
+    }
+    
+    return 0;
+}
