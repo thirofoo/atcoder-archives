8b8c2f10a35246b227452ce667badee57c3ef9d9
thirofoo <thirofoo@gmail.com>
2023-02-17 19:21:29 +0900

    [AC] SubmissionID: 38943151 at 2023-02-17 19:21:29 (1676629289)

---
+++ abc240/abc240_f_38943151_AC.cpp
@@ -0,0 +1,58 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc240/submissions/38943151
+ * Submitted at: 2023-02-17 19:21:29
+ * Problem URL: https://atcoder.jp/contests/abc240/tasks/abc240_f
+ * Result: AC
+ * Execution Time: 86 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+template<class T, class... Ts> void print(const T& a, const Ts&... b){cout << a;(cout << ... << (cout << ' ', b));cout << '\n';}
+template<class... T> void input(T&... a){(cin >> ... >> a);}
+void print(){cout << '\n';}
+typedef long long ll;
+typedef long double ld;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> T;
+// typedef modint1000000007 mint;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    ll t; input(t);
+    while(t--){
+        // 候補は高々N個。
+        ll n,m; input(n,m);
+        vector<ll> rui(n+1,0),ruirui(n+1,0),xx,yy;
+        rep(i,n){
+            ll x,y; input(x,y);
+            xx.emplace_back(x);
+            yy.emplace_back(y);
+            rui[i+1] = rui[i] + x*y;
+            ruirui[i+1] = ruirui[i] + (rui[i]+x)*y + (y*(y-1)/2)*x;
+        }
+
+        ll ans = xx[0];
+        // 累積和が正の時、後ろの何項目まで単調増加か。
+        rep(i,n){
+            if(i != 0 && rui[i] > 0 && rui[i+1] < 0){
+                ll d = xx[i];
+                ll a1 = rui[i]+xx[i];
+                ll tn = abs(rui[i]/xx[i]);
+                ans = max(ans,ruirui[i] + a1*tn + (tn*(tn-1)/2)*d);
+            }
+        }
+        ans = max(ans,ruirui[n]);
+        print(ans);
+    }
+    
+    return 0;
+}
