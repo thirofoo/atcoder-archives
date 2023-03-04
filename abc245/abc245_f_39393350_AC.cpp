6c4a89af283ebf8ad389004f82ee81778c39fb4c
thirofoo <thirofoo@gmail.com>
2023-03-04 15:35:00 +0900

    [AC] SubmissionID: 39393350 at 2023-03-04 15:35:00 (1677911700)

---
+++ abc245/abc245_f_39393350_AC.cpp
@@ -0,0 +1,69 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc245/submissions/39393350
+ * Submitted at: 2023-03-04 15:35:00
+ * Problem URL: https://atcoder.jp/contests/abc245/tasks/abc245_f
+ * Result: AC
+ * Execution Time: 90 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+template<class T, class... Ts> void print(const T& a, const Ts&... b){cout << a;(cout << ... << (cout << ' ', b));cout << '\n';}
+template<class... T> void input(T&... a){(cin >> ... >> a);}
+void print(){cout << '\n';}
+typedef long long ll;
+typedef long double ld;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> T;
+// typedef modint1000000007 mint;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+vector<bool> seen,visited,connect_roop;
+bool dfs(const ll &now, const vector<vector<ll>> &Graph){
+    if(visited[now])return connect_roop[now];
+    if(seen[now] && !visited[now]){
+        visited[now] = true;
+        connect_roop[now] = true;
+        return connect_roop[now];
+    }
+
+    seen[now] = true;
+    for(auto to:Graph[now]){
+        if(dfs(to,Graph))connect_roop[now] = true;
+    }
+    seen[now] = false;
+    visited[now] = true;
+    return connect_roop[now];
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    ll n,m; input(n,m);
+    vector Graph(n,vector<ll>{});
+    rep(i,m){
+        ll u,v; input(u,v);
+        u--; v--;
+        Graph[u].push_back(v);
+    }
+
+    seen.assign(n,false);
+    visited.assign(n,false);
+    connect_roop.assign(n,false);
+    rep(i,n){
+        if(visited[i])continue;
+        dfs(i,Graph);
+    }
+
+    ll ans = 0;
+    rep(i,n)if(connect_roop[i])ans++;
+    print(ans);
+
+    return 0;
+}
