a145f0e0b94db8d812ead898301470d4396bce18
thirofoo <thirofoo@gmail.com>
2023-03-11 21:02:19 +0900

    [AC] SubmissionID: 39601509 at 2023-03-11 21:02:19 (1678536139)

---
+++ abc293/abc293_a_39601509_AC.cpp
@@ -0,0 +1,35 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc293/submissions/39601509
+ * Submitted at: 2023-03-11 21:02:19
+ * Problem URL: https://atcoder.jp/contests/abc293/tasks/abc293_a
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+template<class T, class... Ts> void print(const T& a, const Ts&... b){cout << a;(cout << ... << (cout << ' ', b));cout << '\n';}
+template<class... T> void input(T&... a){(cin >> ... >> a);}
+void print(){cout << '\n';}
+typedef long long ll;
+typedef long double ld;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> T;
+// typedef modint1000000007 mint;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    string s; input(s);
+    rep(i,s.size()/2)swap(s[2*i],s[2*i+1]);
+    print(s);
+    
+    return 0;
+}
