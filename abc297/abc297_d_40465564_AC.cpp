874ff47e2999f3afa12f2f8425f04476017f648e
thirofoo <thirofoo@gmail.com>
2023-04-09 21:15:31 +0900

    [AC] SubmissionID: 40465564 at 2023-04-09 21:15:31 (1681042531)

---
+++ abc297/abc297_d_40465564_AC.cpp
@@ -0,0 +1,44 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc297/submissions/40465564
+ * Submitted at: 2023-04-09 21:15:31
+ * Problem URL: https://atcoder.jp/contests/abc297/tasks/abc297_d
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+template<class T, class... Ts> void print(const T& a, const Ts&... b){cout << a;(cout << ... << (cout << ' ', b));cout << '\n';}
+template<class... T> void input(T&... a){(cin >> ... >> a);}
+void print(){cout << '\n';}
+typedef long long ll;
+typedef long double ld;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> T;
+// typedef modint1000000007 mint;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    ll a,b; input(a,b);
+    ll ans = 0;
+    while(true){
+        if(a < b)swap(a,b);
+        if(a%b == 0){
+            ans += a/b-1;
+            break;
+        }
+        ans += a/b;
+        a -= (a/b)*b;
+    }
+    print(ans);
+    
+    return 0;
+}
