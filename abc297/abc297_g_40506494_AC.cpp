c28104108a8842d13d5b919faca1435be6287aff
thirofoo <thirofoo@gmail.com>
2023-04-10 13:26:39 +0900

    [AC] SubmissionID: 40506494 at 2023-04-10 13:26:39 (1681100799)

---
+++ abc297/abc297_g_40506494_AC.cpp
@@ -0,0 +1,39 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc297/submissions/40506494
+ * Submitted at: 2023-04-10 13:26:39
+ * Problem URL: https://atcoder.jp/contests/abc297/tasks/abc297_g
+ * Result: AC
+ * Execution Time: 32 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+template<class T, class... Ts> void print(const T& a, const Ts&... b){cout << a;(cout << ... << (cout << ' ', b));cout << '\n';}
+template<class... T> void input(T&... a){(cin >> ... >> a);}
+void print(){cout << '\n';}
+typedef long long ll;
+typedef long double ld;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> T;
+// typedef modint1000000007 mint;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    ll n,l,r; input(n,l,r);
+    ll ans = 0;
+    rep(i,n){
+        ll a; input(a);
+        ans ^= (a%(l+r))/l;
+    }
+    print(ans ? "First" : "Second");
+    
+    return 0;
+}
