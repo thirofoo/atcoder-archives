f3638dad1d72c433713c667e26adbc783c855683
thirofoo <thirofoo@gmail.com>
2023-04-30 01:07:05 +0900

    [WA] SubmissionID: 41057617 at 2023-04-30 01:07:05 (1682784425)

---
+++ abc300/abc300_f_41057617_WA.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc300/submissions/41057617
+ * Submitted at: 2023-04-30 01:07:05
+ * Problem URL: https://atcoder.jp/contests/abc300/tasks/abc300_f
+ * Result: WA
+ * Execution Time: 51 ms
+ */
+
+// code not found
