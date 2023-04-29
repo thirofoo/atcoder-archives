b34a6de018d655b46405314a5645f6a8869d374c
thirofoo <thirofoo@gmail.com>
2023-04-29 21:08:21 +0900

    [AC] SubmissionID: 41028263 at 2023-04-29 21:08:21 (1682770101)

---
+++ abc300/abc300_b_41028263_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc300/submissions/41028263
+ * Submitted at: 2023-04-29 21:08:21
+ * Problem URL: https://atcoder.jp/contests/abc300/tasks/abc300_b
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+// code not found
