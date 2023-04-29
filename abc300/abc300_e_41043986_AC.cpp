9be9eed4d66c3df388bc33fe15cd9481241c52fe
thirofoo <thirofoo@gmail.com>
2023-04-29 22:06:08 +0900

    [AC] SubmissionID: 41043986 at 2023-04-29 22:06:08 (1682773568)

---
+++ abc300/abc300_e_41043986_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc300/submissions/41043986
+ * Submitted at: 2023-04-29 22:06:08
+ * Problem URL: https://atcoder.jp/contests/abc300/tasks/abc300_e
+ * Result: AC
+ * Execution Time: 30 ms
+ */
+
+// code not found
