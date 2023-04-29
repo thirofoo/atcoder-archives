b632d66c95e327778521e539afee5f1668e6124f
thirofoo <thirofoo@gmail.com>
2023-04-30 01:08:15 +0900

    [WA] SubmissionID: 41057642 at 2023-04-30 01:08:15 (1682784495)

---
+++ abc300/abc300_f_41057642_WA.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc300/submissions/41057642
+ * Submitted at: 2023-04-30 01:08:15
+ * Problem URL: https://atcoder.jp/contests/abc300/tasks/abc300_f
+ * Result: WA
+ * Execution Time: 49 ms
+ */
+
+// code not found
