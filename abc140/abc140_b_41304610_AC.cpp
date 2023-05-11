b5788e3ba37aaa6ffb7b5ac83a6a717618392b89
thirofoo <thirofoo@gmail.com>
2023-05-11 18:40:17 +0900

    [AC] SubmissionID: 41304610 at 2023-05-11 18:40:17 (1683798017)

---
+++ abc140/abc140_b_41304610_AC.cpp
@@ -0,0 +1,35 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc140/submissions/41304610
+ * Submitted at: 2023-05-11 18:40:17
+ * Problem URL: https://atcoder.jp/contests/abc140/tasks/abc140_b
+ * Result: AC
+ * Execution Time: 10 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+typedef long long ll;
+typedef pair<int, int> P;
+typedef tuple<int, int, int> T;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    int n; cin >> n;
+    vector<int> a(n),b(n),c(n);
+    rep(i,n)cin >> a[i];
+    rep(i,n)cin >> b[i];
+    for(int i=1;i<n;i++)cin >> c[i];
+
+    int ans = 0;
+    rep(i,n){
+        ans += b[a[i]-1];
+        if(i && a[i] == a[i-1]+1)ans += c[a[i]-1];
+    }
+    cout << ans << endl;
+    return 0;
+}
