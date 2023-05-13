f4a4cd5dbdb490fffd33f3da38588c3f4071955f
thirofoo <thirofoo@gmail.com>
2023-05-13 21:01:11 +0900

    [WA] SubmissionID: 41344179 at 2023-05-13 21:01:11 (1683979271)

---
+++ abc301/abc301_a_41344179_WA.cpp
@@ -0,0 +1,37 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc301/submissions/41344179
+ * Submitted at: 2023-05-13 21:01:11
+ * Problem URL: https://atcoder.jp/contests/abc301/tasks/abc301_a
+ * Result: WA
+ * Execution Time: 7 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+template<class T, class... Ts> void print(const T& a, const Ts&... b){cout << a;(cout << ... << (cout << ' ', b));cout << '\n';}
+template<class... T> void input(T&... a){(cin >> ... >> a);}
+void print(){cout << '\n';}
+typedef long long ll;
+typedef long double ld;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> T;
+// typedef modint1000000007 mint;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    ll n; input(n);
+    string s; input(s);
+    ll cnt = 0;
+    rep(i,n)if(s[i] == 'T')cnt++;
+    print( (cnt >= (n-cnt) ? "T" : "A") );
+    
+    return 0;
+}
