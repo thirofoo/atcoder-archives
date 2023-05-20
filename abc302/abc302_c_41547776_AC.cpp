98a9bf131b35135c6b937054ee3820418743e323
thirofoo <thirofoo@gmail.com>
2023-05-20 21:15:16 +0900

    [AC] SubmissionID: 41547776 at 2023-05-20 21:15:16 (1684584916)

---
+++ abc302/abc302_c_41547776_AC.cpp
@@ -0,0 +1,59 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc302/submissions/41547776
+ * Submitted at: 2023-05-20 21:15:16
+ * Problem URL: https://atcoder.jp/contests/abc302/tasks/abc302_c
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+template<class T, class... Ts> void print(const T& a, const Ts&... b){cout << a;(cout << ... << (cout << ' ', b));cout << '\n';}
+template<class... T> void input(T&... a){(cin >> ... >> a);}
+void print(){cout << '\n';}
+typedef long long ll;
+typedef long double ld;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> T;
+// typedef modint1000000007 mint;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    ll n,m; input(n,m);
+    vector<string> s(n);
+    rep(i,n)input(s[i]);
+
+    vector<ll> z(n);
+    iota(z.begin(),z.end(),0);
+    do{
+        bool f = true;
+        string now = s[z[0]],next = "";
+        for(ll i=1;i<n;i++){
+            next = s[z[i]];
+            ll cnt = 0;
+            rep(j,m){
+                if(now[j] != next[j])cnt++;
+            }
+            if(cnt > 1){
+                f = false;
+                break;
+            }
+            now = next;
+        }
+        if(f){
+            print("Yes");
+            return 0;
+        }
+    }while(next_permutation(z.begin(),z.end()));
+    print("No");
+    
+    return 0;
+}
