9ddf842852210f27ec4184e73e98de86c024d978
thirofoo <thirofoo@gmail.com>
2023-05-27 23:48:41 +0900

    [TLE] SubmissionID: 41779550 at 2023-05-27 23:48:41 (1685198921)

---
+++ abc303/abc303_c_41779550_TLE.cpp
@@ -0,0 +1,45 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc303/submissions/41779550
+ * Submitted at: 2023-05-27 23:48:41
+ * Problem URL: https://atcoder.jp/contests/abc303/tasks/abc303_c
+ * Result: TLE
+ * Execution Time: 2206 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    int n,m,h,k;
+    string s;
+    cin >> n >> m >> h >> k >> s;
+    int x = 0,y = 0;
+    vector<vector<int>> w(m,vector<int>(2));
+    for(int i=0;i<m;i++){
+        for(int j=0;j<2;j++){
+            cin >> w.at(i).at(j);
+        }
+    }
+    for(int i=0;i<n;i++){
+        if(s[i] == 'R')x++;
+        if(s[i] == 'L')x--;
+        if(s[i] == 'U')y++;
+        if(s[i] == 'D')y--;
+        h--;
+        if(h < 0)return cout << "No" << endl,0;
+        if(h < k){
+            for(int j=0;j<w.size();j++){
+                if(x == w[j][0] && y == w[j][1]){
+                    h = k;
+                    w.erase(w.begin()+j);
+                }
+            }
+        }
+    }
+    cout << "Yes" << endl;
+    
+    return 0;
+}
+
