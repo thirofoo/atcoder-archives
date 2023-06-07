a30d8377d93d214fed7c6759826e78f2b3f1271d
thirofoo <thirofoo@gmail.com>
2023-06-07 23:00:47 +0900

    [AC] SubmissionID: 42062481 at 2023-06-07 23:00:47 (1686146447)

---
+++ typical90/typical90_o_42062481_AC.cpp
@@ -0,0 +1,50 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/typical90/submissions/42062481
+ * Submitted at: 2023-06-07 23:00:47
+ * Problem URL: https://atcoder.jp/contests/typical90/tasks/typical90_o
+ * Result: AC
+ * Execution Time: 42 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+template<class T, class... Ts> void print(const T& a, const Ts&... b){cout << a;(cout << ... << (cout << ' ', b));cout << '\n';}
+template<class... T> void input(T&... a){(cin >> ... >> a);}
+void print(){cout << '\n';}
+typedef long long ll;
+typedef long double ld;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> T;
+typedef modint1000000007 mint;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    ll n; input(n);
+    vector<mint> fact(2*n+1,1);
+    vector<mint> r_fact(2*n+1,1);
+    for(int i=1;i<=2*n;i++){
+        fact[i] = fact[i-1]*i;
+        r_fact[i] = (mint)1/fact[i];
+    }
+    vector<mint> ans(n+1,0);
+    for(ll ball=1;ball<=n;ball++){
+        for(ll space=1;space<=n;space++){
+            ll min_num = ball + (ball-1)*(space-1);
+            // ここのfor文がballに応じて 1/2, 1/3 ... と調和級数の様に減っていく
+            // → 高々 O(logN) しかかからない
+            if(min_num > n)break;
+            ans[space] += fact[n-min_num+ball] * r_fact[n-min_num] * r_fact[ball];
+        }
+    }
+    rep(i,n)print(ans[i+1].val());
+    
+    return 0;
+}
