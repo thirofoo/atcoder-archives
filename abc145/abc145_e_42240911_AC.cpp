181fe92605885a176e5fe1e2a99064bfcb2133b8
thirofoo <thirofoo@gmail.com>
2023-06-13 22:39:55 +0900

    [AC] SubmissionID: 42240911 at 2023-06-13 22:39:55 (1686663595)

---
+++ abc145/abc145_e_42240911_AC.cpp
@@ -0,0 +1,52 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc145/submissions/42240911
+ * Submitted at: 2023-06-13 22:39:55
+ * Problem URL: https://atcoder.jp/contests/abc145/tasks/abc145_e
+ * Result: AC
+ * Execution Time: 126 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+template<class T, class... Ts> void print(const T& a, const Ts&... b){cout << a;(cout << ... << (cout << ' ', b));cout << '\n';}
+template<class... T> void input(T&... a){(cin >> ... >> a);}
+void print(){cout << '\n';}
+typedef long long ll;
+typedef long double ld;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> T;
+// typedef modint1000000007 mint;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    ll n,t; input(n,t);
+    vector<P> cook;
+    rep(i,n){
+        ll a,b; input(a,b);
+        cook.push_back(P(a,b));
+    }
+    sort(cook.begin(),cook.end());
+    vector<vector<ll>> dp(n+1,vector<ll>(6001,-1));
+    dp[0][0] = 0;
+    rep(i,n){
+        auto &&[a,b] = cook[i];
+        rep(j,6001){
+            if(dp[i][j] == -1)continue;
+            dp[i+1][j] = max(dp[i+1][j],dp[i][j]);
+            if(j < t)dp[i+1][j+a] = max(dp[i+1][j+a],dp[i][j]+b);
+        }
+    }
+    ll ans = 0;
+    rep(i,6001)ans = max(ans,dp[n][i]);
+    print(ans);
+    
+    return 0;
+}
