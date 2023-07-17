11a6012e0e48a58c3fcea2e8e4a55b7f9c5d955d
thirofoo <thirofoo@gmail.com>
2023-07-17 13:34:51 +0900

    [WA] SubmissionID: 43686469 at 2023-07-17 13:34:51 (1689568491)

---
+++ abc119/abc119_d_43686469_WA.cpp
@@ -0,0 +1,62 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc119/submissions/43686469
+ * Submitted at: 2023-07-17 13:34:51
+ * Problem URL: https://atcoder.jp/contests/abc119/tasks/abc119_d
+ * Result: WA
+ * Execution Time: 250 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+typedef long long ll;
+typedef pair<ll, ll> P;
+// typedef modint1000000007 mint;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    ll a,b,q; cin >> a >> b >> q;
+    vector<ll> s(a), t(b), x(q);
+    rep(i,a) cin >> s[i];
+    rep(i,b) cin >> t[i];
+    rep(i,q) {
+        cin >> x[i];
+        // 8通り全探索
+        // 1. 初動東西どちらか
+        // 2. 神社・寺どちらに先行くか
+        // 3. そのあと東西どちらか
+        ll ans = 1e16;
+        rep(j,2){
+            // j == 0 : 西 , j == 1 : 東
+            rep(k,2){
+                // k == 0 : 神社 , k == 1 : 寺
+                ll cand = 0, now = x[i];
+                auto itr1 = lower_bound(s.begin(), s.end(), now);
+                if( (itr1 == s.end() && j == 1) || (itr1 == s.begin() && j == 0) ) continue;
+                if( j == 0 ) itr1--;
+
+                cand += abs(now-(*itr1));
+                now = *itr1;
+
+                auto itr2 = lower_bound(t.begin(), t.end(), now);
+                ll mini = 1e16;
+                if( itr2 != t.end() ) mini = min(mini,abs(now-(*itr2)));
+                if( itr2 != t.begin() ) mini = min(mini,abs(now-(*(--itr2))));
+                cand += mini;
+                ans = min(ans,cand);
+
+                swap(s,t);
+            }
+        }
+        cout << ans << endl;
+    }
+    
+    return 0;
+}
