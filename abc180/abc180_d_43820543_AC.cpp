44ce82a7c5e39a9a3f30e65cb69569593f1827f5
thirofoo <thirofoo@gmail.com>
2023-07-22 16:40:42 +0900

    [AC] SubmissionID: 43820543 at 2023-07-22 16:40:42 (1690011642)

---
+++ abc180/abc180_d_43820543_AC.cpp
@@ -0,0 +1,35 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc180/submissions/43820543
+ * Submitted at: 2023-07-22 16:40:42
+ * Problem URL: https://atcoder.jp/contests/abc180/tasks/abc180_d
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<ll, ll>;
+// #include <atcoder/all>
+// using namespace atcoder;
+// using mint = modint1000000007;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+#include<boost/multiprecision/cpp_int.hpp>
+using namespace boost::multiprecision;
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    cpp_int x,y,a,b; cin >> x >> y >> a >> b;
+    cpp_int ans = 0;
+    while( x*a < y && x*a <= x+b ){
+        x *= a;
+        ans++;
+    }
+    ans += (y-1-x) / b;
+    cout << ans << endl;
+    
+    return 0;
+}
