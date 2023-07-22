058c37d281f3691bc8935e876e023f0fe59c704f
thirofoo <thirofoo@gmail.com>
2023-07-22 15:45:30 +0900

    [WA] SubmissionID: 43819159 at 2023-07-22 15:45:30 (1690008330)

---
+++ abc182/abc182_d_43819159_WA.cpp
@@ -0,0 +1,36 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc182/submissions/43819159
+ * Submitted at: 2023-07-22 15:45:30
+ * Problem URL: https://atcoder.jp/contests/abc182/tasks/abc182_d
+ * Result: WA
+ * Execution Time: 34 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<ll, ll>;
+// #include <atcoder/all>
+// using namespace atcoder;
+// using mint = modint1000000007;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    ll n; cin >> n;
+    vector<ll> a(n+1);
+    ll ans = 0, now = 0, max_d = -1e16;
+    rep(i,n) {
+        cin >> a[i+1];
+        a[i+1] += a[i];
+        ans = max(ans, now + max_d);
+        now += a[i+1];
+        max_d = max(max_d, a[i+1]);
+    }
+    cout << ans << endl;
+    
+    return 0;
+}
