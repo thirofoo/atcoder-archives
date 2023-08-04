e76b5dd0bed7a8a78bf4f2f97777b1960aa4e80b
thirofoo <thirofoo@gmail.com>
2023-08-04 15:56:10 +0900

    [WA] SubmissionID: 44225523 at 2023-08-04 15:56:10 (1691132170)

---
+++ abc173/abc173_e_44225523_WA.cpp
@@ -0,0 +1,72 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc173/submissions/44225523
+ * Submitted at: 2023-08-04 15:56:10
+ * Problem URL: https://atcoder.jp/contests/abc173/tasks/abc173_e
+ * Result: WA
+ * Execution Time: 46 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<ll, ll>;
+#include <atcoder/all>
+using namespace atcoder;
+using mint = modint1000000007;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    ll n,k; cin >> n >> k;
+    vector<ll> a(n);
+    rep(i,n) cin >> a[i];
+    sort(a.begin(), a.end(), [](ll e1, ll e2){
+        return abs(e1) > abs(e2);
+    });
+    mint ans = 1;
+    ll minus_cnt = 0, m1 = 1e16, m2 = 1e16, m3 = -1, m4 = -1;
+    bool all_minus = true;
+    rep(i,n) {
+        if( i < k ) {
+            ans *= a[i];
+            minus_cnt += (a[i] < 0);
+            if( a[i] < 0 ) m1 = min(m1,abs(a[i]));
+            else m2 = min(m2,abs(a[i]));
+        }
+        else {
+            if( a[i] < 0 ) m3 = max(m3,abs(a[i]));
+            else m4 = max(m4,abs(a[i]));
+        }
+        all_minus &= (a[i] < 0);
+    }
+    if( minus_cnt%2 == 0 || k == n ) return cout << ans.val() << endl, 0;
+    if( all_minus ) {
+        ans = 1;
+        sort( a.begin(), a.end() );
+        reverse( a.begin(), a.end() );
+        rep(i,k) ans *= a[i];
+        return cout << ans.val() << endl, 0;
+    }
+
+    // ここから↓は、正負ともに1個以上の時
+    if( m3 == -1 || m4*m2 >= m3*m1 ) {
+        if( m1 == 0 ) ans = 0;
+        else {
+            ans *= m4;
+            ans /= m1;
+        }
+    }
+    else {
+        if( m2 == 0 ) ans = 0;
+        else {
+            ans *= m3;
+            ans /= m2;
+        }
+    }
+    cout << ans.val() << endl;
+    
+    return 0;
+}
