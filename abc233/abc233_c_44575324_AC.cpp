71d9302fed91e37f3d3fd90e74dc3e1f70b2e4cb
thirofoo <thirofoo@gmail.com>
2023-08-14 09:58:28 +0900

    [AC] SubmissionID: 44575324 at 2023-08-14 09:58:28 (1691974708)

---
+++ abc233/abc233_c_44575324_AC.cpp
@@ -0,0 +1,48 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc233/submissions/44575324
+ * Submitted at: 2023-08-14 09:58:28
+ * Problem URL: https://atcoder.jp/contests/abc233/tasks/abc233_c
+ * Result: AC
+ * Execution Time: 12 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<ll, ll>;
+using T = tuple<ll, ll, ll>;
+// #include <atcoder/all>
+// using namespace atcoder;
+// using mint = modint1000000007;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    ll n,x; cin >> n >> x;
+    vector<vector<ll>> a;
+    rep(i,n) {
+        ll l; cin >> l;
+        vector<ll> tmp(l);
+        rep(j,l) cin >> tmp[j];
+        a.push_back(tmp);
+    }
+    map<__int128_t,ll> memo1;
+    rep(i,a[0].size()) memo1[a[0][i]]++;
+    for(ll i=1;i<n;i++) {
+        map<__int128_t,ll> memo2;
+        for(auto [key,value]:memo1) {
+            rep(j,a[i].size()) {
+                if( key*a[i][j] > (__int128_t)x ) continue;
+                memo2[key*a[i][j]] += value;
+            }
+        }
+        swap(memo1,memo2);
+    }
+    // for(auto [key,value]:memo1) cout << key << " : " << value << endl;
+    cout << memo1[x] << endl;
+    
+    return 0;
+}
