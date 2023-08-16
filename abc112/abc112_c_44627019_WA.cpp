87953a229f4a532746108437c621a297e1e1065d
thirofoo <thirofoo@gmail.com>
2023-08-16 09:52:53 +0900

    [WA] SubmissionID: 44627019 at 2023-08-16 09:52:53 (1692147173)

---
+++ abc112/abc112_c_44627019_WA.cpp
@@ -0,0 +1,9 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc112/submissions/44627019
+ * Submitted at: 2023-08-16 09:52:53
+ * Problem URL: https://atcoder.jp/contests/abc112/tasks/abc112_c
+ * Result: WA
+ * Execution Time: 16 ms
+ */
+
