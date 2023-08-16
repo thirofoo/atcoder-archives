813c2651e5e66951b1b7464e48d7d5d3f2593b0b
thirofoo <thirofoo@gmail.com>
2023-08-16 09:10:37 +0900

    [AC] SubmissionID: 44626485 at 2023-08-16 09:10:37 (1692144637)

---
+++ abc236/abc236_a_44626485_AC.cpp
@@ -0,0 +1,9 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc236/submissions/44626485
+ * Submitted at: 2023-08-16 09:10:37
+ * Problem URL: https://atcoder.jp/contests/abc236/tasks/abc236_a
+ * Result: AC
+ * Execution Time: 5 ms
+ */
+
