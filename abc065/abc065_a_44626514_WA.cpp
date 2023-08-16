aa317342e480972bd65d351cc3246166d55c615b
thirofoo <thirofoo@gmail.com>
2023-08-16 09:13:18 +0900

    [WA] SubmissionID: 44626514 at 2023-08-16 09:13:18 (1692144798)

---
+++ abc065/abc065_a_44626514_WA.cpp
@@ -0,0 +1,9 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc065/submissions/44626514
+ * Submitted at: 2023-08-16 09:13:18
+ * Problem URL: https://atcoder.jp/contests/abc065/tasks/abc065_a
+ * Result: WA
+ * Execution Time: 6 ms
+ */
+
