1500b707ad44a30d6344833a7ee735c832c7f363
thirofoo <thirofoo@gmail.com>
2023-08-16 09:14:13 +0900

    [AC] SubmissionID: 44626521 at 2023-08-16 09:14:13 (1692144853)

---
+++ abc065/abc065_a_44626521_AC.cpp
@@ -0,0 +1,9 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc065/submissions/44626521
+ * Submitted at: 2023-08-16 09:14:13
+ * Problem URL: https://atcoder.jp/contests/abc065/tasks/abc065_a
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
