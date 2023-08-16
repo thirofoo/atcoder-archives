33b48ffdf8aa679e90b08605272e2f0cd608d0e0
thirofoo <thirofoo@gmail.com>
2023-08-16 11:29:54 +0900

    [AC] SubmissionID: 44629118 at 2023-08-16 11:29:54 (1692152994)

---
+++ abc046/arc062_a_44629118_AC.cpp
@@ -0,0 +1,9 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc046/submissions/44629118
+ * Submitted at: 2023-08-16 11:29:54
+ * Problem URL: https://atcoder.jp/contests/abc046/tasks/arc062_a
+ * Result: AC
+ * Execution Time: 7 ms
+ */
+
