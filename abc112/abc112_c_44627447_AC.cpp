3849219706c999e034447f88147edcccec98f77c
thirofoo <thirofoo@gmail.com>
2023-08-16 10:16:21 +0900

    [AC] SubmissionID: 44627447 at 2023-08-16 10:16:21 (1692148581)

---
+++ abc112/abc112_c_44627447_AC.cpp
@@ -0,0 +1,9 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc112/submissions/44627447
+ * Submitted at: 2023-08-16 10:16:21
+ * Problem URL: https://atcoder.jp/contests/abc112/tasks/abc112_c
+ * Result: AC
+ * Execution Time: 15 ms
+ */
+
