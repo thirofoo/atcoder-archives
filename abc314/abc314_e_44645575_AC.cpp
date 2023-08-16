9fa4e699a58453685a30a777df7d0467b02738ab
thirofoo <thirofoo@gmail.com>
2023-08-16 23:20:11 +0900

    [AC] SubmissionID: 44645575 at 2023-08-16 23:20:11 (1692195611)

---
+++ abc314/abc314_e_44645575_AC.cpp
@@ -0,0 +1,9 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc314/submissions/44645575
+ * Submitted at: 2023-08-16 23:20:11
+ * Problem URL: https://atcoder.jp/contests/abc314/tasks/abc314_e
+ * Result: AC
+ * Execution Time: 2 ms
+ */
+
