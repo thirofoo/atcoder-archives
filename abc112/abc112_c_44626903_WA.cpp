b3b2caf10c784c9c2372ae356cc3371ce2ddb124
thirofoo <thirofoo@gmail.com>
2023-08-16 09:44:40 +0900

    [WA] SubmissionID: 44626903 at 2023-08-16 09:44:40 (1692146680)

---
+++ abc112/abc112_c_44626903_WA.cpp
@@ -0,0 +1,9 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc112/submissions/44626903
+ * Submitted at: 2023-08-16 09:44:40
+ * Problem URL: https://atcoder.jp/contests/abc112/tasks/abc112_c
+ * Result: WA
+ * Execution Time: 15 ms
+ */
+
