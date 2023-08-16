5655a3a7fb64d0b1b00fcf5d1f4bbb5c099470b4
thirofoo <thirofoo@gmail.com>
2023-08-16 09:32:41 +0900

    [AC] SubmissionID: 44626746 at 2023-08-16 09:32:41 (1692145961)

---
+++ abc254/abc254_c_44626746_AC.cpp
@@ -0,0 +1,9 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc254/submissions/44626746
+ * Submitted at: 2023-08-16 09:32:41
+ * Problem URL: https://atcoder.jp/contests/abc254/tasks/abc254_c
+ * Result: AC
+ * Execution Time: 58 ms
+ */
+
