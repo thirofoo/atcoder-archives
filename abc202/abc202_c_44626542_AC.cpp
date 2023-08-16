72f707f9edbf552458b2612506aaaa68feacfc85
thirofoo <thirofoo@gmail.com>
2023-08-16 09:16:36 +0900

    [AC] SubmissionID: 44626542 at 2023-08-16 09:16:36 (1692144996)

---
+++ abc202/abc202_c_44626542_AC.cpp
@@ -0,0 +1,9 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc202/submissions/44626542
+ * Submitted at: 2023-08-16 09:16:36
+ * Problem URL: https://atcoder.jp/contests/abc202/tasks/abc202_c
+ * Result: AC
+ * Execution Time: 35 ms
+ */
+
