3696993a71d3dbdadb826c42b18cd3cfa755e880
thirofoo <thirofoo@gmail.com>
2023-08-16 09:21:19 +0900

    [AC] SubmissionID: 44626600 at 2023-08-16 09:21:19 (1692145279)

---
+++ abc183/abc183_c_44626600_AC.cpp
@@ -0,0 +1,9 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc183/submissions/44626600
+ * Submitted at: 2023-08-16 09:21:19
+ * Problem URL: https://atcoder.jp/contests/abc183/tasks/abc183_c
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
