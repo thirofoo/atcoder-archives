8dd2b3017d409fce01745899d0693b9642d00e00
thirofoo <thirofoo@gmail.com>
2023-08-17 10:04:54 +0900

    [AC] SubmissionID: 44651402 at 2023-08-17 10:04:54 (1692234294)

---
+++ abc299/abc299_d_44651402_AC.cpp
@@ -0,0 +1,9 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc299/submissions/44651402
+ * Submitted at: 2023-08-17 10:04:54
+ * Problem URL: https://atcoder.jp/contests/abc299/tasks/abc299_d
+ * Result: AC
+ * Execution Time: 18 ms
+ */
+
