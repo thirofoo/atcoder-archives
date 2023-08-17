05aef3636ced84acc1a1b997fe4022a62a52189f
thirofoo <thirofoo@gmail.com>
2023-08-17 10:18:21 +0900

    [WA] SubmissionID: 44651602 at 2023-08-17 10:18:21 (1692235101)

---
+++ abc277/abc277_e_44651602_WA.cpp
@@ -0,0 +1,9 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc277/submissions/44651602
+ * Submitted at: 2023-08-17 10:18:21
+ * Problem URL: https://atcoder.jp/contests/abc277/tasks/abc277_e
+ * Result: WA
+ * Execution Time: 1318 ms
+ */
+
