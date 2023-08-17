cf77ec0b1c577051fcd78d350507e0bfea7061ed
thirofoo <thirofoo@gmail.com>
2023-08-17 09:58:03 +0900

    [AC] SubmissionID: 44651278 at 2023-08-17 09:58:03 (1692233883)

---
+++ abc198/abc198_c_44651278_AC.cpp
@@ -0,0 +1,9 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc198/submissions/44651278
+ * Submitted at: 2023-08-17 09:58:03
+ * Problem URL: https://atcoder.jp/contests/abc198/tasks/abc198_c
+ * Result: AC
+ * Execution Time: 7 ms
+ */
+
