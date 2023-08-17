797368427e54dd04558ce09e69fc6ccde783d1f3
thirofoo <thirofoo@gmail.com>
2023-08-17 10:01:51 +0900

    [AC] SubmissionID: 44651348 at 2023-08-17 10:01:51 (1692234111)

---
+++ abc094/arc095_a_44651348_AC.cpp
@@ -0,0 +1,9 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc094/submissions/44651348
+ * Submitted at: 2023-08-17 10:01:51
+ * Problem URL: https://atcoder.jp/contests/abc094/tasks/arc095_a
+ * Result: AC
+ * Execution Time: 318 ms
+ */
+
