129d5163037db3d2d5f6e5c2cd8a2343c831ec73
thirofoo <thirofoo@gmail.com>
2023-08-17 09:54:58 +0900

    [AC] SubmissionID: 44651221 at 2023-08-17 09:54:58 (1692233698)

---
+++ abc055/abc055_b_44651221_AC.cpp
@@ -0,0 +1,9 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc055/submissions/44651221
+ * Submitted at: 2023-08-17 09:54:58
+ * Problem URL: https://atcoder.jp/contests/abc055/tasks/abc055_b
+ * Result: AC
+ * Execution Time: 8 ms
+ */
+
