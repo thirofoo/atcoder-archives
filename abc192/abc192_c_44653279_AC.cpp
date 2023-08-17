3c62cfa9e012a653b6d6583bd6481d7be7e34e6a
thirofoo <thirofoo@gmail.com>
2023-08-17 11:46:30 +0900

    [AC] SubmissionID: 44653279 at 2023-08-17 11:46:30 (1692240390)

---
+++ abc192/abc192_c_44653279_AC.cpp
@@ -0,0 +1,9 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc192/submissions/44653279
+ * Submitted at: 2023-08-17 11:46:30
+ * Problem URL: https://atcoder.jp/contests/abc192/tasks/abc192_c
+ * Result: AC
+ * Execution Time: 55 ms
+ */
+
