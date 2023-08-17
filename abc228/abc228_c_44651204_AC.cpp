256ff2de612bd23ed24c2584d3b07a6d0676b7ef
thirofoo <thirofoo@gmail.com>
2023-08-17 09:54:05 +0900

    [AC] SubmissionID: 44651204 at 2023-08-17 09:54:05 (1692233645)

---
+++ abc228/abc228_c_44651204_AC.cpp
@@ -0,0 +1,9 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc228/submissions/44651204
+ * Submitted at: 2023-08-17 09:54:05
+ * Problem URL: https://atcoder.jp/contests/abc228/tasks/abc228_c
+ * Result: AC
+ * Execution Time: 176 ms
+ */
+
