a0a3467be99c2cb9dec0a3bb12d31c05cdc52d1d
thirofoo <thirofoo@gmail.com>
2023-08-17 09:57:42 +0900

    [WA] SubmissionID: 44651275 at 2023-08-17 09:57:42 (1692233862)

---
+++ abc198/abc198_c_44651275_WA.cpp
@@ -0,0 +1,9 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc198/submissions/44651275
+ * Submitted at: 2023-08-17 09:57:42
+ * Problem URL: https://atcoder.jp/contests/abc198/tasks/abc198_c
+ * Result: WA
+ * Execution Time: 8 ms
+ */
+
