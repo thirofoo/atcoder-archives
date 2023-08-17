14c903692929a1ef2ac5638b134ab0d7d7ab1ca5
thirofoo <thirofoo@gmail.com>
2023-08-17 10:21:18 +0900

    [AC] SubmissionID: 44651636 at 2023-08-17 10:21:18 (1692235278)

---
+++ abc277/abc277_e_44651636_AC.cpp
@@ -0,0 +1,9 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc277/submissions/44651636
+ * Submitted at: 2023-08-17 10:21:18
+ * Problem URL: https://atcoder.jp/contests/abc277/tasks/abc277_e
+ * Result: AC
+ * Execution Time: 1292 ms
+ */
+
