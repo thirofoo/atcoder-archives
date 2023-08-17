f188da85a3aaeced2de914def801b7ffa8dc15d4
thirofoo <thirofoo@gmail.com>
2023-08-17 09:31:11 +0900

    [AC] SubmissionID: 44650861 at 2023-08-17 09:31:11 (1692232271)

---
+++ abc160/abc160_b_44650861_AC.cpp
@@ -0,0 +1,9 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc160/submissions/44650861
+ * Submitted at: 2023-08-17 09:31:11
+ * Problem URL: https://atcoder.jp/contests/abc160/tasks/abc160_b
+ * Result: AC
+ * Execution Time: 9 ms
+ */
+
