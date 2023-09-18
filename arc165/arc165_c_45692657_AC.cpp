cc0a4bd96079554fd304d50564870329578d281f
thirofoo <thirofoo@gmail.com>
2023-09-18 09:25:47 +0900

    [AC] SubmissionID: 45692657 at 2023-09-18 09:25:47 (1694996747)

---
+++ arc165/arc165_c_45692657_AC.cpp
@@ -0,0 +1,81 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/arc165/submissions/45692657
+ * Submitted at: 2023-09-18 09:25:47
+ * Problem URL: https://atcoder.jp/contests/arc165/tasks/arc165_c
+ * Result: AC
+ * Execution Time: 1941 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<ll, ll>;
+using T = tuple<ll, ll, ll>;
+#include <atcoder/all>
+using namespace atcoder;
+// using mint = modint1000000007;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    ll n, m; cin >> n >> m;
+    vector<vector<P>> Graph(n);
+    rep(i,m) {
+        ll a, b, w; cin >> a >> b >> w;
+        a--; b--;
+        Graph[a].emplace_back(P(b,w));
+        Graph[b].emplace_back(P(a,w));
+    }
+    ll min_2edge = 1e16;
+    rep(i,n) {
+        if( Graph[i].size() <= 1 ) continue;
+        sort(Graph[i].begin(), Graph[i].end(), [](P p1, P p2) {
+            return p1.second < p2.second;
+        });
+        min_2edge = min(min_2edge, Graph[i][0].second + Graph[i][1].second);
+    }
+
+    ll left = 0, right = min_2edge+1;
+    while( right-left > 1 ) {
+        ll mid = (right+left)/2;
+        // cout << left << " " << right << endl;
+
+        bool f = true;
+        vector<vector<ll>> Graph2(n);
+        rep(i,n) {
+            for(auto [j,w]:Graph[i]) {
+                if( w < mid ) {
+                    Graph2[i].emplace_back(j);
+                    Graph2[j].emplace_back(i);
+                }
+            };
+        }
+
+        vector<ll> color(n,-1);
+        auto dfs = [&](auto self, ll now, ll pre) {
+            if( pre == -1 ) color[now] = 0;
+            else color[now] = 1 - color[pre];
+
+            for(auto to:Graph2[now]) {
+                if( !f ) return;
+                if( color[to] == color[now] ) {
+                    f = false;
+                    return;
+                }
+                if( color[to] != -1 ) continue;
+                self(self, to, now);
+            }
+        };
+        rep(i,n) if(f && color[i] == -1) dfs(dfs, i, -1);
+
+        if( f ) left = mid;
+        else right = mid;
+    }
+    cout << left << endl;
+    
+    return 0;
+}
+
