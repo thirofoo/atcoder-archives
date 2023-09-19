deb44a64e9d423b93636fc92720287bf33a4880a
thirofoo <thirofoo@gmail.com>
2023-09-20 02:21:05 +0900

    [AC] SubmissionID: 45737185 at 2023-09-20 02:21:05 (1695144065)

---
+++ abc320/abc320_e_45737185_AC.cpp
@@ -0,0 +1,48 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc320/submissions/45737185
+ * Submitted at: 2023-09-20 02:21:05
+ * Problem URL: https://atcoder.jp/contests/abc320/tasks/abc320_e
+ * Result: AC
+ * Execution Time: 290 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<ll, ll>;
+using T = tuple<ll, ll, ll>;
+// #include <atcoder/all>
+// using namespace atcoder;
+// using mint = modint1000000007;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    ll n, m; cin >> n >> m;
+    set<ll> st;
+    rep(i,n) st.insert(i);
+    priority_queue<P,vector<P>,greater<P>> todo;
+    vector<ll> ans(n,0);
+    rep(i,m) {
+        ll t, w, s; cin >> t >> w >> s;
+        while( !todo.empty() ) {
+            auto [tt,idx] = todo.top();
+            if( tt <= t ) {
+                st.insert(idx);
+                todo.pop();
+            }
+            else break;
+        }
+        if( !st.empty() ) {
+            ans[(*st.begin())] += w;
+            todo.push(P(t+s,(*st.begin())) );
+            st.erase(st.begin());
+        }
+    }
+    rep(i,n) cout << ans[i] << endl;
+    
+    return 0;
+}
