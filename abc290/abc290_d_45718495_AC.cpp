6eccef10f76c49dc957a277903f31c380590fd1c
thirofoo <thirofoo@gmail.com>
2023-09-19 10:50:50 +0900

    [AC] SubmissionID: 45718495 at 2023-09-19 10:50:50 (1695088250)

---
+++ abc290/abc290_d_45718495_AC.cpp
@@ -0,0 +1,50 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc290/submissions/45718495
+ * Submitted at: 2023-09-19 10:50:50
+ * Problem URL: https://atcoder.jp/contests/abc290/tasks/abc290_d
+ * Result: AC
+ * Execution Time: 134 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<ll, ll>;
+using T = tuple<ll, ll, ll>;
+// #include <atcoder/all>
+// using namespace atcoder;
+// using mint = modint1000000007;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+inline long long GCD(long long a, long long b){
+    // 負 or a == 0 で -1
+    if( a <= 0 || b < 0 ) return -1;
+    if( b != 0 ) return GCD(b,a%b);
+    return a;
+}
+
+inline long long LCM(long long a, long long b){
+    // 負 or overflow の時は -1
+    if( a < 0 || b < 0 ) return -1;
+    if( (a / GCD(a,b)) >= 1e18 / b ) return -1;
+    return a  / GCD(a,b) * b;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    ll t; cin >> t;
+    while( t-- ) {
+        ll n, d, k; cin >> n >> d >> k;
+        ll gcd = GCD(n,d);
+        if( gcd == 1 ) cout << ((k-1)*d)%n << endl;
+        else {
+            // 2周以上回る周期の時もあった…。。
+            cout << (k-1)/(n/gcd) + (((k-1)%(n/gcd))*d)%n << endl;
+        }
+    }
+    
+    return 0;
+}
