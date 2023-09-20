b1ceab8d2ddc5f48ec83702885898d62b4238b38
thirofoo <thirofoo@gmail.com>
2023-09-20 09:46:24 +0900

    [TLE] SubmissionID: 45740292 at 2023-09-20 09:46:24 (1695170784)

---
+++ abc227/abc227_c_45740292_TLE.cpp
@@ -0,0 +1,34 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc227/submissions/45740292
+ * Submitted at: 2023-09-20 09:46:24
+ * Problem URL: https://atcoder.jp/contests/abc227/tasks/abc227_c
+ * Result: TLE
+ * Execution Time: 2210 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<ll, ll>;
+using T = tuple<ll, ll, ll>;
+// #include <atcoder/all>
+// using namespace atcoder;
+// using mint = modint1000000007;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    ll n; cin >> n;
+    ll ans = 0;
+    for(ll a=1;a<=cbrt(n)+5;a++) {
+        for(ll b=a;b<=sqrt(n)+5;b++) {
+            ans += max(n/(a*b) - b + 1, 0ll);
+        }
+    }
+    cout << ans << endl;
+    
+    return 0;
+}
