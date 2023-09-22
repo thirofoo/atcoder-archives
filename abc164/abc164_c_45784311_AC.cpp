d31068c35b97f825bb1409f0a146d2b63e8b3ff1
thirofoo <thirofoo@gmail.com>
2023-09-22 10:39:59 +0900

    [AC] SubmissionID: 45784311 at 2023-09-22 10:39:59 (1695346799)

---
+++ abc164/abc164_c_45784311_AC.cpp
@@ -0,0 +1,27 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc164/submissions/45784311
+ * Submitted at: 2023-09-22 10:39:59
+ * Problem URL: https://atcoder.jp/contests/abc164/tasks/abc164_c
+ * Result: AC
+ * Execution Time: 55 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    int n; cin >> n;
+    unordered_map<string,int> mp;
+    string s;
+    for(int i=0;i<n;i++) {
+        cin >> s;
+        mp[s]++;
+    }
+    cout << mp.size() << endl;
+    
+    return 0;
+}
