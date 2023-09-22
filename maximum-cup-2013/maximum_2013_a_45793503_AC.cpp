df2e925ee72ec604ef3cadc0a2acfc6b51994106
thirofoo <thirofoo@gmail.com>
2023-09-22 19:54:09 +0900

    [AC] SubmissionID: 45793503 at 2023-09-22 19:54:09 (1695380049)

---
+++ maximum-cup-2013/maximum_2013_a_45793503_AC.cpp
@@ -0,0 +1,61 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/maximum-cup-2013/submissions/45793503
+ * Submitted at: 2023-09-22 19:54:09
+ * Problem URL: https://atcoder.jp/contests/maximum-cup-2013/tasks/maximum_2013_a
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int g[15][15];
+int dp[1 << 15][15];
+int lj[15], lk[15];
+int res = 1e9, ji, ki;
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    int n; cin >> n;
+    if( n == 2 ) {
+        cout << "0 0" << '\n';
+        return 0;
+    }
+
+    for (int i=0; i<n-1; i++) {
+        for (int j=i+1; j<n; j++) {
+            cin >> g[i][j];
+            g[j][i] = g[i][j];
+        }
+    }
+    int N = (1 << n), K;
+    for (int i=1; i<N; i++) for (int j=0; j<n; j++) dp[i][j] = 1e9;
+
+    // bitDP
+    dp[1][0] = 0;
+    for (uint i=1; i<N; i++) {
+        // 遷移先候補を絞って計算量down
+        // bitが立ってる　　 ⇒ jの候補
+        // bitが立っていない ⇒ kの候補
+        ji = 0, ki = 0;
+        for (uint j=0; j<n; j++) {
+            if( i & 1 << j ) lj[ji++] = j;
+            else lk[ki++] = j;
+        }
+
+        for (uint j=lj[0], i1=1; i1<=ji; j=lj[i1++]) {
+            for (uint k=lk[0], i2=1; i2<=ki; k=lk[i2++]) {
+                if( i & 1 << k ) continue;
+                K = i | 1 << k;
+                dp[K][k] = min(dp[K][k], dp[i][j] + g[j][k]);
+            }
+        }
+    }
+    for(int i=0;i<n;i++) res = min(res, dp[N-1][i] + g[i][0]);
+    cout << n << " " << res << '\n';
+
+    return 0;
+}
