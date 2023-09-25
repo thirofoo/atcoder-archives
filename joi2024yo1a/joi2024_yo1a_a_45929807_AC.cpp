57e4d7dbb5197599083e581abaaa064695905621
thirofoo <thirofoo@gmail.com>
2023-09-25 09:11:24 +0900

    [AC] SubmissionID: 45929807 at 2023-09-25 09:11:24 (1695600684)

---
+++ joi2024yo1a/joi2024_yo1a_a_45929807_AC.cpp
@@ -0,0 +1,28 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/joi2024yo1a/submissions/45929807
+ * Submitted at: 2023-09-25 09:11:24
+ * Problem URL: https://atcoder.jp/contests/joi2024yo1a/tasks/joi2024_yo1a_a
+ * Result: AC
+ * Execution Time: 1 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<ll, ll>;
+using T = tuple<ll, ll, ll>;
+// #include <atcoder/all>
+// using namespace atcoder;
+// using mint = modint1000000007;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    ll x, y; cin >> x >> y;
+    cout << x + y + 3 << endl;
+    
+    return 0;
+}
