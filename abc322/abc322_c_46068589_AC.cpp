f15441f42029b1f46efb7e517ddcec121b0bd393
thirofoo <thirofoo@gmail.com>
2023-09-30 21:07:52 +0900

    [AC] SubmissionID: 46068589 at 2023-09-30 21:07:52 (1696075672)

---
+++ abc322/abc322_c_46068589_AC.cpp
@@ -0,0 +1,33 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc322/submissions/46068589
+ * Submitted at: 2023-09-30 21:07:52
+ * Problem URL: https://atcoder.jp/contests/abc322/tasks/abc322_c
+ * Result: AC
+ * Execution Time: 193 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<ll, ll>;
+using T = tuple<ll, ll, ll>;
+// #include <atcoder/all>
+// using namespace atcoder;
+// using mint = modint1000000007;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    ll n, m; cin >> n >> m;
+    vector<ll> a(m);
+    rep(i,m) cin >> a[i];
+    for(ll i=1;i<=n;i++) {
+        auto itr = lower_bound(a.begin(), a.end(), i);
+        cout << (*itr) - i << endl;
+    }
+    
+    return 0;
+}
