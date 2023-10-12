60254c4c32261cbe0c32b068e62e303f374f4307
thirofoo <thirofoo@gmail.com>
2023-10-12 18:46:52 +0900

    [RE] SubmissionID: 46473850 at 2023-10-12 18:46:52 (1697104012)

---
+++ abc289/abc289_d_46473850_RE.cpp
@@ -0,0 +1,47 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc289/submissions/46473850
+ * Submitted at: 2023-10-12 18:46:52
+ * Problem URL: https://atcoder.jp/contests/abc289/tasks/abc289_d
+ * Result: RE
+ * Execution Time: 96 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<ll, ll>;
+using T = tuple<ll, ll, ll>;
+// #include <atcoder/all>
+// using namespace atcoder;
+// using mint = modint1000000007;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    ll n, m, x; cin >> n;
+    vector<ll> a(n);
+    rep(i,n) cin >> a[i];
+    cin >> m;
+    set<ll> b;
+    rep(i,m) {
+        ll tb; cin >> tb;
+        b.insert(tb);
+    }
+    cin >> x;
+
+    vector<bool> dp(100005,false);
+    dp[0] = true;
+    rep(i,100005) {
+        if( !dp[i] ) continue;
+        rep(j,n) {
+            if( b.count(i+a[j]) ) continue;
+            dp[i+a[j]] = true;
+        }
+    }
+    cout << ( dp[x] ? "Yes" : "No" ) << endl;
+    
+    return 0;
+}
