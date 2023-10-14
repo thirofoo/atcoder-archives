3122d926461a9a7e0e093c2f736b19f6410dada1
thirofoo <thirofoo@gmail.com>
2023-10-14 18:04:20 +0900

    [WA] SubmissionID: 46515939 at 2023-10-14 18:04:20 (1697274260)

---
+++ ttpc2023/ttpc2023_c_46515939_WA.cpp
@@ -0,0 +1,38 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/ttpc2023/submissions/46515939
+ * Submitted at: 2023-10-14 18:04:20
+ * Problem URL: https://atcoder.jp/contests/ttpc2023/tasks/ttpc2023_c
+ * Result: WA
+ * Execution Time: 106 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = unsigned long long;
+using P = pair<ll, ll>;
+using T = tuple<ll, ll, ll>;
+// #include <atcoder/all>
+// using namespace atcoder;
+// using mint = modint1000000007;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    ll t; cin >> t;
+    auto f2 = [&](ll n) {
+        return n*n + 2;
+    };
+
+    while( t-- ) {
+        ll n; cin >> n;
+        ll rest = n-(ll)sqrt(n)*(ll)sqrt(n), r = (ll)sqrt(n);
+        ll ans = min((ll)sqrt(n-1)*(ll)sqrt(n-1), f2(r-1) + (rest-1)*2);
+        // cerr << (ll)(sqrt(n-1)-1)*(ll)(sqrt(n-1)-1) << " " << f2(r-1) + rest*2 << endl;
+        cout << ans << endl;
+    }
+    
+    return 0;
+}
