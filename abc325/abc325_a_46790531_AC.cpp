38a9c354b8c22283b797c179b7e47e14bd876dc8
thirofoo <thirofoo@gmail.com>
2023-10-21 21:00:46 +0900

    [AC] SubmissionID: 46790531 at 2023-10-21 21:00:46 (1697889646)

---
+++ abc325/abc325_a_46790531_AC.cpp
@@ -0,0 +1,28 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc325/submissions/46790531
+ * Submitted at: 2023-10-21 21:00:46
+ * Problem URL: https://atcoder.jp/contests/abc325/tasks/abc325_a
+ * Result: AC
+ * Execution Time: 1 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<ll, ll>;
+using T = tuple<ll, ll, ll>;
+// #include <atcoder/all>
+// using namespace atcoder;
+// using mint = modint1000000007;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    string s, t; cin >> s >> t;
+    cout << s << " san" << endl;
+    
+    return 0;
+}
