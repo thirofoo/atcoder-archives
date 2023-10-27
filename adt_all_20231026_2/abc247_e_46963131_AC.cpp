83cf3701bb2137471d5f51c1429b0d46503dffa8
thirofoo <thirofoo@gmail.com>
2023-10-27 16:46:43 +0900

    [AC] SubmissionID: 46963131 at 2023-10-27 16:46:43 (1698392803)

---
+++ adt_all_20231026_2/abc247_e_46963131_AC.cpp
@@ -0,0 +1,41 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/adt_all_20231026_2/submissions/46963131
+ * Submitted at: 2023-10-27 16:46:43
+ * Problem URL: https://atcoder.jp/contests/adt_all_20231026_2/tasks/abc247_e
+ * Result: AC
+ * Execution Time: 12 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<ll, ll>;
+using T = tuple<ll, ll, ll>;
+// #include <atcoder/all>
+// using namespace atcoder;
+// using mint = modint1000000007;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    // 解説AC 2個目 (天才解法) 
+    // a[i] < y || x < a[i] が成り立つ部分で数列を区切ってシミュ
+    ll n, x, y; cin >> n >> x >> y;
+    vector<ll> a(n);
+    rep(i,n) cin >> a[i];
+    ll ans = 0, sx = -1, sy = -1, b = -1;
+    rep(i,n) {
+        if( a[i] < y || x < a[i] ) sx = -1, sy = -1, b = i;
+        else {
+            if( a[i] == x ) sx = i;
+            if( a[i] == y ) sy = i;
+        }
+        if( sx != -1 && sy != -1 ) ans += min(sx, sy) - b;
+    }
+    cout << ans << endl;
+    
+    return 0;
+}
