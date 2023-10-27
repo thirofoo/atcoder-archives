b8a9d0a08592f8dd507a2a234feef9d721927bf3
thirofoo <thirofoo@gmail.com>
2023-10-27 16:16:17 +0900

    [AC] SubmissionID: 46962429 at 2023-10-27 16:16:17 (1698390977)

---
+++ adt_all_20231026_2/abc247_e_46962429_AC.cpp
@@ -0,0 +1,51 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/adt_all_20231026_2/submissions/46962429
+ * Submitted at: 2023-10-27 16:16:17
+ * Problem URL: https://atcoder.jp/contests/adt_all_20231026_2/tasks/abc247_e
+ * Result: AC
+ * Execution Time: 113 ms
+ */
+
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<ll, ll>;
+using T = tuple<ll, ll, ll>;
+// #include <atcoder/all>
+// using namespace atcoder;
+// using mint = modint1000000007;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    ll n, x, y; cin >> n >> x >> y;
+    vector<ll> a(n);
+    rep(i,n) cin >> a[i];
+    multiset<ll> st;
+    ll l = 0, r = 0, ans = 0, b = 0;
+    queue<ll> sx, sy;
+    while(l < n){
+        // cerr << l << " " << r << " " << ans << endl << flush;
+        if(r == n || ( !st.empty() && ((*st.begin()) < y || x < (*st.rbegin())) ) ){
+            st.erase(st.find(a[l]));
+            while( !sx.empty() && sx.front() <= l ) sx.pop();
+            while( !sy.empty() && sy.front() <= l ) sy.pop();
+            if( !sx.empty() && !sy.empty() ) ans += b - max(sx.front(), sy.front());
+            ++l;
+        }
+        else{
+            if( a[r] == x ) sx.push(r);
+            if( a[r] == y ) sy.push(r);
+            st.insert(a[r]);
+            if( !st.empty() && y == (*st.begin()) && (*st.rbegin()) == x ) ans++, b = r+1;
+            ++r;
+        }
+    }
+    cout << ans << endl;
+    
+    return 0;
+}
