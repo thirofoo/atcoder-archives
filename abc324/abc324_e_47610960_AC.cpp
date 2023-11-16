9c28fa5dd3117909f37928b7d7ce27b62c48d8e7
thirofoo <thirofoo@gmail.com>
2023-11-16 14:05:52 +0900

    [AC] SubmissionID: 47610960 at 2023-11-16 14:05:52 (1700111152)

---
+++ abc324/abc324_e_47610960_AC.cpp
@@ -0,0 +1,46 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc324/submissions/47610960
+ * Submitted at: 2023-11-16 14:05:52
+ * Problem URL: https://atcoder.jp/contests/abc324/tasks/abc324_e
+ * Result: AC
+ * Execution Time: 49 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<ll, ll>;
+using T = tuple<ll, ll, ll>;
+// #include <atcoder/all>
+// using namespace atcoder;
+// using mint = modint1000000007;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    ll n; string t; cin >> n >> t;
+    vector<string> s(n);
+    vector<ll> l(n,0), r(n,0);
+    rep(i,n) {
+        cin >> s[i];
+        rep(j,s[i].size()) {
+            if( l[i] >= t.size() ) break;
+            if( s[i][j] == t[l[i]] ) l[i]++;
+        }
+        for(ll j=s[i].size()-1; j>=0; j--) {
+            if( r[i] >= t.size() ) break;
+            if( s[i][j] == t[t.size()-1-r[i]] ) r[i]++;
+        }
+    }
+    // 左端全探索
+    ll ans = 0;
+    sort(l.begin(), l.end());
+    sort(r.begin(), r.end());
+    rep(i,n) ans += r.end() - lower_bound(r.begin(),r.end(),t.size()-l[i]);
+    cout << ans << endl;
+    
+    return 0;
+}
