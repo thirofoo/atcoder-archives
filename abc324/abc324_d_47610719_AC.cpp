5fbc3c5f4a664c36d0f1763e6c62edf48c4e92ad
thirofoo <thirofoo@gmail.com>
2023-11-16 13:49:12 +0900

    [AC] SubmissionID: 47610719 at 2023-11-16 13:49:12 (1700110152)

---
+++ abc324/abc324_d_47610719_AC.cpp
@@ -0,0 +1,40 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc324/submissions/47610719
+ * Submitted at: 2023-11-16 13:49:12
+ * Problem URL: https://atcoder.jp/contests/abc324/tasks/abc324_d
+ * Result: AC
+ * Execution Time: 96 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<ll, ll>;
+using T = tuple<ll, ll, ll>;
+// #include <atcoder/all>
+// using namespace atcoder;
+// using mint = modint1000000007;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    ll n; string s; cin >> n >> s;
+    vector<ll> cnt(10,0), cnt2;
+    rep(i,n) cnt[s[i]-'0']++;
+    ll ans = 0;
+    for(ll i=0; i*i<=1e14; i++) {
+        string tmp = to_string(i*i);
+        if( tmp.size() > n ) break;
+        cnt2.assign(10,0);
+        rep(j,tmp.size()) cnt2[tmp[j]-'0']++;
+        cnt2[0] += n-tmp.size();
+        // if( cnt == cnt2 ) cerr << tmp << endl;
+        ans += (cnt == cnt2);
+    }
+    cout << ans << endl;
+    
+    return 0;
+}
