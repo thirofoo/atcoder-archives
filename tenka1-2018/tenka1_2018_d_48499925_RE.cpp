41af4a1aa96e6a7f5353caaca45277258568057a
thirofoo <thirofoo@gmail.com>
2023-12-15 09:41:27 +0900

    [RE] SubmissionID: 48499925 at 2023-12-15 09:41:27 (1702600887)

---
+++ tenka1-2018/tenka1_2018_d_48499925_RE.cpp
@@ -0,0 +1,47 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/tenka1-2018/submissions/48499925
+ * Submitted at: 2023-12-15 09:41:27
+ * Problem URL: https://atcoder.jp/contests/tenka1-2018/tasks/tenka1_2018_d
+ * Result: RE
+ * Execution Time: 90 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<ll, ll>;
+using T = tuple<ll, ll, ll>;
+// #include <atcoder/all>
+// using namespace atcoder;
+// using mint = modint1000000007;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    ll n; cin >> n;
+    for(ll i=1; i*(i+1)/2<=n; i++) {
+        if( i*(i+1)/2 == n ) {
+            cout << "Yes\n";
+            cout << n << '\n';
+            ll cnt = 1;
+            vector<vector<ll>> edge(n,vector<ll>{});
+            rep(j,i+1) for(ll k=j+1; k<i+1; k++){
+                edge[j].emplace_back(cnt);
+                edge[k].emplace_back(cnt);
+                cnt++;
+            }
+            for(auto v:edge) {
+                cout << v.size() << " ";
+                for(auto ele:v) cout << ele << " ";
+                cout << '\n';
+            }
+            return 0;
+        }
+    }
+    cout << "No\n";
+    
+    return 0;
+}
