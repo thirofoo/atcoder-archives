df79a417ec79bb53007a1195e65a977975ea64c7
thirofoo <thirofoo@gmail.com>
2023-12-16 21:00:22 +0900

    [AC] SubmissionID: 48532317 at 2023-12-16 21:00:22 (1702728022)

---
+++ abc333/abc333_a_48532317_AC.cpp
@@ -0,0 +1,29 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc333/submissions/48532317
+ * Submitted at: 2023-12-16 21:00:22
+ * Problem URL: https://atcoder.jp/contests/abc333/tasks/abc333_a
+ * Result: AC
+ * Execution Time: 1 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<ll, ll>;
+using T = tuple<ll, ll, ll>;
+// #include <atcoder/all>
+// using namespace atcoder;
+// using mint = modint1000000007;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    ll n; cin >> n;
+    rep(i,n) cout << n;
+    cout << endl;
+    
+    return 0;
+}
