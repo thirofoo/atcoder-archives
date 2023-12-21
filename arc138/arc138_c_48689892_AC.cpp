6ee2bdfd72cb4a73e187ab94626ff0561a5c6b4d
thirofoo <thirofoo@gmail.com>
2023-12-21 16:15:52 +0900

    [AC] SubmissionID: 48689892 at 2023-12-21 16:15:52 (1703142952)

---
+++ arc138/arc138_c_48689892_AC.cpp
@@ -0,0 +1,50 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/arc138/submissions/48689892
+ * Submitted at: 2023-12-21 16:15:52
+ * Problem URL: https://atcoder.jp/contests/arc138/tasks/arc138_c
+ * Result: AC
+ * Execution Time: 32 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<ll, ll>;
+using T = tuple<ll, ll, ll>;
+// #include <atcoder/all>
+// using namespace atcoder;
+// using mint = modint1000000007;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    ll n; cin >> n;
+    vector<ll> v(n), b(n+1,1);
+    vector<P> a;
+    rep(i,n) {
+        cin >> v[i];
+        a.emplace_back(P(v[i],i+1));
+    }
+    sort(a.begin(), a.end());
+    b[0] = 0;
+    ll ans = 0;
+    for(ll i=n/2; i<n; i++) {
+        auto [ele,idx] = a[i];
+        b[idx] = -1;
+        ans += ele;
+    }
+    ll mini = 1e16, mini_op = 0;
+    rep(i,n) {
+        b[i+1] += b[i];
+        if( mini > b[i+1] ) {
+            mini = b[i+1];
+            mini_op = i+1;
+        }
+    }
+    cout << mini_op%n << " " << ans << endl;
+    
+    return 0;
+}
