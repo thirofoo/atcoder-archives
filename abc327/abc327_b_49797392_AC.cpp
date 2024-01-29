58e48791ad29f35fa3201482cecac14f596b9578
thirofoo <thirofoo@gmail.com>
2024-01-30 02:14:12 +0900

    [AC] SubmissionID: 49797392 at 2024-01-30 02:14:12 (1706548452)

---
+++ abc327/abc327_b_49797392_AC.cpp
@@ -0,0 +1,36 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc327/submissions/49797392
+ * Submitted at: 2024-01-30 02:14:12
+ * Problem URL: https://atcoder.jp/contests/abc327/tasks/abc327_b
+ * Result: AC
+ * Execution Time: 1 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<ll, ll>;
+using T = tuple<ll, ll, ll>;
+// #include <atcoder/all>
+// using namespace atcoder;
+// using mint = modint1000000007;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+
+    ll b; cin >> b;
+    for(ll a=1; a<=18; a++) {
+        ll tmp = 1;
+        rep(i,a) tmp *= a;
+        if( tmp == b ) {
+            cout << a << endl;
+            return 0;
+        }
+    }
+    cout << -1 << endl;
+    
+    return 0;
+}
