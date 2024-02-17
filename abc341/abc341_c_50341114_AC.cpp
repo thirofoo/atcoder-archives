85fad3e9f84595002d5fdd47cb02a06552a91c3a
thirofoo <thirofoo@gmail.com>
2024-02-17 21:08:48 +0900

    [AC] SubmissionID: 50341114 at 2024-02-17 21:08:48 (1708171728)

---
+++ abc341/abc341_c_50341114_AC.cpp
@@ -0,0 +1,54 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc341/submissions/50341114
+ * Submitted at: 2024-02-17 21:08:48
+ * Problem URL: https://atcoder.jp/contests/abc341/tasks/abc341_c
+ * Result: AC
+ * Execution Time: 465 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<ll, ll>;
+using T = tuple<ll, ll, ll>;
+#include <atcoder/all>
+using namespace atcoder;
+// using mint = modint998244353;
+// using mint = modint1000000007;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+#define reps(i, l, r) for(ll i = l; i < r; i++)
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    ll h, w, n; cin >> h >> w >> n;
+    string t; cin >> t;
+    vector<string> s(h);
+    map<char, P> mp;
+    mp['L'] = P(0, -1);
+    mp['R'] = P(0, 1);
+    mp['U'] = P(-1, 0);
+    mp['D'] = P(1, 0);
+    rep(i, h) cin >> s[i];
+    ll ans = 0;
+    for(ll i=1; i<h-1; i++) for(ll j=1; j<w-1; j++) {
+        if( s[i][j] == '#' ) continue;
+        ll x = i, y = j;
+        bool flag = true;
+        rep(k, n) {
+            auto [dx, dy] = mp[t[k]];
+            ll nx = x+dx, ny = y+dy;
+            if( s[nx][ny] == '#' ) {
+                flag = false;
+                break;
+            }
+            x = nx, y = ny;
+        }
+        if( flag ) ans++;
+    }
+    cout << ans << endl;
+    
+    return 0;
+}
