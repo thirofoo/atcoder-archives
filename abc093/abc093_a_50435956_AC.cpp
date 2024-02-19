f8b45b2ede7b18f63b7c3f4e808e294ee752ab7a
thirofoo <thirofoo@gmail.com>
2024-02-19 09:02:54 +0900

    [AC] SubmissionID: 50435956 at 2024-02-19 09:02:54 (1708300974)

---
+++ abc093/abc093_a_50435956_AC.cpp
@@ -0,0 +1,33 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc093/submissions/50435956
+ * Submitted at: 2024-02-19 09:02:54
+ * Problem URL: https://atcoder.jp/contests/abc093/tasks/abc093_a
+ * Result: AC
+ * Execution Time: 1 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<ll, ll>;
+using T = tuple<ll, ll, ll>;
+#include <atcoder/all>
+using namespace atcoder;
+// using mint = modint998244353;
+// using mint = modint1000000007;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+#define reps(i, l, r) for(ll i = l; i < r; i++)
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    string s; cin >> s;
+    set<char> st;
+    for(auto c : s) st.insert(c);
+    if( st.size() == 3 ) cout << "Yes" << endl;
+    else cout << "No" << endl;
+    
+    return 0;
+}
