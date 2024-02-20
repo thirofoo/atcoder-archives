af4352cffadf1a8c60fa175a081cb0b0992407dd
thirofoo <thirofoo@gmail.com>
2024-02-20 09:57:16 +0900

    [AC] SubmissionID: 50460869 at 2024-02-20 09:57:16 (1708390636)

---
+++ abc235/abc235_d_50460869_AC.cpp
@@ -0,0 +1,55 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc235/submissions/50460869
+ * Submitted at: 2024-02-20 09:57:16
+ * Problem URL: https://atcoder.jp/contests/abc235/tasks/abc235_d
+ * Result: AC
+ * Execution Time: 36 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<ll, ll>;
+using T = tuple<ll, ll, ll>;
+#include <atcoder/all>
+using namespace atcoder;
+// using mint = modint998244353;
+// using mint = modint1000000007;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+#define reps(i, l, r) for(ll i = l; i < r; i++)
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    ll a, n; cin >> a >> n;
+    vector<bool> vis(10000005, false);
+    vector<ll> num(10000005, 1e9);
+    num[1] = 0;
+    
+    auto dfs = [&](auto dfs, ll x) -> void {
+        if( vis[x] ) return;
+        vis[x] = true;
+        ll nx = x/a;
+        if( x%a == 0 ) {
+            dfs(dfs, nx);
+            num[x] = min(num[x], num[nx]+1);
+        }
+        if( x < 10 ) return;
+
+        // 上から 2 桁目が 0 の時は x
+        ll digit = 0, tx = x;
+        while( tx ) digit++, tx /= 10;
+        if( (x/(ll)pow(10, digit-2))%10 == 0 ) return;
+        nx = (x%(ll)pow(10, digit-1))*10 + x/pow(10, digit-1);
+        dfs(dfs, nx);
+        num[x] = min(num[x], num[nx]+1);
+
+        return;
+    };
+    dfs(dfs, n);
+    cout << (num[n] != 1e9 ? num[n] : -1) << endl;
+    
+    return 0;
+}
