8bca06975dbcea7477b9e26d1025d1511cb1affb
thirofoo <thirofoo@gmail.com>
2024-02-23 09:03:00 +0900

    [AC] SubmissionID: 50523775 at 2024-02-23 09:03:00 (1708646580)

---
+++ abc028/abc028_b_50523775_AC.cpp
@@ -0,0 +1,32 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc028/submissions/50523775
+ * Submitted at: 2024-02-23 09:03:00
+ * Problem URL: https://atcoder.jp/contests/abc028/tasks/abc028_b
+ * Result: AC
+ * Execution Time: 1 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<ll, ll>;
+using T = tuple<ll, ll, ll>;
+#include <atcoder/all>
+using namespace atcoder;
+// using mint = modint998244353;
+// using mint = modint1000000007;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+#define reps(i, l, r) for(ll i = l; i < r; i++)
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    string s; cin >> s;
+    vector<ll> cnt(6,0);
+    rep(i,s.size()) cnt[s[i]-'A']++;
+    rep(i,6) cout << cnt[i] << (i == 5 ? "\n" : " ");
+    
+    return 0;
+}
