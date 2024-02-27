6509dcd25e0652629758481f4e80df299c193acc
thirofoo <thirofoo@gmail.com>
2024-02-27 10:30:41 +0900

    [WA] SubmissionID: 50671698 at 2024-02-27 10:30:41 (1708997441)

---
+++ abc042/abc042_b_50671698_WA.cpp
@@ -0,0 +1,44 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc042/submissions/50671698
+ * Submitted at: 2024-02-27 10:30:41
+ * Problem URL: https://atcoder.jp/contests/abc042/tasks/abc042_b
+ * Result: WA
+ * Execution Time: 1 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using P = pair<ll, ll>;
+using T = tuple<ll, ll, ll>;
+#include <atcoder/all>
+using namespace atcoder;
+// using mint = modint998244353;
+// using mint = modint1000000007;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+#define reps(i, l, r) for(ll i = l; i < r; i++)
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    ll n, l; cin >> n >> l;
+    vector<string> s(n);
+    rep(i,n) {
+        cin >> s[i];
+        sort(s[i].begin(), s[i].end());
+    }
+    sort(s.begin(), s.end(), [&](string &a, string &b) {
+        rep(i,l) {
+            if( a[i] < b[i] ) return true;
+            if( a[i] > b[i] ) return false;
+        }
+        return false;
+    });
+    rep(i,n) cout << s[i];
+    cout << endl;
+    
+    return 0;
+}
