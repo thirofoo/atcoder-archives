02fee799c65daf197c7a41fee6f0a9348a96fe72
thirofoo <thirofoo@gmail.com>
2024-02-29 20:07:55 +0900

    [AC] SubmissionID: 50723825 at 2024-02-29 20:07:55 (1709204875)

---
+++ ahc002/ahc002_a_50723825_AC.cpp
@@ -0,0 +1,326 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/ahc002/submissions/50723825
+ * Submitted at: 2024-02-29 20:07:55
+ * Problem URL: https://atcoder.jp/contests/ahc002/tasks/ahc002_a
+ * Result: AC
+ * Execution Time: 1805 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> P;
+typedef tuple<int, int, int, int> T;
+#define rep(i, n) for(int i = 0; i < n; i++)
+#define reps(i, l, r) for(ll i = l; i < r; i++)
+
+namespace utility {
+    struct timer {
+        chrono::system_clock::time_point start;
+        // 開始時間を記録
+        void CodeStart() {
+            start = chrono::system_clock::now();
+        }
+        // 経過時間 (ms) を返す
+        double elapsed() const {
+        using namespace std::chrono;
+            return (double)duration_cast<milliseconds>(system_clock::now() - start).count();
+        }
+    } mytm;
+}
+
+inline unsigned int rand_int() {
+    static unsigned int tx = 123456789, ty=362436069, tz=521288629, tw=88675123;
+    unsigned int tt = (tx^(tx<<11));
+    tx = ty; ty = tz; tz = tw;
+    return ( tw=(tw^(tw>>19))^(tt^(tt>>8)) );
+}
+
+inline double rand_double() {
+    return (double)(rand_int()%(int)1e9)/1e9;
+}
+
+inline double gaussian(double mean, double stddev) {
+    // 標準正規分布からの乱数生成（Box-Muller変換
+    double z0 = sqrt(-2.0 * log(rand_double())) * cos(2.0 * M_PI * rand_double());
+    // 平均と標準偏差の変換
+    return mean + z0 * stddev;
+}
+
+//温度関数
+#define TIME_LIMIT 1800
+inline double temp(double start) {
+    double start_temp = 100,end_temp = 1;
+    return start_temp + (end_temp-start_temp)*((utility::mytm.elapsed()-start)/TIME_LIMIT);
+}
+
+//焼きなましの採用確率
+inline double prob(int best,int now,int start) {
+    return exp((double)(now - best) / temp(start));
+}
+
+//-----------------以下から実装部分-----------------//
+
+const int N = 50;
+
+// right | down | left | up
+#define DIR_NUM 4
+const vector<int> dx = { 0, 1, 0,-1};
+const vector<int> dy = { 1, 0,-1, 0};
+const vector<char> dir_to_char = {'R', 'D', 'L', 'U'};
+map<char, P> char_to_delta = {{'R', { 0, 1}}, {'D', { 1, 0}}, {'L', { 0,-1}}, {'U', {-1, 0}}};
+map<P, char> delta_to_char = {{{ 0, 1}, 'R'}, {{ 1, 0}, 'D'}, {{ 0,-1}, 'L'}, {{-1, 0}, 'U'}};
+
+struct Solver{
+    int sx, sy;
+    vector<vector<int>> tile, point;
+    vector<vector<bool>> used;
+    vector<vector<P>> group;
+
+    int ans_score;
+    string ans_str;
+    vector<P> ans_point, now_point;
+
+    // 解に多様性を持たせる parameter
+    int back_cnt;
+
+    // query の時に保存しておく系変数
+    vector<P> pre_route;
+    string best_str;
+
+    Solver() {
+        this->input();
+        used.resize(N, vector<bool>(N, false));
+        now_point.assign({});
+    }
+
+    void input() {
+        cin >> sx >> sy;
+        tile.resize(N, vector<int>(N));
+        point.resize(N, vector<int>(N));
+        group.resize(N*N, {});
+        rep(i,N) rep(j,N) {
+            cin >> tile[i][j];
+            group[tile[i][j]].emplace_back(P(i,j));
+        }
+        rep(i,N) rep(j,N) cin >> point[i][j];
+        return;
+    }
+
+    void output() {
+        ans_str = calcString(ans_point);
+        cout << ans_str << endl;
+        return;
+    }
+
+    void solve() {
+        // 1. dfs で貪欲初期解生成
+        // ※ (TIME_LIMIT / 5) ms までで初期解生成
+        utility::mytm.CodeStart();
+        ans_score = 0;
+        for(auto &&[i, j] : group[tile[sx][sy]]) used[i][j] = true;
+        firstDFS(sx, sy, 0, TIME_LIMIT / 5);
+
+        if( ans_point.size() <= 2 ) {
+            cerr << "Warn: 初期解小さすぎ" << endl;
+            return;
+        }
+
+        // used 更新
+        used.assign(N, vector<bool>(N, false));
+        for(auto &&[x,y]: ans_point) for(auto &&[i, j] : group[tile[x][y]]) used[i][j] = true;
+
+        // 2. 部分破壊 & 再構築 の山登り
+        // ※ 一旦計算量爆発しないように矩形範囲内で再構築
+        while( utility::mytm.elapsed() < TIME_LIMIT ) {
+            // 近傍 1. (l,r] の経路破壊 ⇒ dfs で再構築 ※ 経路長は 5 ~ 10
+            int l = rand_int() % ((int)ans_point.size()-1) + 1; // Start 以外
+            int r = min(l + rand_int()%5 + 5, (unsigned int)ans_point.size()-1); // End 以外
+
+            int now_score = calcScore(l,r);
+            int next_score = query1(l,r);
+            
+            // cerr << "Now: " << now_score << endl;
+            // cerr << "Next: " << next_score << endl << endl;
+
+            if( next_score > now_score ) {
+                // cerr << "ans update!\n";
+
+                // score 更新
+                ans_score -= now_score;
+                ans_score += next_score;
+                
+                // point 更新
+                vector<P> cand_point = {};
+                reps(i,0,l) cand_point.emplace_back(ans_point[i]);
+                for(auto &&[i, j] : best_point) cand_point.emplace_back(P(i,j));
+                reps(i,r+1,ans_point.size()) cand_point.emplace_back(ans_point[i]);
+                swap(ans_point, cand_point);
+
+                // output();
+
+                // used 更新
+                used.assign(N, vector<bool>(N, false));
+                for(auto &&[x,y]: ans_point) for(auto &&[i, j] : group[tile[x][y]]) used[i][j] = true;
+
+                pre_route.clear();
+            }
+            else rollback();
+        }
+        return;
+    }
+
+    inline void firstDFS(int x, int y, int score, const int &end_time) {
+        score += point[x][y];
+        now_point.emplace_back(P(x,y));
+
+        // dx, dy を壁に近い順に変更
+        vector<ll> perm(DIR_NUM); // 訪問順序
+        iota(perm.begin(), perm.end(), 0);
+        sort(perm.begin(), perm.end(), [&](int a, int b) {
+            int dis1 = abs(N/2 - (x+dx[a])) + abs(N/2 - (y+dy[a]));
+            int dis2 = abs(N/2 - (x+dx[b])) + abs(N/2 - (y+dy[b]));
+            return dis1 > dis2;
+        });
+
+        rep(d,DIR_NUM) {
+            int nx = x + dx[perm[d]], ny = y + dy[perm[d]];
+            if( outField(nx, ny, N, N) || used[nx][ny] ) continue;
+
+            for(auto &&[i, j] : group[tile[nx][ny]]) used[i][j] = true;
+
+            firstDFS(nx, ny, score, end_time);
+            if( utility::mytm.elapsed() > end_time ) return;
+
+            for(auto &&[i, j] : group[tile[nx][ny]]) used[i][j] = false;
+
+            if( back_cnt ) {
+                back_cnt--;
+                score -= point[x][y];
+                now_point.pop_back();
+                return;
+            }
+        }
+        if( score > ans_score ) {
+            // cerr << "score: " << score << endl;
+            ans_score = score;
+            ans_point = now_point;
+        }
+        
+        score -= point[x][y];
+        now_point.pop_back();
+
+        // 経路に多様性を持たせる ⇒ 行き止まりがある時に min(path.size() / 300) ターン逆流
+        back_cnt = now_point.size() / 300; // 人力パラメータ調整
+        return;
+    }
+
+    // ※ 0 ≦ left < right ≦ ans_str.size() 
+    inline int query1(int left, int right) {
+        // 1. (l,r] の経路破壊
+        reps(pi,left+1,right+1) {
+            auto &&[x, y] = ans_point[pi];
+            for(auto &&[i, j] : group[tile[x][y]]) used[i][j] = false;
+            pre_route.emplace_back(P(x,y));
+        }
+
+        // 2. (l,r] の経路再構築
+        best_score = 0;
+        now_point = {};
+        auto [tsx, tsy] = ans_point[left];
+        auto [gx, gy] = ans_point[right];
+        // ※ 矩形範囲内で再構築
+        min_x = max(0, min(tsx, gx) - 5);
+        min_y = max(0, min(tsy, gy) - 5);
+        max_x = min(N, max(tsx, gx) + 5);
+        max_y = min(N, max(tsy, gy) + 5);
+
+        // rep(i,N) {
+        //     rep(j,N) {
+        //         if( i == tsx && j == tsy ) cerr << "S";
+        //         else if( i == gx && j == gy ) cerr << "G";
+        //         else cerr << (used[i][j] ? "#" : ".");
+        //     }
+        //     cerr << endl;
+        // }
+        // cerr << endl;
+
+        query1DFS(tsx, tsy, best_score, gx, gy);
+        return best_score;
+    }
+
+    // query1DFS の時のみに使う変数
+    int best_score, min_x, min_y, max_x, max_y;
+    vector<P> best_point;
+
+    inline void query1DFS(int x, int y, int score, const int &gx, const int &gy) {
+        score += point[x][y];
+        now_point.emplace_back(P(x,y));
+
+        if( x == gx && y == gy && score > best_score ) {
+            best_score = score;
+            best_point = now_point;
+        }
+
+        rep(d,DIR_NUM) {
+            int nx = x + dx[d], ny = y + dy[d];
+            if( nx < min_x || max_x <= nx || ny < min_y || max_y <= ny ) continue;
+            if( used[nx][ny] ) continue;
+
+            for(auto &&[i, j] : group[tile[nx][ny]]) used[i][j] = true;
+
+            query1DFS(nx, ny, score, gx, gy);
+            if( utility::mytm.elapsed() > TIME_LIMIT ) return;
+
+            for(auto &&[i, j] : group[tile[nx][ny]]) used[i][j] = false;
+        }
+        score -= point[x][y];
+        now_point.pop_back();
+        return;
+    }
+
+    inline int calcScore(int left, int right) {
+        int res = 0;
+        reps(i,left,right+1) {
+            auto &&[x, y] = ans_point[i];
+            res += point[x][y];
+        }
+        return res;
+    }
+
+    inline string calcString(const vector<P> &point) {
+        string res = "";
+        reps(i,1,point.size()) {
+            auto &&[x1, y1] = point[i-1];
+            auto &&[x2, y2] = point[i];
+            res += delta_to_char[P(x2-x1, y2-y1)];
+        }
+        return res;
+    }
+
+    inline void rollback() {
+        while( !pre_route.empty() ) {
+            auto &&[x, y] = pre_route.back();
+            for(auto &&[i, j] : group[tile[x][y]]) used[i][j] = true;
+            pre_route.pop_back();
+        }
+        return;
+    }
+
+    inline bool outField(int x,int y,int h,int w) {
+        if(0 <= x && x < h && 0 <= y && y < w)return false;
+        return true;
+    }
+};
+
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    Solver solver;
+    solver.solve();
+    solver.output();
+    
+    return 0;
+}
