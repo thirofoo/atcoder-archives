bff1cfc56a1a7a54cb6fd2662eb7c34a52837e52
thirofoo <thirofoo@gmail.com>
2024-03-30 21:04:47 +0900

    [WA] SubmissionID: 51808247 at 2024-03-30 21:04:47 (1711800287)

---
+++ abc347/abc347_c_51808247_WA.cpp
@@ -0,0 +1,38 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc347/submissions/51808247
+ * Submitted at: 2024-03-30 21:04:47
+ * Problem URL: https://atcoder.jp/contests/abc347/tasks/abc347_c
+ * Result: WA
+ * Execution Time: 12 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using P = pair<ll, ll>;
+using T = tuple<ll, ll, ll>;
+#include <atcoder/all>
+using namespace atcoder;
+// using mint = modint998244353;
+// using mint = modint1000000007;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+#define reps(i, l, r) for(ll i = l; i < r; i++)
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    ll n, a, b; cin >> n >> a >> b;
+    vector<ll> d(n);
+    bool f = true;
+    rep(i,n) {
+        cin >> d[i]; d[i]--;
+        d[i] %= (a+b);
+        f &= (d[i] < a);
+    }
+    cout << ( f ? "Yes" : "No" ) << endl;
+    
+    return 0;
+}
