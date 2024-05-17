9b1dc6ce4047532ea2844177748018acbd2adc4b
thirofoo <thirofoo@gmail.com>
2024-05-17 22:16:19 +0900

    [AC] SubmissionID: 53554611 at 2024-05-17 22:16:19 (1715951779)

---
+++ abc161/abc161_a_53554611_AC.rs
@@ -0,0 +1,40 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc161/submissions/53554611
+ * Submitted at: 2024-05-17 22:16:19
+ * Problem URL: https://atcoder.jp/contests/abc161/tasks/abc161_a
+ * Result: AC
+ * Execution Time: 1 ms
+ */
+
+use proconio::input;
+
+fn main() {
+    // hoge = 2;
+    // for i in 0..20 {
+    //     // let fuga: i8 = 2;
+    //     let mut fuga: i8 = 2;
+    //     // fuga に 2 を i 回掛ける
+    //     for _ in 0..i {
+    //         fuga *= 2;
+    //     }
+    //     println!("Hello, world! {}", fuga);
+    // }
+
+    // println!("\"Fool,\" said I, \"you do not know\"");
+    // println!(r#""Fool," said I, "you do not know""#);
+    // println!("/\\\n\\/");
+    // println!(r"\\\\\\\\\\");
+
+    // let big_number;
+    // big_number = 2147483648_i64;
+    // println!("{}", big_number);
+
+    input! {
+        x: i32,
+        y: i32,
+        z: i32,
+    }
+    println!("{} {} {}", z, x, y);
+
+}
