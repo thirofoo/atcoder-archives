2bc755db9a42b5a706afd3707aff9eb2c88d88ff
thirofoo <thirofoo@gmail.com>
2024-06-06 14:21:08 +0900

    [AC] SubmissionID: 54267528 at 2024-06-06 14:21:08 (1717651268)

---
+++ abc214/abc214_d_54267528_AC.cpp
@@ -0,0 +1,44 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc214/submissions/54267528
+ * Submitted at: 2024-06-06 14:21:08
+ * Problem URL: https://atcoder.jp/contests/abc214/tasks/abc214_d
+ * Result: AC
+ * Execution Time: 25 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<ll, ll>;
+using T = tuple<ll, ll, ll>;
+#include <atcoder/all>
+using namespace atcoder;
+// using mint = modint998244353;
+// using mint = modint1000000007;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+#define reps(i, l, r) for(ll i = l; i < r; i++)
+const ll INF = ((1LL << 62)-(1LL << 31));
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    ll n; cin >> n;
+    vector<T> edge;
+    rep(i,n-1) {
+        ll u, v, w; cin >> u >> v >> w;
+        u--; v--;
+        edge.emplace_back(T(w, u, v));
+    }
+    dsu uf(n);
+    ll ans = 0;
+    sort(edge.begin(), edge.end());
+    for(auto [w,u,v]:edge) {
+        ans += uf.size(u) * uf.size(v) * w;
+        uf.merge(u, v);
+    }
+    cout << ans << '\n';
+    
+    return 0;
+}
