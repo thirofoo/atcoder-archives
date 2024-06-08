e024e70c372d5cb8d7f7375c05e4610e179441ae
thirofoo <thirofoo@gmail.com>
2024-06-08 21:20:22 +0900

    [AC] SubmissionID: 54346630 at 2024-06-08 21:20:22 (1717849222)

---
+++ abc357/abc357_c_54346630_AC.cpp
@@ -0,0 +1,70 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc357/submissions/54346630
+ * Submitted at: 2024-06-08 21:20:22
+ * Problem URL: https://atcoder.jp/contests/abc357/tasks/abc357_c
+ * Result: AC
+ * Execution Time: 4 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<ll, ll>;
+using T = tuple<ll, ll, ll>;
+#include <atcoder/all>
+using namespace atcoder;
+// using mint = modint998244353;
+// using mint = modint1000000007;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+#define reps(i, l, r) for(ll i = l; i < r; i++)
+const ll INF = ((1LL << 62)-(1LL << 31));
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    ll n; cin >> n;
+    if(n == 0) {
+        cout << "#" << endl;
+        return 0;
+    }
+    vector s(n+1, vector<string>{});
+    s[0] = {"#"};
+    s[1] = {"###", "#.#", "###"};
+    reps(i,2,n+1) {
+        ll size = s[i-1].size();
+        vector<string> tmp(3*size, "");
+        rep(j, 3*size) {
+            rep(k, size) {
+                tmp[j] += s[i-1][j%size][k];
+            }
+        }
+        rep(j, size) {
+            rep(k, size) {
+                tmp[j] += s[i-1][j%size][k];
+            }
+        }
+        rep(j, size) {
+            rep(k, size) {
+                tmp[j+size] += ".";
+            }
+        }
+        rep(j, size) {
+            rep(k, size) {
+                tmp[j+2*size] += s[i-1][j%size][k];
+            }
+        }
+        rep(j, 3*size) {
+            rep(k, size) {
+                tmp[j] += s[i-1][j%size][k];
+            }
+        }
+        s[i] = tmp;
+    }
+    rep(i, s[n].size()) {
+        cout << s[n][i] << endl;
+    }
+    
+    return 0;
+}
