240211b7633405312cfe1f6a323c35537798a554
thirofoo <thirofoo@gmail.com>
2024-06-09 16:35:24 +0900

    [WA] SubmissionID: 54400897 at 2024-06-09 16:35:24 (1717918524)

---
+++ abc253/abc253_e_54400897_WA.cpp
@@ -0,0 +1,39 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc253/submissions/54400897
+ * Submitted at: 2024-06-09 16:35:24
+ * Problem URL: https://atcoder.jp/contests/abc253/tasks/abc253_e
+ * Result: WA
+ * Execution Time: 20 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<ll, ll>;
+using T = tuple<ll, ll, ll>;
+#include <atcoder/all>
+using namespace atcoder;
+using mint = modint998244353;
+// using mint = modint1000000007;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+#define reps(i, l, r) for(ll i = l; i < r; i++)
+const ll INF = ((1LL << 62)-(1LL << 31));
+
+int main() {
+	cin.tie(nullptr);
+	ios_base::sync_with_stdio(false);
+	
+	ll n, m, k; cin >> n >> m >> k;
+	vector<vector<mint>> dp(n+1, vector<mint>(m+1, 0));
+	rep(i, m+1) dp[0][i] = 1;
+	reps(i, 1, n+1) {
+		reps(j, 1, m+1) {
+			dp[i][j] = dp[i-1].back() - (dp[i-1][min(j+k-1, m)] - dp[i-1][max(j-k, 0LL)]);
+			dp[i][j] += dp[i][j-1];
+		}
+	}
+	cout << dp[n][m].val() << endl;
+	
+	return 0;
+}
