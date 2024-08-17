fe471b10b5a16dbdc0c5fdbc42797ee2af908189
thirofoo <thirofoo@gmail.com>
2024-08-17 22:29:58 +0900

    [TLE] SubmissionID: 56828378 at 2024-08-17 22:29:58 (1723901398)

---
+++ abc367/abc367_f_56828378_TLE.cpp
@@ -0,0 +1,146 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc367/submissions/56828378
+ * Submitted at: 2024-08-17 22:29:58
+ * Problem URL: https://atcoder.jp/contests/abc367/tasks/abc367_f
+ * Result: TLE
+ * Execution Time: 2213 ms
+ */
+
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+// using mint = modint998244353;
+// using mint = modint1000000007;
+using ll  = long long;
+using ull = unsigned long long;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+constexpr ll INF = ((1LL << 61) + (1LL << 30) - 1);
+
+template<class S>
+struct Zobrist_hash_set {
+  Zobrist_hash_set(): v(0) {
+    mt.seed(rand());
+    rnd = uniform_int_distribution<ll>(-LLONG_MAX, LLONG_MAX);
+  }
+
+  void flip(const S &x) {
+    // x 初出現 → 新hash値を割当
+    if(!x_to_hash.count(x)) x_to_hash[x] = rnd(mt);
+    // 状態のhash値更新
+    v ^= x_to_hash[x];
+  }
+
+  // hash初期化
+  void init() {
+    v = 0;
+  }
+
+  // 現時点の状態hash値を返す．
+  ll get() {
+    return v;
+  }
+
+  ll v; // hash値
+
+  // 各 x ∈ X に対するハッシュの割当
+  unordered_map<S, ll> x_to_hash;
+  mt19937_64 mt;
+  uniform_int_distribution<ll> rnd;
+};
+
+int main() {
+  ll N, Q;
+  cin >> N >> Q;
+  vector<ll> A(N), B(N);
+  for(ll i = 0; i < N; i++) cin >> A[i];
+  for(ll i = 0; i < N; i++) cin >> B[i];
+
+  vector<pair<ll, ll>> queries1, queries2;
+  for(ll i = 0; i < Q; i++) {
+    ll li, ri, Li, Ri;
+    cin >> li >> ri >> Li >> Ri;
+    queries1.emplace_back(li - 1, ri);
+    queries2.emplace_back(Li - 1, Ri);
+  }
+
+  vector<ll> freqA(200001, 0), freqB(200001, 0);
+  Zobrist_hash_set<ll> z;
+  z.init();
+
+  auto addL = [&](ll value, vector<ll> &freq) {
+    z.flip(value * 200005 + freq[value]);
+    freq[value]--;
+    z.flip(value * 200005 + freq[value]);
+  };
+  auto delL = [&](ll value, vector<ll> &freq) {
+    z.flip(value * 200005 + freq[value]);
+    freq[value]++;
+    z.flip(value * 200005 + freq[value]);
+  };
+  auto addR = [&](ll value, vector<ll> &freq) {
+    z.flip(value * 200005 + freq[value]);
+    freq[value]++;
+    z.flip(value * 200005 + freq[value]);
+  };
+  auto delR = [&](ll value, vector<ll> &freq) {
+    z.flip(value * 200005 + freq[value]);
+    freq[value]--;
+    z.flip(value * 200005 + freq[value]);
+  };
+
+  vector<ll> q_idx1(queries1.size()), q_idx2(queries2.size());
+  long long sq = sqrt(N);
+  iota(q_idx1.begin(), q_idx1.end(), 0);
+  sort(q_idx1.begin(), q_idx1.end(), [&](ll &i1, ll &i2) {
+    auto &&[l1, r1] = queries1[i1];
+    auto &&[l2, r2] = queries1[i2];
+    if(l1 / sq != l2 / sq) return l1 < l2;
+    if(l1 / sq % 2 == 1) return r1 > r2;
+    return r1 < r2;
+  });
+  iota(q_idx2.begin(), q_idx2.end(), 0);
+  sort(q_idx2.begin(), q_idx2.end(), [&](ll &i1, ll &i2) {
+    auto &&[l1, r1] = queries2[i1];
+    auto &&[l2, r2] = queries2[i2];
+    if(l1 / sq != l2 / sq) return l1 < l2;
+    if(l1 / sq % 2 == 1) return r1 > r2;
+    return r1 < r2;
+  });
+
+  long long l = 0, r = 0;
+  vector<ll> ret1(queries1.size()), ret2(queries2.size());
+  for(int i = 0; i < queries1.size(); i++) {
+    auto &&[L, R] = queries1[q_idx1[i]];
+    while(r < R) addR(A[r++], freqA);
+    while(l > L) delL(A[--l], freqA);
+    while(l < L) addL(A[l++], freqA);
+    while(r > R) delR(A[--r], freqA);
+    // cerr << "L: " << L << " R: " << R << '\n';
+    // rep(j, 6) cerr << freqA[j] << ' ';
+    // cerr << '\n';
+    // cerr << "hash: " << z.get() << '\n';
+    ret1[q_idx1[i]] = z.get();
+  }
+  // cerr << '\n';
+
+  z.init();
+  l = 0, r = 0;
+  for(int i = 0; i < queries2.size(); i++) {
+    auto &&[L, R] = queries2[q_idx2[i]];
+    while(r < R) addR(B[r++], freqB);
+    while(l > L) delL(B[--l], freqB);
+    while(l < L) addL(B[l++], freqB);
+    while(r > R) delR(B[--r], freqB);
+    // cerr << "L: " << L << " R: " << R << '\n';
+    // rep(j, 6) cerr << freqB[j] << ' ';
+    // cerr << '\n';
+    // cerr << "hash: " << z.get() << '\n';
+    ret2[q_idx2[i]] = z.get();
+  }
+
+  for(int i = 0; i < Q; i++) cout << (ret1[i] == ret2[i] ? "Yes" : "No") << '\n';
+
+  return 0;
+}
