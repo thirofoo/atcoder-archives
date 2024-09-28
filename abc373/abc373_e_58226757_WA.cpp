aecdc3bac113170633a2338d4ff7faf7e080483b
thirofoo <thirofoo@gmail.com>
2024-09-28 22:06:58 +0900

    [WA] SubmissionID: 58226757 at 2024-09-28 22:06:58 (1727528818)

---
+++ abc373/abc373_e_58226757_WA.cpp
@@ -0,0 +1,55 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc373/submissions/58226757
+ * Submitted at: 2024-09-28 22:06:58
+ * Problem URL: https://atcoder.jp/contests/abc373/tasks/abc373_e
+ * Result: WA
+ * Execution Time: 75 ms
+ */
+
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+// using mint = modint998244353;
+// using mint = modint1000000007;
+using ll  = long long;
+using ull = unsigned long long;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+constexpr ll INF = ((1LL << 61) + (1LL << 30) - 1);
+
+int main() {
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+
+  ll n, m, k;
+  cin >> n >> m >> k;
+  vector<ll> a(n);
+  ll rest = k, max_a = -INF;
+  rep(i, n) {
+    cin >> a[i];
+    rest -= a[i];
+    max_a = max(max_a, a[i]);
+  }
+  vector<ll> ta = a;
+  sort(ta.begin(), ta.end());
+  ll top_m_sum = 0;
+  rep(i, m) top_m_sum += ta[n - 1 - i];
+  cerr << "rest: " << rest << endl;
+
+  rep(i, n) {
+    // 自分以外の上位 M 人の総和を求める
+    ll sum_m = top_m_sum + (ta[n - m] <= a[i] ? ta[n - m - 1] - a[i] : 0);
+    ll left = a[i] - 1, right = INF;
+    while(right - left > 1) {
+      ll mid    = (left + right) / 2;
+      ll border = (mid + 1) * m - sum_m;
+      if(border <= rest - (mid - a[i])) left = mid;
+      else right = mid;
+    }
+    cout << (right - a[i] > rest ? -1 : right - a[i]) << " ";
+  }
+  cout << endl;
+
+  return 0;
+}
