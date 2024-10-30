66ab6ecd8c898b5294302d8635d1c3033e817b15
thirofoo <thirofoo@gmail.com>
2024-10-30 18:04:59 +0900

    [AC] SubmissionID: 59273213 at 2024-10-30 18:04:59 (1730279099)

---
+++ abc376/abc376_a_59273213_AC.cpp
@@ -0,0 +1,39 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc376/submissions/59273213
+ * Submitted at: 2024-10-30 18:04:59
+ * Problem URL: https://atcoder.jp/contests/abc376/tasks/abc376_a
+ * Result: AC
+ * Execution Time: 6 ms
+ */
+
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+// using mint = modint998244353;
+// using mint = modint1000000007;
+using ll  = long long;
+using ull = unsigned long long;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+constexpr ll INF = ((1LL << 61) + (1LL << 30) - 1);
+
+int main() {
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+
+  ll n, c;
+  cin >> n >> c;
+  vector<ll> t(n);
+  rep(i, n) cin >> t[i];
+  ll ans = 1, pre = t[0];
+  rep(i, n - 1) {
+    if(t[i + 1] - pre >= c) {
+      pre = t[i + 1];
+      ans++;
+    }
+  }
+  cout << ans << endl;
+
+  return 0;
+}
