3c275c67818acdee9988df5ac7119a72df6200e2
thirofoo <thirofoo@gmail.com>
2024-12-07 21:02:07 +0900

    [AC] SubmissionID: 60504100 at 2024-12-07 21:02:07 (1733572927)

---
+++ abc383/abc383_a_60504100_AC.cpp
@@ -0,0 +1,38 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc383/submissions/60504100
+ * Submitted at: 2024-12-07 21:02:07
+ * Problem URL: https://atcoder.jp/contests/abc383/tasks/abc383_a
+ * Result: AC
+ * Execution Time: 1 ms
+ */
+
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+// using mint = modint998244353;
+// using mint = modint1000000007;
+using ll  = long long;
+using ull = unsigned long long;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+constexpr ll INF = ((1LL << 61) + (1LL << 30) - 1);
+
+int main() {
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+
+  ll n;
+  cin >> n;
+  ll ans = 0, pre_t = 0;
+  rep(i, n) {
+    ll t, v;
+    cin >> t >> v;
+    ans = max(0LL, ans - (t - pre_t));
+    ans += v;
+    pre_t = t;
+  }
+  cout << ans << '\n';
+
+  return 0;
+}
