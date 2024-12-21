2a3ec7e14839611ae0cd18def31c2c6d4b372d0e
thirofoo <thirofoo@gmail.com>
2024-12-21 21:09:56 +0900

    [WA] SubmissionID: 60950184 at 2024-12-21 21:09:56 (1734782996)

---
+++ abc385/abc385_c_60950184_WA.cpp
@@ -0,0 +1,49 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc385/submissions/60950184
+ * Submitted at: 2024-12-21 21:09:56
+ * Problem URL: https://atcoder.jp/contests/abc385/tasks/abc385_c
+ * Result: WA
+ * Execution Time: 8 ms
+ */
+
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+// using mint = modint998244353;
+// using mint = modint1000000007;
+using ll  = long long;
+using ull = unsigned long long;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+constexpr ll INF = ((1LL << 61) + (1LL << 30) - 1);
+
+int main() {
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+
+  ll n;
+  cin >> n;
+  vector<ll> h(n);
+  rep(i, n) cin >> h[i];
+
+  ll ans = 0;
+  for(ll dis = 1; dis < n; dis++) {
+    for(ll start = 0; start < dis; start++) {
+      ll now = start + dis, cnt = 1;
+      while(now < n) {
+        if(h[now - dis] == h[now]) {
+          cnt++;
+        } else {
+          ans = max(ans, cnt);
+          cnt = 1;
+        }
+        now += dis;
+      }
+      ans = max(ans, cnt);
+    }
+  }
+  cout << ans << '\n';
+
+  return 0;
+}
