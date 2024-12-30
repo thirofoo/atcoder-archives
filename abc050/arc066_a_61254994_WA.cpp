5e1347d6a05f1558f06a3ef22d20255f2b045b36
thirofoo <thirofoo@gmail.com>
2024-12-30 22:51:40 +0900

    [WA] SubmissionID: 61254994 at 2024-12-30 22:51:40 (1735566700)

---
+++ abc050/arc066_a_61254994_WA.cpp
@@ -0,0 +1,41 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc050/submissions/61254994
+ * Submitted at: 2024-12-30 22:51:40
+ * Problem URL: https://atcoder.jp/contests/abc050/tasks/arc066_a
+ * Result: WA
+ * Execution Time: 6 ms
+ */
+
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+// using mint = modint998244353;
+using mint = modint1000000007;
+using ll   = long long;
+using ull  = unsigned long long;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+constexpr ll INF = ((1LL << 61) + (1LL << 30) - 1);
+
+int main() {
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+
+  ll n;
+  cin >> n;
+  vector<ll> a(n), cnt(1e5 + 5, 0);
+  bool ng = false;
+  rep(i, n) {
+    cin >> a[i];
+    cnt[a[i]]++;
+    ng |= (cnt[a[i]] > 2);
+  }
+  if(ng) {
+    cout << 0 << '\n';
+    return 0;
+  }
+  cout << mint(2).pow(n / 2).val() << '\n';
+
+  return 0;
+}
