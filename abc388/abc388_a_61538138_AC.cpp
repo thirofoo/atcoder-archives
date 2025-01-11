0eb5e8a7f223a4eaaed71d416b3537f34e189f6e
thirofoo <thirofoo@gmail.com>
2025-01-11 21:00:41 +0900

    [AC] SubmissionID: 61538138 at 2025-01-11 21:00:41 (1736596841)

---
+++ abc388/abc388_a_61538138_AC.cpp
@@ -0,0 +1,30 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc388/submissions/61538138
+ * Submitted at: 2025-01-11 21:00:41
+ * Problem URL: https://atcoder.jp/contests/abc388/tasks/abc388_a
+ * Result: AC
+ * Execution Time: 3 ms
+ */
+
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+// using mint = modint998244353;
+// using mint = modint1000000007;
+using ll  = long long;
+using ull = unsigned long long;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+constexpr ll INF = ((1LL << 61) + (1LL << 30) - 1);
+
+int main() {
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+
+  string s;
+  cin >> s;
+  cout << s[0] << "UPC" << endl;
+
+  return 0;
+}
