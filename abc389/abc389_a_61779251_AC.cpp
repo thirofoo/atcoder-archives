650ad251b0cd4a474853ef48d4692450c8a706b9
thirofoo <thirofoo@gmail.com>
2025-01-18 21:00:47 +0900

    [AC] SubmissionID: 61779251 at 2025-01-18 21:00:47 (1737201647)

---
+++ abc389/abc389_a_61779251_AC.cpp
@@ -0,0 +1,30 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc389/submissions/61779251
+ * Submitted at: 2025-01-18 21:00:47
+ * Problem URL: https://atcoder.jp/contests/abc389/tasks/abc389_a
+ * Result: AC
+ * Execution Time: 1 ms
+ */
+
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+// using mint = modint998244353;
+// using mint = modint1000000007;
+using ll  = long long;
+using ull = unsigned long long;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+constexpr ll INF = ((1LL << 61) + (1LL << 30) - 1);
+
+int main() {
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+
+  string s;
+  cin >> s;
+  cout << (s[0] - '0') * (s[2] - '0') << '\n';
+
+  return 0;
+}
