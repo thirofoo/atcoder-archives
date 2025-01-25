50099dcd35e4222333039b047c25f8e55af427de
thirofoo <thirofoo@gmail.com>
2025-01-25 22:25:14 +0900

    [TLE] SubmissionID: 62090620 at 2025-01-25 22:25:14 (1737811514)

---
+++ abc390/abc390_d_62090620_TLE.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc390/submissions/62090620
+ * Submitted at: 2025-01-25 22:25:14
+ * Problem URL: https://atcoder.jp/contests/abc390/tasks/abc390_d
+ * Result: TLE
+ * Execution Time: 3319 ms
+ */
+
+// code not found
