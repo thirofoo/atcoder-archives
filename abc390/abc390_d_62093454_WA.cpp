5166733c3327750dc147a454b0424698d194d3a2
thirofoo <thirofoo@gmail.com>
2025-01-25 22:32:16 +0900

    [WA] SubmissionID: 62093454 at 2025-01-25 22:32:16 (1737811936)

---
+++ abc390/abc390_d_62093454_WA.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc390/submissions/62093454
+ * Submitted at: 2025-01-25 22:32:16
+ * Problem URL: https://atcoder.jp/contests/abc390/tasks/abc390_d
+ * Result: WA
+ * Execution Time: 3319 ms
+ */
+
+// code not found
