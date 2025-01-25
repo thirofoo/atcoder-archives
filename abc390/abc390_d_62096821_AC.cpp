59a26a35409a88fdec4c142972afe3130340c1f1
thirofoo <thirofoo@gmail.com>
2025-01-25 22:39:28 +0900

    [AC] SubmissionID: 62096821 at 2025-01-25 22:39:28 (1737812368)

---
+++ abc390/abc390_d_62096821_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc390/submissions/62096821
+ * Submitted at: 2025-01-25 22:39:28
+ * Problem URL: https://atcoder.jp/contests/abc390/tasks/abc390_d
+ * Result: AC
+ * Execution Time: 1777 ms
+ */
+
+// code not found
