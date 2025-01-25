12c64d637a4be71d9ec3b8f4cdf18245021e857c
thirofoo <thirofoo@gmail.com>
2025-01-25 21:48:19 +0900

    [RE] SubmissionID: 62073325 at 2025-01-25 21:48:19 (1737809299)

---
+++ abc390/abc390_e_62073325_RE.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc390/submissions/62073325
+ * Submitted at: 2025-01-25 21:48:19
+ * Problem URL: https://atcoder.jp/contests/abc390/tasks/abc390_e
+ * Result: RE
+ * Execution Time: 202 ms
+ */
+
+// code not found
