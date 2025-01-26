77742893c808d7d62284ff640609841740b69f3d
thirofoo <thirofoo@gmail.com>
2025-01-26 23:24:49 +0900

    [AC] SubmissionID: 62139465 at 2025-01-26 23:24:49 (1737901489)

---
+++ arc191/arc191_c_62139465_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/arc191/submissions/62139465
+ * Submitted at: 2025-01-26 23:24:49
+ * Problem URL: https://atcoder.jp/contests/arc191/tasks/arc191_c
+ * Result: AC
+ * Execution Time: 15 ms
+ */
+
+// code not found
