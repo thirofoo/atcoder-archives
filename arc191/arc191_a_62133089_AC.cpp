7a5f0cf9a75d090e32e8d6358244cf7becd220a1
thirofoo <thirofoo@gmail.com>
2025-01-26 21:50:52 +0900

    [AC] SubmissionID: 62133089 at 2025-01-26 21:50:52 (1737895852)

---
+++ arc191/arc191_a_62133089_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/arc191/submissions/62133089
+ * Submitted at: 2025-01-26 21:50:52
+ * Problem URL: https://atcoder.jp/contests/arc191/tasks/arc191_a
+ * Result: AC
+ * Execution Time: 10 ms
+ */
+
+// code not found
