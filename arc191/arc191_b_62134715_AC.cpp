2f06a1d8cfbbc9115675e7970b5369363de8f925
thirofoo <thirofoo@gmail.com>
2025-01-26 22:12:25 +0900

    [AC] SubmissionID: 62134715 at 2025-01-26 22:12:25 (1737897145)

---
+++ arc191/arc191_b_62134715_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/arc191/submissions/62134715
+ * Submitted at: 2025-01-26 22:12:25
+ * Problem URL: https://atcoder.jp/contests/arc191/tasks/arc191_b
+ * Result: AC
+ * Execution Time: 284 ms
+ */
+
+// code not found
