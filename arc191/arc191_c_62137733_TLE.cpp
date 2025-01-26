285ad40a96beaf9b5371f2f9298245ee00a32be6
thirofoo <thirofoo@gmail.com>
2025-01-26 22:55:14 +0900

    [TLE] SubmissionID: 62137733 at 2025-01-26 22:55:14 (1737899714)

---
+++ arc191/arc191_c_62137733_TLE.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/arc191/submissions/62137733
+ * Submitted at: 2025-01-26 22:55:14
+ * Problem URL: https://atcoder.jp/contests/arc191/tasks/arc191_c
+ * Result: TLE
+ * Execution Time: 2210 ms
+ */
+
+// code not found
