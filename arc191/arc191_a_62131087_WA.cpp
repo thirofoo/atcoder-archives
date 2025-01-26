4d7885713d67005c4cb19b3670968d059aa996de
thirofoo <thirofoo@gmail.com>
2025-01-26 21:29:21 +0900

    [WA] SubmissionID: 62131087 at 2025-01-26 21:29:21 (1737894561)

---
+++ arc191/arc191_a_62131087_WA.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/arc191/submissions/62131087
+ * Submitted at: 2025-01-26 21:29:21
+ * Problem URL: https://atcoder.jp/contests/arc191/tasks/arc191_a
+ * Result: WA
+ * Execution Time: 10 ms
+ */
+
+// code not found
