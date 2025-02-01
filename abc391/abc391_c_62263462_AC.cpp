b8201662164b400569017c45811917d8dda76b1b
thirofoo <thirofoo@gmail.com>
2025-02-01 21:05:17 +0900

    [AC] SubmissionID: 62263462 at 2025-02-01 21:05:17 (1738411517)

---
+++ abc391/abc391_c_62263462_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc391/submissions/62263462
+ * Submitted at: 2025-02-01 21:05:17
+ * Problem URL: https://atcoder.jp/contests/abc391/tasks/abc391_c
+ * Result: AC
+ * Execution Time: 92 ms
+ */
+
+// code not found
