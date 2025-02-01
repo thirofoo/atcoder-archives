5e3c34bc59ed55f6cb0449baa1acc167390c102f
thirofoo <thirofoo@gmail.com>
2025-02-01 21:20:39 +0900

    [AC] SubmissionID: 62275701 at 2025-02-01 21:20:39 (1738412439)

---
+++ abc391/abc391_d_62275701_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc391/submissions/62275701
+ * Submitted at: 2025-02-01 21:20:39
+ * Problem URL: https://atcoder.jp/contests/abc391/tasks/abc391_d
+ * Result: AC
+ * Execution Time: 98 ms
+ */
+
+// code not found
