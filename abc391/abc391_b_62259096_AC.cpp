e08ef23914f93317c43aaead0e84f78d27002ac6
thirofoo <thirofoo@gmail.com>
2025-02-01 21:02:40 +0900

    [AC] SubmissionID: 62259096 at 2025-02-01 21:02:40 (1738411360)

---
+++ abc391/abc391_b_62259096_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc391/submissions/62259096
+ * Submitted at: 2025-02-01 21:02:40
+ * Problem URL: https://atcoder.jp/contests/abc391/tasks/abc391_b
+ * Result: AC
+ * Execution Time: 1 ms
+ */
+
+// code not found
