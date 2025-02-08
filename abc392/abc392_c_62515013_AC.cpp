6ea5706e2d591fbbfee99c1493fe189499381401
thirofoo <thirofoo@gmail.com>
2025-02-08 21:08:59 +0900

    [AC] SubmissionID: 62515013 at 2025-02-08 21:08:59 (1739016539)

---
+++ abc392/abc392_c_62515013_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc392/submissions/62515013
+ * Submitted at: 2025-02-08 21:08:59
+ * Problem URL: https://atcoder.jp/contests/abc392/tasks/abc392_c
+ * Result: AC
+ * Execution Time: 75 ms
+ */
+
+// code not found
