003f2c4de2e4839692670d62f0b5e42137722d5f
thirofoo <thirofoo@gmail.com>
2025-02-08 21:57:07 +0900

    [AC] SubmissionID: 62545561 at 2025-02-08 21:57:07 (1739019427)

---
+++ abc392/abc392_e_62545561_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc392/submissions/62545561
+ * Submitted at: 2025-02-08 21:57:07
+ * Problem URL: https://atcoder.jp/contests/abc392/tasks/abc392_e
+ * Result: AC
+ * Execution Time: 64 ms
+ */
+
+// code not found
