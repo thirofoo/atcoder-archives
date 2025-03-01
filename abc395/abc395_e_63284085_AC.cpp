b12ea656179f8457c0dae84ef11f702c8e243bef
thirofoo <thirofoo@gmail.com>
2025-03-01 21:53:56 +0900

    [AC] SubmissionID: 63284085 at 2025-03-01 21:53:56 (1740833636)

---
+++ abc395/abc395_e_63284085_AC.cpp
@@ -0,0 +1,59 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc395/submissions/63284085
+ * Submitted at: 2025-03-01 21:53:56
+ * Problem URL: https://atcoder.jp/contests/abc395/tasks/abc395_e
+ * Result: AC
+ * Execution Time: 283 ms
+ */
+
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+// using mint = modint998244353;
+// using mint = modint1000000007;
+using ll  = long long;
+using ull = unsigned long long;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+constexpr ll INF = ((1LL << 61) + (1LL << 30) - 1);
+
+int main() {
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+
+  ll n, m, x;
+  cin >> n >> m >> x;
+  using P = pair<ll, ll>;
+  vector<vector<P>> graph(n * 2, vector<P>{});
+  rep(i, m) {
+    ll a, b;
+    cin >> a >> b;
+    a--, b--;
+    graph[a].emplace_back(P(b, 1));
+    graph[b + n].emplace_back(P(a + n, 1));
+    graph[a].emplace_back(P(a + n, x));
+    graph[a + n].emplace_back(P(a, x));
+    graph[b].emplace_back(P(b + n, x));
+    graph[b + n].emplace_back(P(b, x));
+  }
+  vector<ll> dist(n * 2, INF);
+  priority_queue<P, vector<P>, greater<P>> q;
+  q.push({0, 0});
+
+  while(!q.empty()) {
+    auto [d, v] = q.top();
+    q.pop();
+    if(dist[v] < d) continue;
+    dist[v] = d;
+
+    for(auto [nv, c]: graph[v]) {
+      if(dist[nv] <= d + c) continue;
+      dist[nv] = d + c;
+      q.push({d + c, nv});
+    }
+  }
+  cout << min(dist[n - 1], dist[n * 2 - 1]) << '\n';
+
+  return 0;
+}
