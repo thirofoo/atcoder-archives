819a985ed7af6c8ca77978836cce30acf7f03cad
thirofoo <thirofoo@gmail.com>
2025-04-05 21:13:54 +0900

    [WA] SubmissionID: 64516804 at 2025-04-05 21:13:54 (1743855234)

---
+++ abc400/abc400_c_64516804_WA.cpp
@@ -0,0 +1,43 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc400/submissions/64516804
+ * Submitted at: 2025-04-05 21:13:54
+ * Problem URL: https://atcoder.jp/contests/abc400/tasks/abc400_c
+ * Result: WA
+ * Execution Time: 1 ms
+ */
+
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+// using mint = modint998244353;
+// using mint = modint1000000007;
+using ll  = long long;
+using ull = unsigned long long;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+constexpr ll INF = ((1LL << 61) + (1LL << 30) - 1);
+
+int main() {
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+
+  ll n;
+  cin >> n;
+  ll ans = 0, tmp = 2, upper = 1000000000;
+  while(tmp <= n) {
+    ll left = 1, right = upper;
+    while(right - left > 1) {
+      ll mid = (left + right) / 2;
+      if(mid * mid * tmp <= n) left = mid;
+      else right = mid;
+    }
+    ans += (left + 1) / 2;
+    // cerr << left << " " << tmp << " " << ans << endl;
+    tmp *= 2;
+    upper /= 2;
+  }
+  cout << ans << endl;
+
+  return 0;
+}
