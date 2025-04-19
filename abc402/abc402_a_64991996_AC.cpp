db5b97b56b70dcf63c45ef11c2781d0f94d73d24
thirofoo <thirofoo@gmail.com>
2025-04-19 21:00:56 +0900

    [AC] SubmissionID: 64991996 at 2025-04-19 21:00:56 (1745064056)

---
+++ abc402/abc402_a_64991996_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc402/submissions/64991996
+ * Submitted at: 2025-04-19 21:00:56
+ * Problem URL: https://atcoder.jp/contests/abc402/tasks/abc402_a
+ * Result: AC
+ * Execution Time: 1 ms
+ */
+
+// code not found
