537d51d0c4a21c1b3894086526863d309ca245ad
thirofoo <thirofoo@gmail.com>
2025-04-19 23:51:53 +0900

    [WA] SubmissionID: 65044776 at 2025-04-19 23:51:53 (1745074313)

---
+++ abc402/abc402_e_65044776_WA.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc402/submissions/65044776
+ * Submitted at: 2025-04-19 23:51:53
+ * Problem URL: https://atcoder.jp/contests/abc402/tasks/abc402_e
+ * Result: WA
+ * Execution Time: 2209 ms
+ */
+
+// code not found
