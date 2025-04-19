669410295907132943c94bea4bd2a1442d1204da
thirofoo <thirofoo@gmail.com>
2025-04-19 15:08:47 +0900

    [AC] SubmissionID: 64983064 at 2025-04-19 15:08:47 (1745042927)

---
+++ masters2025-final-open/masters2025_final_c_64983064_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/masters2025-final-open/submissions/64983064
+ * Submitted at: 2025-04-19 15:08:47
+ * Problem URL: https://atcoder.jp/contests/masters2025-final-open/tasks/masters2025_final_c
+ * Result: AC
+ * Execution Time: 1951 ms
+ */
+
+// code not found
