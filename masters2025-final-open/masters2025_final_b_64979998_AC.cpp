6f625654cfe031a96dd6197bb92d0c82130d21b2
thirofoo <thirofoo@gmail.com>
2025-04-19 12:30:04 +0900

    [AC] SubmissionID: 64979998 at 2025-04-19 12:30:04 (1745033404)

---
+++ masters2025-final-open/masters2025_final_b_64979998_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/masters2025-final-open/submissions/64979998
+ * Submitted at: 2025-04-19 12:30:04
+ * Problem URL: https://atcoder.jp/contests/masters2025-final-open/tasks/masters2025_final_b
+ * Result: AC
+ * Execution Time: 1952 ms
+ */
+
+// code not found
