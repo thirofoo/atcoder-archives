8e90bd9e6824ff131788803126e6ff8e40073add
thirofoo <thirofoo@gmail.com>
2025-04-19 16:48:29 +0900

    [AC] SubmissionID: 64985756 at 2025-04-19 16:48:29 (1745048909)

---
+++ masters2025-final-open/masters2025_final_a_64985756_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/masters2025-final-open/submissions/64985756
+ * Submitted at: 2025-04-19 16:48:29
+ * Problem URL: https://atcoder.jp/contests/masters2025-final-open/tasks/masters2025_final_a
+ * Result: AC
+ * Execution Time: 992 ms
+ */
+
+// code not found
