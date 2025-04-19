5d76dfac7a9018c22b479cc54dcdcb28956157e0
thirofoo <thirofoo@gmail.com>
2025-04-19 22:39:38 +0900

    [AC] SubmissionID: 65040080 at 2025-04-19 22:39:38 (1745069978)

---
+++ abc402/abc402_e_65040080_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc402/submissions/65040080
+ * Submitted at: 2025-04-19 22:39:38
+ * Problem URL: https://atcoder.jp/contests/abc402/tasks/abc402_e
+ * Result: AC
+ * Execution Time: 125 ms
+ */
+
+// code not found
