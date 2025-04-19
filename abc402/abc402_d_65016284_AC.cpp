97c82d10a3bbf1c2d99004a28c64c3c465fcf8e5
thirofoo <thirofoo@gmail.com>
2025-04-19 21:25:31 +0900

    [AC] SubmissionID: 65016284 at 2025-04-19 21:25:31 (1745065531)

---
+++ abc402/abc402_d_65016284_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc402/submissions/65016284
+ * Submitted at: 2025-04-19 21:25:31
+ * Problem URL: https://atcoder.jp/contests/abc402/tasks/abc402_d
+ * Result: AC
+ * Execution Time: 61 ms
+ */
+
+// code not found
