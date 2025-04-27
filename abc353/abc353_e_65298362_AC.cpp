984d0704e42a2d501802ccf9dd784859a1a2abf2
thirofoo <thirofoo@gmail.com>
2025-04-28 02:25:28 +0900

    [AC] SubmissionID: 65298362 at 2025-04-28 02:25:28 (1745774728)

---
+++ abc353/abc353_e_65298362_AC.cpp
@@ -0,0 +1,129 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc353/submissions/65298362
+ * Submitted at: 2025-04-28 02:25:28
+ * Problem URL: https://atcoder.jp/contests/abc353/tasks/abc353_e
+ * Result: AC
+ * Execution Time: 37 ms
+ */
+
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+// using mint = modint998244353;
+// using mint = modint1000000007;
+using ll  = long long;
+using ull = unsigned long long;
+using P   = pair<ll, ll>;
+using T   = tuple<ll, ll, ll>;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+constexpr ll INF = ((1LL << 61) + (1LL << 30) - 1);
+
+// ==================== Trie ====================
+/* verify: https://atcoder.jp/contests/abc403/submissions/65298165 */
+template<size_t AlphabetSize, char MinChar = 'a'>
+struct Trie {
+  struct Node {
+    array<int, AlphabetSize> next;
+    ll cnt  = 0; // 末端に達した単語数
+    ll pass = 0; // このノードを通過した単語数
+    Node() {
+      next.fill(-1);
+    }
+  };
+
+  vector<Node> nodes;
+  Trie() {
+    nodes.emplace_back();
+  }
+
+  // 挿入
+  void insert(const string& s) {
+    ll u = 0;
+    nodes[u].pass++;
+    for(char ch: s) {
+      ll c = ch - MinChar;
+      if(c < 0 || c >= (ll) AlphabetSize) return;
+      if(nodes[u].next[c] < 0) {
+        nodes[u].next[c] = nodes.size();
+        nodes.emplace_back();
+      }
+      u = nodes[u].next[c];
+      nodes[u].pass++;
+    }
+    nodes[u].cnt++;
+  }
+
+  // s と完全一致する単語数 (prefix=false)、
+  // s を prefix にもつ単語数 (prefix=true)
+  ll search(const string& s, bool prefix = false) const {
+    ll u = 0;
+    for(char ch: s) {
+      ll c = ch - MinChar;
+      if(c < 0 || c >= (ll) AlphabetSize) return 0;
+      u = nodes[u].next[c];
+      if(u < 0) return 0;
+    }
+    return prefix ? nodes[u].pass : nodes[u].cnt;
+  }
+
+  // s と完全一致する文字列の個数
+  inline ll count_equal(const string& s) const {
+    return search(s, false);
+  }
+
+  // s を prefix として持つ文字列の個数
+  inline ll count_with_prefix(const string& s) const {
+    return search(s, true);
+  }
+
+  // s の中に完全に含まれる（挿入済みの文字列が s の prefix となる）文字列の個数
+  inline ll count_prefix_of(const string& s) const {
+    int u  = 0;
+    ll ans = 0;
+    for(char ch: s) {
+      int c = ch - MinChar;
+      if(c < 0 || c >= (int) AlphabetSize) break;
+      u = nodes[u].next[c];
+      if(u < 0) break;
+      ans += nodes[u].cnt;
+    }
+    return ans;
+  }
+
+  // LPS (Longest Prefix Suffix) の長さの総和
+  inline ll lps(const string& s) const {
+    int u  = 0;
+    ll ans = 0;
+    for(char ch: s) {
+      int c = ch - MinChar;
+      if(c < 0 || c >= (int) AlphabetSize) break;
+      u = nodes[u].next[c];
+      if(u < 0) break;
+      ans += nodes[u].pass;
+    }
+    return ans;
+  }
+};
+
+int main() {
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+
+  ll n;
+  cin >> n;
+  vector<string> s(n);
+  rep(i, n) cin >> s[i];
+
+  Trie<26, 'a'> trie;
+  ll ans = 0;
+  for(ll i = 0; i < n; ++i) {
+    ans += trie.lps(s[i]);
+    trie.insert(s[i]);
+    // cerr << ans << endl;
+  }
+  cout << ans << endl;
+
+  return 0;
+}
