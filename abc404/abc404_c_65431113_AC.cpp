c079871aa1723b632f5fd8fddecf9e7956ab185b
thirofoo <thirofoo@gmail.com>
2025-05-03 21:11:22 +0900

    [AC] SubmissionID: 65431113 at 2025-05-03 21:11:22 (1746274282)

---
+++ abc404/abc404_c_65431113_AC.cpp
@@ -0,0 +1,45 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc404/submissions/65431113
+ * Submitted at: 2025-05-03 21:11:22
+ * Problem URL: https://atcoder.jp/contests/abc404/tasks/abc404_c
+ * Result: AC
+ * Execution Time: 31 ms
+ */
+
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+// using mint = modint998244353;
+// using mint = modint1000000007;
+using ll  = long long;
+using ull = unsigned long long;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+constexpr ll INF = ((1LL << 61) + (1LL << 30) - 1);
+
+int main() {
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+
+  ll n, m;
+  cin >> n >> m;
+  dsu uf(n);
+  bool cycle = false;
+  vector<ll> cnt(n, 0);
+  rep(i, m) {
+    ll a, b;
+    cin >> a >> b;
+    a--, b--;
+    cnt[a]++;
+    cnt[b]++;
+    cycle |= uf.same(a, b);
+    uf.merge(a, b);
+  }
+  rep(i, n) cycle &= (cnt[i] == 2);
+  cycle &= (m == n);
+  cycle &= (uf.groups().size() == 1);
+  cout << (cycle ? "Yes" : "No") << endl;
+
+  return 0;
+}
