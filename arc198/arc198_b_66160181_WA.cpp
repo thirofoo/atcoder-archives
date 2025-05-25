a2cd6870101c12dc2d4fe9aea3fd58b8e7608e1b
thirofoo <thirofoo@gmail.com>
2025-05-25 21:21:12 +0900

    [WA] SubmissionID: 66160181 at 2025-05-25 21:21:12 (1748175672)

---
+++ arc198/arc198_b_66160181_WA.cpp
@@ -0,0 +1,35 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/arc198/submissions/66160181
+ * Submitted at: 2025-05-25 21:21:12
+ * Problem URL: https://atcoder.jp/contests/arc198/tasks/arc198_b
+ * Result: WA
+ * Execution Time: 224 ms
+ */
+
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+// using mint = modint998244353;
+// using mint = modint1000000007;
+using ll  = long long;
+using ull = unsigned long long;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+constexpr ll INF = ((1LL << 61) + (1LL << 30) - 1);
+
+int main() {
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+
+  ll t;
+  cin >> t;
+  while(t--) {
+    ll x, y, z;
+    cin >> x >> y >> z;
+    if(x == 0 || z > x || 2 * z < y) cout << "No" << endl;
+    else cout << "Yes" << endl;
+  }
+
+  return 0;
+}
