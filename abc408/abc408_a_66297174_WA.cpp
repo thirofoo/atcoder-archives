683e3892fb3c41d4fce7e4b892b29c17710fe7b8
thirofoo <thirofoo@gmail.com>
2025-05-31 21:02:20 +0900

    [WA] SubmissionID: 66297174 at 2025-05-31 21:02:20 (1748692940)

---
+++ abc408/abc408_a_66297174_WA.cpp
@@ -0,0 +1,34 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc408/submissions/66297174
+ * Submitted at: 2025-05-31 21:02:20
+ * Problem URL: https://atcoder.jp/contests/abc408/tasks/abc408_a
+ * Result: WA
+ * Execution Time: 1 ms
+ */
+
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+// using mint = modint998244353;
+// using mint = modint1000000007;
+using ll  = long long;
+using ull = unsigned long long;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+constexpr ll INF = ((1LL << 61) + (1LL << 30) - 1);
+
+int main() {
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+
+  ll n, s;
+  cin >> n >> s;
+  vector<ll> t(n);
+  rep(i, n) cin >> t[i];
+  bool ok = true;
+  rep(i, n - 1) ok &= abs(t[i] - t[i + 1]) <= s;
+  cout << (ok ? "Yes" : "No") << endl;
+
+  return 0;
+}
