f12b84f63ba2f61b10ef06244b34fbebe2cbfbc6
thirofoo <thirofoo@gmail.com>
2025-06-07 21:06:28 +0900

    [AC] SubmissionID: 66514012 at 2025-06-07 21:06:28 (1749297988)

---
+++ abc409/abc409_b_66514012_AC.cpp
@@ -0,0 +1,39 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc409/submissions/66514012
+ * Submitted at: 2025-06-07 21:06:28
+ * Problem URL: https://atcoder.jp/contests/abc409/tasks/abc409_b
+ * Result: AC
+ * Execution Time: 1 ms
+ */
+
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+// using mint = modint998244353;
+// using mint = modint1000000007;
+using ll  = long long;
+using ull = unsigned long long;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+constexpr ll INF = ((1LL << 61) + (1LL << 30) - 1);
+
+int main() {
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+
+  ll n;
+  cin >> n;
+  vector<ll> a(n);
+  rep(i, n) cin >> a[i];
+
+  ll ans = 0;
+  rep(x, 200) {
+    ll cnt = 0;
+    rep(i, n) cnt += (a[i] >= x);
+    if(cnt >= x) ans = max(ans, x);
+  }
+  cout << ans << endl;
+
+  return 0;
+}
