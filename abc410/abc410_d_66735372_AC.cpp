68b53d42dfb57b1c544f13818041885556703cc3
thirofoo <thirofoo@gmail.com>
2025-06-14 21:15:19 +0900

    [AC] SubmissionID: 66735372 at 2025-06-14 21:15:19 (1749903319)

---
+++ abc410/abc410_d_66735372_AC.cpp
@@ -0,0 +1,59 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc410/submissions/66735372
+ * Submitted at: 2025-06-14 21:15:19
+ * Problem URL: https://atcoder.jp/contests/abc410/tasks/abc410_d
+ * Result: AC
+ * Execution Time: 7 ms
+ */
+
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+// using mint = modint998244353;
+// using mint = modint1000000007;
+using ll  = long long;
+using ull = unsigned long long;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+constexpr ll INF = ((1LL << 61) + (1LL << 30) - 1);
+
+int main() {
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+
+  ll n, m;
+  cin >> n >> m;
+  using P = pair<ll, ll>;
+  vector<vector<P>> graph(n);
+  rep(i, m) {
+    ll a, b, w;
+    cin >> a >> b >> w;
+    a--, b--;
+    graph[a].emplace_back(b, w);
+  }
+  vector<vector<bool>> visited(n, vector<bool>((1LL << 10), false));
+  queue<P> q;
+  q.emplace(0, 0);
+  while(!q.empty()) {
+    auto [node, mask] = q.front();
+    q.pop();
+    if(visited[node][mask]) continue;
+    visited[node][mask] = true;
+    for(auto [next_node, weight]: graph[node]) {
+      ll next_mask = mask ^ weight;
+      if(!visited[next_node][next_mask]) {
+        q.emplace(next_node, next_mask);
+      }
+    }
+  }
+  rep(i, (1LL << 10)) {
+    if(visited[n - 1][i]) {
+      cout << i << endl;
+      return 0;
+    }
+  }
+  cout << -1 << endl;
+
+  return 0;
+}
