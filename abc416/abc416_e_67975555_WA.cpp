9aa41ec3fc1439c6c113bbe9b02ca9b549523a25
thirofoo <thirofoo@gmail.com>
2025-07-26 23:49:37 +0900

    [WA] SubmissionID: 67975555 at 2025-07-26 23:49:37 (1753541377)

---
+++ abc416/abc416_e_67975555_WA.cpp
@@ -0,0 +1,90 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc416/submissions/67975555
+ * Submitted at: 2025-07-26 23:49:37
+ * Problem URL: https://atcoder.jp/contests/abc416/tasks/abc416_e
+ * Result: WA
+ * Execution Time: 403 ms
+ */
+
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+// using mint = modint998244353;
+// using mint = modint1000000007;
+using ll  = long long;
+using ull = unsigned long long;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+constexpr ll INF = ((1LL << 61) + (1LL << 30) - 1);
+
+int main() {
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+
+  ll n, m;
+  cin >> n >> m;
+  using P = pair<ll, ll>;
+  vector<vector<ll>> dist(n + 1, vector<ll>(n + 1, INF));
+  rep(i, n) dist[i][i] = 0;
+  rep(i, m) {
+    ll a, b, c;
+    cin >> a >> b >> c;
+    a--, b--;
+    dist[a][b] = min(dist[a][b], c);
+    dist[b][a] = min(dist[b][a], c);
+  }
+
+  //
+
+  ll k, t;
+  cin >> k >> t;
+  vector<ll> d(k);
+  rep(i, k) {
+    cin >> d[i];
+    d[i]--;
+    dist[d[i]][n] = t;
+    dist[n][d[i]] = 0;
+  }
+  rep(l, n + 1) rep(i, n + 1) rep(j, n + 1) {
+    if(dist[i][j] > dist[i][l] + dist[l][j]) {
+      dist[i][j] = dist[i][l] + dist[l][j];
+    }
+  }
+
+  ll q;
+  cin >> q;
+  while(q--) {
+    ll type;
+    cin >> type;
+    if(type == 1) {
+      ll x, y, time;
+      cin >> x >> y >> time;
+      x--, y--;
+      dist[x][y] = min(dist[x][y], time);
+      dist[y][x] = min(dist[y][x], time);
+      rep(i, n + 1) rep(j, n + 1) {
+        dist[i][j] = min({dist[i][j], dist[i][x] + dist[x][j]});
+        dist[i][j] = min({dist[i][j], dist[i][y] + dist[y][j]});
+      }
+    } else if(type == 2) {
+      ll x;
+      cin >> x;
+      x--;
+      dist[x][n] = min(dist[x][n], t);
+      dist[n][x] = min(dist[n][x], 0LL);
+      rep(i, n + 1) rep(j, n + 1) {
+        dist[i][j] = min({dist[i][j], dist[i][x] + dist[x][j]});
+        dist[i][j] = min({dist[i][j], dist[i][n] + dist[n][j]});
+      }
+    } else {
+      ll ans = 0;
+      rep(i, n) rep(j, n) {
+        if(dist[i][j] != INF) ans += dist[i][j];
+      }
+      cout << ans << endl;
+    }
+  }
+
+  return 0;
+}
