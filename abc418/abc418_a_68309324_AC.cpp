df90c3d501c688d3c88eb557c78c97a64d36538b
thirofoo <thirofoo@gmail.com>
2025-08-09 21:01:47 +0900

    [AC] SubmissionID: 68309324 at 2025-08-09 21:01:47 (1754740907)

---
+++ abc418/abc418_a_68309324_AC.cpp
@@ -0,0 +1,33 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc418/submissions/68309324
+ * Submitted at: 2025-08-09 21:01:47
+ * Problem URL: https://atcoder.jp/contests/abc418/tasks/abc418_a
+ * Result: AC
+ * Execution Time: 1 ms
+ */
+
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+// using mint = modint998244353;
+// using mint = modint1000000007;
+using ll  = long long;
+using ull = unsigned long long;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+constexpr ll INF = ((1LL << 61) + (1LL << 30) - 1);
+
+int main() {
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+
+  ll n;
+  cin >> n;
+  string s;
+  cin >> s;
+  s = "???" + s;
+  cout << (s[s.size() - 3] == 't' && s[s.size() - 2] == 'e' && s[s.size() - 1] == 'a' ? "Yes" : "No") << endl;
+
+  return 0;
+}
