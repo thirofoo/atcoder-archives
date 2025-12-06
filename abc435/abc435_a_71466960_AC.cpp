e89073bd5841531105bf4534577e7e496f03ca0e
thirofoo <thirofoo@gmail.com>
2025-12-06 21:01:03 +0900

    [AC] SubmissionID: 71466960 at 2025-12-06 21:01:03 (1765022463)

---
+++ abc435/abc435_a_71466960_AC.cpp
@@ -0,0 +1,30 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc435/submissions/71466960
+ * Submitted at: 2025-12-06 21:01:03
+ * Problem URL: https://atcoder.jp/contests/abc435/tasks/abc435_a
+ * Result: AC
+ * Execution Time: 1 ms
+ */
+
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+// using mint = modint998244353;
+// using mint = modint1000000007;
+using ll  = long long;
+using ull = unsigned long long;
+#define rep(i, n) for(ll i = 0; i < n; i++)
+constexpr ll INF = ((1LL << 61) + (1LL << 30) - 1);
+
+int main() {
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+
+  ll n;
+  cin >> n;
+  cout << (n + 1) * n / 2 << "\n";
+
+  return 0;
+}
