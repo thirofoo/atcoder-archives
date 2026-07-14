541a2060d050b55df008fe82272b6368ef036f11
thirofoo <thirofoo@gmail.com>
2026-07-14 19:13:17 +0900

    [AC] SubmissionID: 77484451 at 2026-07-14 19:13:17 (1784023997)

---
+++ abc466/abc466_c_77484451_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc466/submissions/77484451
+ * Submitted at: 2026-07-14 19:13:17
+ * Problem URL: https://atcoder.jp/contests/abc466/tasks/abc466_c
+ * Result: AC
+ * Execution Time: 9 ms
+ */
+
+// code not found
