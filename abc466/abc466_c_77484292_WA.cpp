0b1c1f20119e1e51c58652a5be393347278cd8ea
thirofoo <thirofoo@gmail.com>
2026-07-14 19:01:50 +0900

    [WA] SubmissionID: 77484292 at 2026-07-14 19:01:50 (1784023310)

---
+++ abc466/abc466_c_77484292_WA.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/abc466/submissions/77484292
+ * Submitted at: 2026-07-14 19:01:50
+ * Problem URL: https://atcoder.jp/contests/abc466/tasks/abc466_c
+ * Result: WA
+ * Execution Time: 10 ms
+ */
+
+// code not found
