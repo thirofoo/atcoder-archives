5dfd1d3d2d3cfb3eae2cf374a09304d57c6b84fe
thirofoo <thirofoo@gmail.com>
2026-07-18 18:39:53 +0900

    [AC] SubmissionID: 77563768 at 2026-07-18 18:39:53 (1784367593)

---
+++ ahc068/ahc068_a_77563768_AC.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: through
+ * Submission URL: https://atcoder.jp/contests/ahc068/submissions/77563768
+ * Submitted at: 2026-07-18 18:39:53
+ * Problem URL: https://atcoder.jp/contests/ahc068/tasks/ahc068_a
+ * Result: AC
+ * Execution Time: 1949 ms
+ */
+
+// code not found
